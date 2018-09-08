b3b1de0e71315e10ec5bd443454ba598bb83f3ef
James Nylen <jnylen@gmail.com>
2018-09-07 22:24:44 -0500

    Update table prefix in wp-config-sample.php

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -63,7 +63,7 @@ define('NONCE_SALT',       'put your unique phrase here');
  * You can have multiple installations in one database if you give each
  * a unique prefix. Only numbers, letters, and underscores please!
  */
-$table_prefix  = 'wp_';
+$table_prefix  = 'cp_';
 
 /**
  * For developers: ClassicPress debugging mode.
