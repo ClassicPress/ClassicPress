30dfe581a02475577481812d26f895a287fa2987
Luiz Paulo "Bills <luizpbills@gmail.com>
2018-09-11 17:22:29 -0300

    change 'Howdy' to 'Hello'

--- src/wp-includes/admin-bar.php
+++ src/wp-includes/admin-bar.php
@@ -218,7 +218,7 @@ function wp_admin_bar_my_account_item( $wp_admin_bar ) {
 
 	$avatar = get_avatar( $user_id, 26 );
 	/* translators: %s: current user's display name */
-	$howdy  = sprintf( __( 'Howdy, %s' ), '<span class="display-name">' . $current_user->display_name . '</span>' );
+	$howdy  = sprintf( __( 'Hello, %s' ), '<span class="display-name">' . $current_user->display_name . '</span>' );
 	$class  = empty( $avatar ) ? '' : 'with-avatar';
 
 	$wp_admin_bar->add_menu( array(
