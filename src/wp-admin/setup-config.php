ClassicPress/ClassicPress

commit bc5b8d8342d1d646fa4f8421cf327b70d32206ab

Update version; start planning future versions (#147)
* Update version; start planning future versions

* Make `$wp_version` the latest WP release

* Modify `$cp_version` during build

src/wp-includes/version.php
@@ -2,9 +2,32 @@
 /**
  * The ClassicPress version string
  *
+ * This string is the "base" version of the current source tree.  It may have
+ * one of the following semver-compliant formats:
+ *
+ * - 1.0.0-alpha0+dev  : development version, before any official releases of
+ *                       the 1.0.0 series
+ * - 1.0.0-alpha1      : official release 1.0.0-alpha1
+ * - 1.0.0-alpha2+dev  : the commit immediately after 1.0.0-alpha1, and any
+ *                       development commits before the next release
+ * - 1.0.0-beta1       : official release 1.0.0-beta1
+ * - 1.0.0-beta2+dev   : the commit immediately after 1.0.0-beta1, and any
+ *                       development commits before the next release
+ * - 1.0.0             : official release 1.0.0
+ * - 1.0.1-alpha0+dev  : the commit immediately after 1.0.0, and any
+ *                       development commits before the 1.0.1 alpha, beta, or
+ *                       final release(s)
+ *
+ * In the source repository this string will always contain the '+dev' suffix.
+ * In released builds it will never contain the '+dev' suffix.
+ *
+ * When nightly (development) builds are created, this suffix is automatically
+ * updated to e.g. '+build.20181019'.  When alpha, beta, or final release
+ * builds are created, the suffix is removed.
+ *
  * @global string $cp_version
  */
-$cp_version = '1.0.0-alpha';
+$cp_version = '1.0.0-alpha0+dev';
 
 /**
  * Return the ClassicPress version string.
@@ -32,7 +55,7 @@ if ( ! function_exists( 'classicpress_version' ) ) {
  *
  * @global string $wp_version
  */
-$wp_version = '4.9.9-alpha-43554-src';
+$wp_version = '4.9.8';
 
 /**
  * Holds the ClassicPress DB revision, increments when changes are made to the ClassicPress DB schema.

Gruntfile.js
@@ -150,14 +150,26 @@ module.exports = function(grunt) {
 			version: {
 				options: {
 					processContent: function( src ) {
-						return src.replace( /^\$wp_version = '(.+?)';/m, function( str, version ) {
-							version = version.replace( /-src$/, '' );
-
-							// If the version includes an SVN commit (-12345), it's not a released alpha/beta. Append a timestamp.
-							version = version.replace( /-[\d]{5}$/, '-' + grunt.template.today( 'yyyymmdd.HHMMss' ) );
+						return src.replace( /^\$cp_version = '(.+?)';/m, function( str, version ) {
+							if ( process.env.CLASSICPRESS_RELEASE ) {
+								// This is an official build.  Remove the
+								// '+dev' suffix from the source tree.
+								version = version.replace( /\+dev$/, '' );
+							} else {
+								// This is a nightly build.  Replace the '+dev'
+								// suffix from the source tree with e.g.
+								// '+build.20181019'.  Use yesterday's date -
+								// nightly builds are baked at midnight UTC.
+								var d = new Date();
+								d.setDate( d.getDate() - 1 );
+								version = version.replace(
+									/\+dev$/,
+									'+build.' + grunt.template.date( d, 'yyyymmdd' )
+								);
+							}
 
 							/* jshint quotmark: true */
-							return "$wp_version = '" + version + "';";
+							return "$cp_version = '" + version + "';";
 						});
 					}
 				},

commit 37a70895061de3ed27141a187636c99f7554154b

Improve handling of development versions
Closes #58.

Fix some breakage introduced by #147 with installs directly from the
source repository, and introduces a more unified way to make this check.

src/wp-admin/setup-config.php
@@ -40,7 +40,7 @@ require_once( ABSPATH . 'wp-admin/includes/translation-install.php' );
 
 nocache_headers();
 
-// Support wp-config-sample.php one level up, for the develop repo.
+// Support wp-config-sample.php one level up, for the source repository.
 if ( file_exists( ABSPATH . 'wp-config-sample.php' ) ) {
 	$config_file = file( ABSPATH . 'wp-config-sample.php' );
 } elseif ( file_exists( dirname( ABSPATH ) . '/wp-config-sample.php' ) ) {

src/wp-includes/class-wp-editor.php
@@ -1411,7 +1411,7 @@ final class _WP_Editors {
 			&& false !== stripos( $_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip' ) && ! $has_custom_theme;
 
 		// Load tinymce.js when running from /src, else load wp-tinymce.js.gz (production) or tinymce.min.js (SCRIPT_DEBUG)
-		$mce_suffix = false !== strpos( get_bloginfo( 'version' ), '-src' ) ? '' : '.min';
+		$mce_suffix = classicpress_is_dev_install() ? '' : '.min';
 
 		if ( $compressed ) {
 			echo "<script type='text/javascript' src='{$baseurl}/wp-tinymce.php?c=1&amp;$version'></script>\n";

src/wp-includes/class-wp-locale.php
@@ -228,7 +228,7 @@ class WP_Locale {
 		elseif ( 'rtl' == _x( 'ltr', 'text direction' ) )
 			$this->text_direction = 'rtl';
 
-		if ( 'rtl' === $this->text_direction && strpos( get_bloginfo( 'version' ), '-src' ) ) {
+		if ( 'rtl' === $this->text_direction && classicpress_is_dev_install() ) {
 			$this->text_direction = 'ltr';
 			add_action( 'all_admin_notices', array( $this, 'rtl_src_admin_notice' ) );
 		}
@@ -241,7 +241,7 @@ class WP_Locale {
 	 */
 	public function rtl_src_admin_notice() {
 		/* translators: %s: Name of the directory (build) */
-		echo '<div class="error"><p>' . sprintf( __( 'The %s directory of the develop repository must be used for RTL.' ), '<code>build</code>' ) . '</p></div>';
+		echo '<div class="error"><p>' . sprintf( __( 'The %s directory of the source repository must be used for RTL.' ), '<code>build</code>' ) . '</p></div>';
 	}
 
 	/**

src/wp-includes/default-constants.php
@@ -84,8 +84,8 @@ function wp_initial_constants() {
 	// Add define('SCRIPT_DEBUG', true); to wp-config.php to enable loading of non-minified,
 	// non-concatenated scripts and stylesheets.
 	if ( ! defined( 'SCRIPT_DEBUG' ) ) {
-		if ( ! empty( $GLOBALS['wp_version'] ) ) {
-			$develop_src = false !== strpos( $GLOBALS['wp_version'], '-src' );
+		if ( ! empty( $GLOBALS['cp_version'] ) && function_exists( 'classicpress_is_dev_version' ) ) {
+			$develop_src = classicpress_is_dev_version();
 		} else {
 			$develop_src = false;
 		}

src/wp-includes/general-template.php
@@ -3887,7 +3887,7 @@ function register_admin_color_schemes() {
 	);
 
 	// Other color schemes are not available when running out of src
-	if ( false !== strpos( get_bloginfo( 'version' ), '-src' ) ) {
+	if ( classicpress_is_dev_install() ) {
 		return;
 	}
 

src/wp-includes/script-loader.php
@@ -46,9 +46,9 @@ require( ABSPATH . WPINC . '/functions.wp-styles.php' );
  * @param WP_Scripts $scripts WP_Scripts object.
  */
 function wp_default_scripts( &$scripts ) {
-	include( ABSPATH . WPINC . '/version.php' ); // include an unmodified $wp_version
+	include( ABSPATH . WPINC . '/version.php' ); // include an unmodified $cp_version
 
-	$develop_src = false !== strpos( $wp_version, '-src' );
+	$develop_src = classicpress_is_dev_install();
 
 	if ( ! defined( 'SCRIPT_DEBUG' ) ) {
 		define( 'SCRIPT_DEBUG', $develop_src );
@@ -926,11 +926,13 @@ function wp_default_scripts( &$scripts ) {
 function wp_default_styles( &$styles ) {
 	include( ABSPATH . WPINC . '/version.php' ); // include an unmodified $wp_version
 
-	if ( ! defined( 'SCRIPT_DEBUG' ) )
-		define( 'SCRIPT_DEBUG', false !== strpos( $wp_version, '-src' ) );
+	if ( ! defined( 'SCRIPT_DEBUG' ) ) {
+		define( 'SCRIPT_DEBUG', classicpress_is_dev_install() );
+	}
 
-	if ( ! $guessurl = site_url() )
+	if ( ! $guessurl = site_url() ) {
 		$guessurl = wp_guess_url();
+	}
 
 	$styles->base_url = $guessurl;
 	$styles->content_url = defined('WP_CONTENT_URL')? WP_CONTENT_URL : '';

src/wp-includes/version.php
@@ -38,10 +38,26 @@ $cp_version = '1.0.0-alpha0+dev';
  * @return string The ClassicPress version string.
  */
 if ( ! function_exists( 'classicpress_version' ) ) {
-    function classicpress_version() {
-        global $cp_version;
-        return $cp_version;
-    }
+	function classicpress_version() {
+		global $cp_version;
+		return $cp_version;
+	}
+}
+
+/**
+ * Return whether ClassicPress is running as a source install (the result of
+ * cloning the source repository rather than installing a built version).
+ *
+ * This is mostly supported, but there are a few things that need to work
+ * slightly differently or need to be disabled.
+ *
+ * @return bool Whether ClassicPress is running as a source install.
+ */
+if ( ! function_exists( 'classicpress_is_dev_install' ) ) {
+	function classicpress_is_dev_install() {
+		global $cp_version;
+		return substr( $cp_version, -4 ) === '+dev';
+	}
 }
 
 /**

tests/phpunit/includes/bootstrap.php
@@ -12,7 +12,7 @@ if ( class_exists( 'PHPUnit\Runner\Version' ) ) {
 
 $config_file_path = dirname( dirname( __FILE__ ) );
 if ( ! file_exists( $config_file_path . '/wp-tests-config.php' ) ) {
-	// Support the config file from the root of the develop repository.
+	// Support the config file from the root of the source repository.
 	if ( basename( $config_file_path ) === 'phpunit' && basename( dirname( $config_file_path ) ) === 'tests' )
 		$config_file_path = dirname( dirname( $config_file_path ) );
 }