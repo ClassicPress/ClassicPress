ddf4ed2ba12b5e44e3fb5024ff0733d0595af81a
Daniele Scasciafratte <mte90net@gmail.com>
2018-10-25 22:32:16 +0200

    refactoring

--- tools/i18n/makepot.php
+++ tools/i18n/makepot.php
@@ -188,7 +188,7 @@ class MakePOT {
 			'default_output' => 'classicpress.pot',
 			'includes' => array(),
 			'excludes' => array_merge(
-				array( 'wp-content/plugins/*', 'wp-content/themes/*', )
+				array( 'wp-content/plugins/*', 'wp-content/themes/*', 'wp-includes/class-pop3.php' )
 			),
 			'extract_not_gettexted' => false,
 			'not_gettexted_files_filter' => false,
