1d1d941fab7ba4fa6653f123214363aa98064e35
Daniele Scasciafratte <mte90net@gmail.com>
2019-01-16 19:19:04 +0100

    Backport scripts

---
+++ bin/rename-wp-commit.sh
@@ -0,0 +1,16 @@
+#/usr/bin/env bash
+
+# First parameter is the ticket number
+
+# To save the changes
+git commit
+
+# Get the data to print
+commit=`git log wp/master --grep=$1 --oneline --pretty=format:'%h' -n 1`
+OLD_MSG=`git log --format=%B -n1`
+changeset=`echo $OLD_MSG | tail -n1 | cut -d "@" -f2 | cut -d " " -f1`
+message=$(printf "#WP-$1: $OLD_MSG\n\n----\nMerges https:\/\/core.trac.wordpress.org\/changeset\/$changeset \/ WordPress\/wordpress-develop@$commit to ClassicPress.")
+git commit --amend -m"$message"
+
+# Push the branch and get ready for the pull request!
+git push origin merge/wp-r$1
---
+++ bin/wp-merge-to-cp.sh
@@ -0,0 +1,40 @@
+#/usr/bin/env bash
+
+# First parameter is your fork
+# Second parameter is the ticket merged
+
+if [ ! -d "ClassicPress" ]; then
+    # Download ClassicPress from your fork
+    git clone git@github.com:$1/ClassicPress.git
+    cd ClassicPress
+
+    # Download original ClassicPress
+    git remote add cp git@github.com:ClassicPress/ClassicPress.git
+
+    # Download WordPress dev
+    git remote add wp https://github.com/WordPress/wordpress-develop
+else
+    cd ClassicPress
+fi
+git fetch cp
+git fetch wp
+
+# Switch to ClassicPress branch
+git fetch origin
+git checkout develop
+
+# Sync your fork with the original
+git merge cp/develop
+git push origin develop
+git checkout origin/develop -B develop
+
+# Create branch with the patch from WordPress
+git checkout -b merge/wp-r$2
+
+# Get the commit from WP git log
+commit=`git log wp/master --grep=$2 --oneline --pretty=format:'%h' -n 1`
+if [ -z "$var" ]; then
+    git cherry-pick $commit
+else
+    echo "Commit not found"
+fi
