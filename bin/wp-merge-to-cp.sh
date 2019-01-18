89c7bdb782e5cc4e8d80b96ab3d706e262daedbd
Daniele Scasciafratte <mte90net@gmail.com>
2019-01-18 12:41:16 +0100

    bugfix

--- bin/wp-merge-to-cp.sh
+++ bin/wp-merge-to-cp.sh
@@ -7,40 +7,42 @@ find_wp_remote=$(git remote -v | grep WordPress/wordpress-develop | tail -n1 | a
 if [ -z "$find_wp_remote" ]; then
     echo "Adding WP remote"
     # Download WordPress dev
-    git remote add wp https://github.com/WordPress/wordpress-develop > /dev/null
+    git remote add wp https://github.com/WordPress/wordpress-develop > /dev/null 2>&1
     find_wp_remote='wp'
 fi
 find_cp_remote=$(git remote -v | grep ClassicPress/ClassicPress | tail -n1 | awk '{print $1;}')
 if [ -z "$find_cp_remote" ]; then
     echo "Adding CP remote"
     # Download WordPress dev
-    git remote add cp git@github.com:ClassicPress/ClassicPress.git > /dev/null
-    find_cp_remote='wp'
+    git remote add cp git@github.com:ClassicPress/ClassicPress.git > /dev/null 2>&1
+    find_cp_remote='cp'
 fi
-git fetch "$find_cp_remote"
+echo "Fetc CP/WP git repository"
+git fetch "$find_wp_remote" > /dev/null 2>&1
+git fetch "$find_cp_remote" > /dev/null 2>&1
 
 # Switch to ClassicPress branch
 echo "Sync your local repo"
-git fetch origin > /dev/null
+git fetch origin > /dev/null 2>&1
 echo "Switch to develop branch"
-git checkout develop > /dev/null
+git checkout develop > /dev/null 2>&1
 
 # Sync your fork with the original
 echo "Merge remote CP to your fork"
-git merge "$find_cp_remote"/develop > /dev/null
-git push origin develop > /dev/null
-git checkout origin/develop -B develop > /dev/null
+git merge "$find_cp_remote"/develop > /dev/null 2>&1
+git push origin develop > /dev/null 2>&1
+git checkout origin/develop -B develop > /dev/null 2>&1
 
 branch="merge/wp-r$1"
 # If branch already exist, remove so the process start from a clean status
 if [ ! -z $(git branch --list "$branch") ]; then
     echo "Remove branch for this changeset because exists"
-	git checkout -D "$branch" > /dev/null
+	git branch -D "$branch" > /dev/null 2>&1
 fi
 
 # Create branch with the changeset from WordPress
 echo "Create branch for changeset $1"
-git checkout -b "$branch" > /dev/null
+git checkout -b "$branch" > /dev/null 2>&1
 
 # Get the commit from WP git log
 commit=$(git log "$find_wp_remote"/"$2" --grep="^git-svn-id: https://develop.svn.wordpress.org/(trunk|\\d\\.\\d)@$1" --oneline --pretty=format:'%h' -n 1)
