1fc90730f0c53b9761cc4493b3f2e6b03512728c
Noifemey <31583081+Noi1279@users.noreply.github.com>
2019-01-28 03:26:29 +0800

    Improve cache-busting logic for child themes (#346)
    Fixes #331.

--- src/wp-content/themes/classicpress-twentyseventeen/functions.php
+++ src/wp-content/themes/classicpress-twentyseventeen/functions.php
@@ -9,27 +9,22 @@
  * @since 1.0.0
  */
 
-
-add_action( 'after_setup_theme', 'cp2017_setup' );
-
 function cp2017_setup() {
-
 	load_theme_textdomain( 'classicpress-twentyseventeen' );
-
 }
+add_action( 'after_setup_theme', 'cp2017_setup' );
 
 // Enqueue parent/child themes styles with cachebusting for child theme styles built in
 add_action( 'wp_enqueue_scripts', 'cp2017_enqueue_styles' );
 
 function cp2017_enqueue_styles() {
+	$parent_style = 'twentyseventeen-style';
+	wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
 
-    $parent_style = 'twentyseventeen-style';
-
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'classicpress-twentyseventeen',
-        get_stylesheet_directory_uri() . '/style.css',
-        array( $parent_style ),
-        filemtime( get_stylesheet_directory() . '/style.css' )
-    );
-
+	wp_enqueue_style(
+		'classicpress-twentyseventeen',
+		get_stylesheet_directory_uri() . '/style.css',
+		array( $parent_style ),
+		classicpress_asset_version( 'style', 'classicpress-twentyseventeen' )
+	);
 }
--- src/wp-content/themes/classicpress-twentysixteen/functions.php
+++ src/wp-content/themes/classicpress-twentysixteen/functions.php
@@ -13,23 +13,20 @@
 add_action( 'after_setup_theme', 'cp2016_setup' );
 
 function cp2016_setup() {
-
 	load_theme_textdomain( 'classicpress-twentysixteen' );
-
 }
 
 // Enqueue parent/child themes styles with cachebusting for child theme styles built in
 add_action( 'wp_enqueue_scripts', 'cp2016_enqueue_styles' );
 
 function cp2016_enqueue_styles() {
-
-    $parent_style = 'twentysixteen-style';
-
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'classicpress-twentysixteen',
-        get_stylesheet_directory_uri() . '/style.css',
-        array( $parent_style ),
-        filemtime( get_stylesheet_directory() . '/style.css' )
-    );
-
+	$parent_style = 'twentysixteen-style';
+	wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+	
+	wp_enqueue_style(
+		'classicpress-twentysixteen',
+		get_stylesheet_directory_uri() . '/style.css',
+		array( $parent_style ),
+		classicpress_asset_version( 'style', 'classicpress-twentysixteen' )
+	);
 }
