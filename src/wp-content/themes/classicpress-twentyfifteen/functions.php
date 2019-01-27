c0956647ff48d2ad345379d823064b109671feae
Noifemey <31583081+Noi1279@users.noreply.github.com>
2019-01-27 13:26:54 +0800

    Update functions.php

--- src/wp-content/themes/classicpress-twentyfifteen/functions.php
+++ src/wp-content/themes/classicpress-twentyfifteen/functions.php
@@ -27,7 +27,7 @@ function cp2015_enqueue_styles() {
 
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
 	
-    $version = classicpress_asset_version( 'style', 'classicpress-twentyseventeen' );
+    $version = classicpress_asset_version( 'style', 'classicpress-twentyfifteen' );
     wp_enqueue_style( 'classicpress-twentyfifteen',
         get_stylesheet_directory_uri() . '/style.css',
         array( $parent_style ),
