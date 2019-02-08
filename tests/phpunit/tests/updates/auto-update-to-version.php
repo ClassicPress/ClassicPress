f32728f5278e6d63bc64e4b9718e8f1cf22219b5
James Nylen <jnylen@gmail.com>
2019-02-07 23:52:34 -0500

    Start adding tests
    Not all passing yet!

---
+++ tests/phpunit/tests/updates/auto-update-to-version.php
@@ -0,0 +1,134 @@
+<?php
+/**
+ * Test the logic for whether an automatic update should be applied between two versions.
+ *
+ * @package ClassicPress
+ * @subpackage UnitTests
+ * @since 1.0.0-rc1
+ */
+
+require_once ABSPATH . 'wp-admin/includes/class-wp-upgrader.php';
+require_once ABSPATH . 'wp-admin/includes/class-core-upgrader.php';
+
+/**
+ * Test the logic for whether automatic updates should be enabled.
+ */
+class Tests_Auto_Update_To_Version extends WP_UnitTestCase {
+	public function test_auto_update_same() {
+		$this->assertFalse( Core_Upgrader::auto_update_enabled_for_versions(
+			'1.0.0', '1.0.0', true
+		) );
+
+		$this->assertFalse( Core_Upgrader::auto_update_enabled_for_versions(
+			'1.0.0-beta1', '1.0.0-beta1', true
+		) );
+
+		$this->assertFalse( Core_Upgrader::auto_update_enabled_for_versions(
+			'1.0.0-beta2+nightly.20190206', '1.0.0-beta2+nightly.20190206', true
+		) );
+	}
+
+	public function test_auto_update_basic() {
+		$this->assertTrue( Core_Upgrader::auto_update_enabled_for_versions(
+			'1.0.0', '1.0.1', true
+		) );
+
+		$this->assertTrue( Core_Upgrader::auto_update_enabled_for_versions(
+			'1.0.1', '1.1.0', true
+		) );
+	}
+
+	public function test_auto_update_newer_nightly() {
+		$this->assertTrue( Core_Upgrader::auto_update_enabled_for_versions(
+			'1.0.0-beta1+nightly.20181216', '1.0.0-beta1+nightly.20181217', true
+		) );
+
+		$this->assertTrue( Core_Upgrader::auto_update_enabled_for_versions(
+			'1.0.0-beta1+nightly.20181217', '1.0.0-beta2+nightly.20181218', true
+		) );
+	}
+
+	public function test_auto_update_older_nightly() {
+		$this->assertFalse( Core_Upgrader::auto_update_enabled_for_versions(
+			'1.0.0-beta1+nightly.20181217', '1.0.0-beta1+nightly.20181216', true
+		) );
+
+		$this->assertFalse( Core_Upgrader::auto_update_enabled_for_versions(
+			'1.0.0-beta2+nightly.20181218', '1.0.0-beta1+nightly.20181217', true
+		) );
+	}
+
+	public function test_auto_updates_disabled() {
+		$this->assertFalse( Core_Upgrader::auto_update_enabled_for_versions(
+			'1.0.0', '1.0.1', false
+		) );
+
+		$this->assertFalse( Core_Upgrader::auto_update_enabled_for_versions(
+			'1.0.1', '1.1.0', false
+		) );
+	}
+
+	public function test_auto_update_to_older() {
+		$this->assertFalse( Core_Upgrader::auto_update_enabled_for_versions(
+			'1.0.1', '1.0.0', true
+		) );
+
+		$this->assertFalse( Core_Upgrader::auto_update_enabled_for_versions(
+			'1.1.0', '1.0.1', true
+		) );
+
+		$this->assertFalse( Core_Upgrader::auto_update_enabled_for_versions(
+			'1.0.0', '1.0.0-beta2', true
+		) );
+	}
+
+	public function test_auto_update_between_prereleases_of_same_release() {
+		$this->assertFalse( Core_Upgrader::auto_update_enabled_for_versions(
+			'1.0.0-alpha1', '1.0.0-alpha2', true
+		) );
+
+		$this->assertFalse( Core_Upgrader::auto_update_enabled_for_versions(
+			'1.0.0-alpha2', '1.0.0-beta1', true
+		) );
+
+		$this->assertFalse( Core_Upgrader::auto_update_enabled_for_versions(
+			'1.0.0-beta1', '1.0.0-beta2', true
+		) );
+
+		$this->assertFalse( Core_Upgrader::auto_update_enabled_for_versions(
+			'1.0.0-beta2', '1.0.0-rc1', true
+		) );
+
+		$this->assertFalse( Core_Upgrader::auto_update_enabled_for_versions(
+			'1.0.0-beta2', '1.0.0-rc2', true
+		) );
+	}
+
+	public function test_auto_update_between_prereleases_of_different_releases() {
+		$this->assertFalse( Core_Upgrader::auto_update_enabled_for_versions(
+			'1.0.0-beta1', '1.0.1', true
+		) );
+
+		$this->assertFalse( Core_Upgrader::auto_update_enabled_for_versions(
+			'1.0.0-beta1', '1.0.1-beta1', true
+		) );
+
+		$this->assertFalse( Core_Upgrader::auto_update_enabled_for_versions(
+			'1.0.0-rc', '1.0.1-beta1', true
+		) );
+
+		$this->assertFalse( Core_Upgrader::auto_update_enabled_for_versions(
+			'1.0.0-beta1', '1.0.1-rc1', true
+		) );
+	}
+
+	public function test_auto_update_prerelease_to_final() {
+		$this->assertFalse( Core_Upgrader::auto_update_enabled_for_versions(
+			'1.0.0-beta1', '1.0.0', true
+		) );
+
+		$this->assertFalse( Core_Upgrader::auto_update_enabled_for_versions(
+			'1.0.0-beta1', '1.0.1', true
+		) );
+	}
+}
