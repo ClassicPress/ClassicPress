eb17d2c945028cac45a240d81ad1839d50965850
invisnet <classicpress@invis.net>
2019-02-19 18:58:11 +0000

    Update with correct link

--- src/wp-admin/security.php
+++ src/wp-admin/security.php
@@ -22,7 +22,7 @@ require_once( ABSPATH . 'wp-admin/admin-header.php' );
     <h2><?php _e( '"Security First"' ); ?></h2>
     <p><?php _e( "Security is important to business and it’s important to us here at ClassicPress. By bringing security forward to a place of greater prominence within the admin interface, we create a more streamlined experience for both users and developers." ); ?></p>
     <p><?php _e( "As ClassicPress continues to evolve, the Security page will become the hub for all security features for ClassicPress core and 3<sup>rd</sup> party plugins that choose to support it." ); ?></p>
-    <p><?php _e( "Watch this page and the ClassicPress Security forum for more news as development continues. If you’d like to contribute in this area, please connect with us <a href=\"#\">here</a>." ); ?></p>
+    <p><?php _e( "Watch this page and the ClassicPress Security forum for more news as development continues. If you’d like to contribute in this area, please connect with us <a href=\"https://forums.classicpress.net/security\" target=\"_blank\">here</a>." ); ?></p>
   </div>
 </div>
 <?php
