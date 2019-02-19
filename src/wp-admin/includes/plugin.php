35a6e06f904bf8f3784c52552f6fac191925f2a2
invisnet <classicpress@invis.net>
2019-02-19 15:03:21 +0000

    Add helper function

--- src/wp-admin/includes/plugin.php
+++ src/wp-admin/includes/plugin.php
@@ -1415,6 +1415,26 @@ function add_comments_page( $page_title, $menu_title, $capability, $menu_slug, $
 }
 
 /**
+ * Add submenu page to the Security main menu.
+ *
+ * This function takes a capability which will be used to determine whether
+ * or not a page is included in the menu.
+ *
+ * The function which is hooked in to handle the output of the page must check
+ * that the user has the required capability as well.
+ *
+ * @param string   $page_title The text to be displayed in the title tags of the page when the menu is selected.
+ * @param string   $menu_title The text to be used for the menu.
+ * @param string   $capability The capability required for this menu to be displayed to the user.
+ * @param string   $menu_slug  The slug name to refer to this menu by (should be unique for this menu).
+ * @param callable $function   The function to be called to output the content for this page.
+ * @return false|string The resulting page's hook_suffix, or false if the user does not have the capability required.
+ */
+function add_security_page( $page_title, $menu_title, $capability, $menu_slug, $function = '' ) {
+	return add_submenu_page( 'security.php', $page_title, $menu_title, $capability, $menu_slug, $function );
+}
+
+/**
  * Remove a top-level admin menu.
  *
  * @since WP-3.1.0
