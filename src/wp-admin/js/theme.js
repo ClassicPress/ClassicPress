7d27962a9b2c64573bce8b8754e67ca681ef36ed
James Nylen <jnylen@gmail.com>
2019-02-26 22:56:06 -0500

    Remove WP-only features from the theme installation section
    Follow-up to #342.

--- src/wp-admin/js/theme.js
+++ src/wp-admin/js/theme.js
@@ -1916,7 +1916,7 @@ themes.RunInstaller = {
 		// Set up the view
 		// Passes the default 'section' as an option
 		this.view = new themes.view.Installer({
-			section: 'featured',
+			section: 'popular',
 			SearchView: themes.view.InstallerSearch
 		});
 
@@ -1982,11 +1982,11 @@ themes.RunInstaller = {
 
 		// Handles sorting / browsing routes
 		// Also handles the root URL triggering a sort request
-		// for `featured`, the default view
+		// for `popular`, the default view
 		themes.router.on( 'route:sort', function( sort ) {
 			if ( ! sort ) {
-				sort = 'featured';
-				themes.router.navigate( themes.router.baseUrl( '?browse=featured' ), { replace: true } );
+				sort = 'popular';
+				themes.router.navigate( themes.router.baseUrl( '?browse=popular' ), { replace: true } );
 			}
 			self.view.sort( sort );
 
