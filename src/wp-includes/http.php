831d2d1b1e7c14c0d4e09e6fa37b523118b8202c
James Nylen <jnylen@gmail.com>
2019-03-05 01:58:43 -0500

    Remove pre-release version from '@ deprecated' tag

--- src/wp-includes/http.php
+++ src/wp-includes/http.php
@@ -357,7 +357,7 @@ function wp_remote_retrieve_cookie_value( $response, $name ) {
  * Determines if there is an HTTP Transport that can process this request.
  *
  * @since WP-3.2.0
- * @deprecated 1.0.0-beta1
+ * @deprecated 1.0.0
  *
  * @param array  $capabilities Array of capabilities to test or a wp_remote_request() $args array.
  * @param string $url          Optional. If given, will check if the URL requires SSL and adds
