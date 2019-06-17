d3f54ecc787448498f8130dcd5095dc21d56ca3d
Gary Pendergast <pento@git.wordpress.org>
2019-06-16 20:08:11 -0500

    WP-r44556: Emoji: Fix a code formatting error introduced in https://core.trac.wordpress.org/changeset/44554.
    See https://core.trac.wordpress.org/ticket/45930.
    
    ----
    Merges https://core.trac.wordpress.org/changeset/44556 / WordPress/wordpress-develop@49f5f62f30 to ClassicPress.

--- src/wp-includes/formatting.php
+++ src/wp-includes/formatting.php
@@ -5119,7 +5119,7 @@ function _print_emoji_detection_script() {
  * @return string The encoded content.
  */
 function wp_encode_emoji( $content ) {
-	$emoji = _wp_emoji_list( 'partials' );
+	$emoji  = _wp_emoji_list( 'partials' );
 	$compat = version_compare( phpversion(), '5.4', '<' );
 
 	foreach ( $emoji as $emojum ) {
