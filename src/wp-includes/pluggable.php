c963e3a8a4f9f5a06f8494386a7cdd24d42806c7
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2019-09-14 20:13:37 +0000

    WP-r46115: Mail: Avoid setting duplicate `MIME-Version` and `X-Mailer` headers in `wp_mail()`, they are added automatically by PHPMailer.
    WP:Props lbenicio, junktrunk, danieltj, studyboi, bennemann.
    Fixes https://core.trac.wordpress.org/ticket/43542.
    
    Conflicts:
      src/wp-includes/pluggable.php
    ----
    Merges https://core.trac.wordpress.org/changeset/46115 / WordPress/wordpress-develop@848a329478 to ClassicPress.

--- src/wp-includes/pluggable.php
+++ src/wp-includes/pluggable.php
@@ -447,10 +447,13 @@ function wp_mail( $to, $subject, $message, $headers = '', $attachments = array()
 	 */
 	$phpmailer->CharSet = apply_filters( 'wp_mail_charset', $charset );
 
-	// Set custom headers
-	if ( !empty( $headers ) ) {
+	// Set custom headers.
+	if ( ! empty( $headers ) ) {
 		foreach ( (array) $headers as $name => $content ) {
-			$phpmailer->addCustomHeader( sprintf( '%1$s: %2$s', $name, $content ) );
+			// Only add custom headers not added automatically by PHPMailer.
+			if ( ! in_array( $name, array( 'MIME-Version', 'X-Mailer') ) ) {
+				$phpmailer->addCustomHeader( sprintf( '%1$s: %2$s', $name, $content ) );
+			}
 		}
 
 		if ( false !== stripos( $content_type, 'multipart' ) && ! empty($boundary) )
