e29a2b378f9f78c5288379987335bd6de77a9a8e
James Nylen <jnylen@gmail.com>
2019-09-18 01:01:33 -0500

    Back out unwanted changes to general-template.php

--- src/wp-includes/general-template.php
+++ src/wp-includes/general-template.php
@@ -2903,7 +2903,7 @@ function wp_resource_hints() {
 	 * The path is removed in the foreach loop below.
 	 */
 	/** This filter is documented in wp-includes/formatting.php */
-	$hints['dns-prefetch'][] = apply_filters( 'emoji_svg_url', 'https://twemoji.classicpress.net/11/svg/' );
+	$hints['dns-prefetch'][] = apply_filters( 'emoji_svg_url', 'https://twemoji.classicpress.net/12/svg/' );
 
 	foreach ( $hints as $relation_type => $urls ) {
 		$unique_urls = array();
