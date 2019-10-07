afeee50c5f1db0a55caf914e22e2f814b822a627
James Nylen <jnylen@gmail.com>
2019-10-07 18:25:26 +0000

    Switch to dedicated API endpoint

--- tools/build/emoji.js
+++ tools/build/emoji.js
@@ -11,40 +11,18 @@ exports.setGruntReference = _grunt => {
 	grunt = _grunt;
 };
 
-function callGitHubAPI( url ) {
+// See https://github.com/ClassicPress/ClassicPress-APIs/tree/master/twemoji
+function callTwemojiFilesAPI( url ) {
 	// Grunt error handling is bad
 	try {
-
 		grunt.log.writeln( 'GET ' + url );
-		const res = request(
-			'GET',
-			url,
-			{
-				headers: { 'User-Agent': 'Request' },
-				json: true
-			}
-		);
+		const res = request( 'GET', url, { json: true } );
 
 		const body = res.getBody( 'UTF-8' );
 		const json = JSON.parse( body );
 
-		if ( true === json.truncated ) {
-			grunt.fatal(
-				'Emojis not built due to truncated response from: '
-				+ url
-			);
-		}
-
 		return json;
-
 	} catch ( e ) {
-		if ( /rate limit exceeded/.test( e.message ) ) {
-			const rateLimits = callGitHubAPI( 'https://api.github.com/rate_limit' );
-			grunt.log.writeln(
-				'GitHub API rate limit resets at: '.yellow
-				+ new Date( rateLimits.rate.reset * 1000 )
-			);
-		}
 		grunt.fatal( e.message );
 	}
 }
@@ -54,23 +32,11 @@ exports.replaceEmojiRegex = () => {
 
 	// Fetch a list of the files that Twemoji supplies
 
-	const urlMaster = 'https://api.github.com/repos/twitter/twemoji/commits/6f3545b9';
-	let res = callGitHubAPI( urlMaster );
-	res = callGitHubAPI( res.commit.tree.url );
-
-	let node;
-
-	[ '2', 'svg' ].forEach( dir => {
-		node = res.tree.find( node => node.path === dir );
-		if ( ! node ) {
-			grunt.fatal( "Directory '" + dir + "' not found" );
-		}
-		node = callGitHubAPI( node.url );
-	} );
+	const entityNames = callTwemojiFilesAPI(
+		'https://api-v1.classicpress.net/twemoji/6f3545b9_2_svg.json'
+	);
 
 	// Convert the list of emoji names into PHP code
-
-	const entityNames = node.tree.map( e => e.path );
 
 	let entities = entityNames.join( '\n' );
 
