fd9e5a62cd16a3111ebf69aa8f22174e4da7922d
James Nylen <jnylen@gmail.com>
2019-10-07 05:33:04 +0000

    Readability and error handling improvements

--- tools/build/emoji.js
+++ tools/build/emoji.js
@@ -2,6 +2,7 @@
 /* jshint es3:false */
 /* jshint esversion:6 */
 
+// The grunt tasks we are using do not support async replacement.
 const request = require( 'sync-request' );
 
 let grunt = null;
@@ -11,105 +12,103 @@ exports.setGruntReference = _grunt => {
 };
 
 function callGitHubAPI( url ) {
-	var res = request(
-		'GET',
-		url,
-		{
-			headers: { 'User-Agent': 'Request' },
-			json: true
+	// Grunt error handling is bad
+	try {
+
+		grunt.log.writeln( 'GET ' + url );
+		const res = request(
+			'GET',
+			url,
+			{
+				headers: { 'User-Agent': 'Request' },
+				json: true
+			}
+		);
+
+		const body = res.getBody( 'UTF-8' );
+		const json = JSON.parse( body );
+
+		if ( true === json.truncated ) {
+			grunt.fatal(
+				'Emojis not built due to truncated response from: '
+				+ url
+			);
 		}
-	);
 
-	if ( res.statusCode >= 300 ) {
-		grunt.fatal( 'Unable to fetch Twemoji file resource at:' + url );
-	}
-
-	var json = JSON.parse( res.getBody( 'UTF-8' ) );
-	if ( 'message' in json ) {
-		grunt.fatal( 'API rate limit exceeded, try again later.' );
-	} else if ( true === json.truncated ) {
-		grunt.fatal( 'Emojis not built due to truncated response from: ' + url );
-	} else {
 		return json;
+
+	} catch ( e ) {
+		if ( /rate limit exceeded/.test( e.message ) ) {
+			const rateLimits = callGitHubAPI( 'https://api.github.com/rate_limit' );
+			grunt.log.writeln(
+				'GitHub API rate limit resets at: '.yellow
+				+ new Date( rateLimits.rate.reset * 1000 )
+			);
+		}
+		grunt.fatal( e.message );
 	}
 }
 
-function createEmojiArray( emoji ) {
-	let entityNames = [];
-	let entities;
-	let emojiArray;
-	let partials;
-	let partialsSet;
-	let regex;
+exports.replaceEmojiRegex = () => {
+	grunt.log.writeln( 'Fetching list of Twemoji files...' );
 
-	for ( var k = 0; k < emoji.tree.length; k++ ) {
-		entityNames.push( emoji.tree[k].path );
-	}
+	// Fetch a list of the files that Twemoji supplies
 
-	entities = entityNames.join( '\n' );
+	const urlMaster = 'https://api.github.com/repos/twitter/twemoji/commits/6f3545b9';
+	let res = callGitHubAPI( urlMaster );
+	res = callGitHubAPI( res.commit.tree.url );
+
+	let node;
+
+	[ '2', 'svg' ].forEach( dir => {
+		node = res.tree.find( node => node.path === dir );
+		if ( ! node ) {
+			grunt.fatal( "Directory '" + dir + "' not found" );
+		}
+		node = callGitHubAPI( node.url );
+	} );
+
+	// Convert the list of emoji names into PHP code
+
+	const entityNames = node.tree.map( e => e.path );
+
+	let entities = entityNames.join( '\n' );
 
 	// Tidy up the file list
 	entities = entities.replace( /\.svg/g, '' );
 	entities = entities.replace( /^$/g, '' );
 
 	// Convert the emoji entities to HTML entities
-	partials = entities = entities.replace( /([a-z0-9]+)/g, '&#x$1;' );
+	let partials = entities = entities.replace( /([a-z0-9]+)/g, '&#x$1;' );
 
 	// Remove the hyphens between the HTML entities
 	entities = entities.replace( /-/g, '' );
 
-	// Sort the entities list by length, so the longest emoji will be found first
-	emojiArray = entities.split( '\n' ).sort( ( a, b ) => {
+	// Sort the entities by length, so the longest emoji will be found first
+	const emojiArray = entities.split( '\n' ).sort( ( a, b ) => {
 		return b.length - a.length;
 	} );
 
 	// Convert the entities list to PHP array syntax
-	entities = `'${emojiArray.filter( val => val.length >= 8 ? val : false ).join( '\', \'' )}'`;
+	entities = emojiArray
+		.filter( val => val.length >= 8 )
+		.join( '\', \'' );
 
 	// Create a list of all characters used by the emoji list
 	partials = partials.replace( /-/g, ',' );
 
 	// Set automatically removes duplicates
-	partialsSet = new Set( partials.split( '\n' ) );
+	const partialsSet = new Set( partials.split( '\n' ) );
 
 	// Convert the partials list to PHP array syntax
-	partials = `'${Array.from( partialsSet ).filter( val => val.length >= 8 ? val : false ).join( '\', \'' )}'`;
+	partials = Array.from( partialsSet )
+		.filter( val => val.length >= 8 )
+		.join( '\', \'' );
 
-	regex = '// START: emoji arrays\n';
-	regex += `\t$entities = array( ${entities} );\n`;
-	regex += `\t$partials = array( ${partials} );\n`;
-	regex += '\t// END: emoji arrays';
+	let replacement = '// START: emoji arrays\n';
+	replacement += `\t$entities = array( ${entities} );\n`;
+	replacement += `\t$partials = array( ${partials} );\n`;
+	replacement += '\t// END: emoji arrays';
 
-	return regex;
-}
-
-exports.replaceEmojiRegex = () => {
-	let res;
-	let master;
-	let twoUrl;
-	let svgUrl;
-
-	grunt.log.writeln( 'Fetching list of Twemoji files...' );
-
-	// Fetch a list of the files that Twemoji supplies
-	master = 'https://api.github.com/repos/twitter/twemoji/commits/6f3545b9';
-	res = callGitHubAPI( master );
-
-	res = callGitHubAPI( res.commit.tree.url );
-	for ( var i = 0; i < res.tree.length; i++ ) {
-		if ( '2' === res.tree[i].path ) {
-			twoUrl = res.tree[i].url;
-		}
-	}
-
-	res = callGitHubAPI( twoUrl );
-	for ( var j = 0; j < res.tree.length; j++ ) {
-		if ( 'svg' === res.tree[j].path ) {
-			svgUrl = res.tree[j].url;
-		}
-	}
-
-	res = callGitHubAPI( svgUrl );
-
-	return createEmojiArray( res );
+	return replacement;
 };
