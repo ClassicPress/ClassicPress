9392e18b9d7d78d44f3309d052606f4778ab7f06
Laurence Bahiirwa <laurence@mediauganda.com>
2020-02-07 21:25:17 +0300

    Clean merge conflicts

--- src/wp-admin/includes/class-wp-ajax-upgrader-skin.php
+++ src/wp-admin/includes/class-wp-ajax-upgrader-skin.php
@@ -95,11 +95,8 @@ class WP_Ajax_Upgrader_Skin extends Automatic_Upgrader_Skin {
 
 			// Count existing errors to generate a unique error code.
 			$errors_count = count( $this->errors->get_error_codes() );
-<<<<<<< HEAD
-			$this->errors->add( 'unknown_upgrade_error_' . $errors_count + 1 , $string );
-=======
+
 			$this->errors->add( 'unknown_upgrade_error_' . ( $errors_count + 1 ), $string );
->>>>>>> 9172b9c16b... Upgrade/Install: Correct operator precedence for an error code in `WP_Ajax_Upgrader_Skin::error()`.
 		} elseif ( is_wp_error( $errors ) ) {
 			foreach ( $errors->get_error_codes() as $error_code ) {
 				$this->errors->add( $error_code, $errors->get_error_message( $error_code ), $errors->get_error_data( $error_code ) );
