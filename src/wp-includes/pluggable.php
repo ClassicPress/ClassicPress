534551bb0fa0539c3fa434be5b6ac1a84b881692
mattyrob <mattyrobuk@gmail.com>
2020-02-17 03:49:03 +0000

    WP-r47086: Mail: Remove angle brackets from password reset URL in emails (#536)
    See #467. Follow-up to #468 to sync with WP now that they've adopted this change.
    
    Merges https://core.trac.wordpress.org/changeset/47086 / WordPress/wordpress-develop@480b5c88c3 to ClassicPress.
    
    Co-authored-by: Sergey Biryukov <sergeybiryukov.ru@gmail.com>

--- src/wp-includes/pluggable.php
+++ src/wp-includes/pluggable.php
@@ -1904,7 +1904,7 @@ function wp_new_user_notification( $user_id, $deprecated = null, $notify = '' ) 
 	/* translators: %s: user login */
 	$message = sprintf(__('Username: %s'), $user->user_login) . "\r\n\r\n";
 	$message .= __('To set your password, visit the following address:') . "\r\n\r\n";
-	$message .= '<' . network_site_url("wp-login.php?action=rp&key=$key&login=" . rawurlencode($user->user_login), 'login') . ">\r\n\r\n";
+	$message .= network_site_url("wp-login.php?action=rp&key=$key&login=" . rawurlencode($user->user_login), 'login') . "\r\n\r\n";
 
 	$message .= wp_login_url() . "\r\n";
 
