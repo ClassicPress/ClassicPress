a59c09a5241e022e1aafb96f28d118e5b352ea77
James Nylen <jnylen@gmail.com>
2020-02-18 20:02:09 -0600

    Update the tests for recommended PHP and MySQL versions
    - Split this into 2 tests
    - Reformat

--- tests/phpunit/tests/external-http/basic.php
+++ tests/phpunit/tests/external-http/basic.php
@@ -4,37 +4,73 @@
  */
 class Tests_External_HTTP_Basic extends WP_UnitTestCase {
 
-	function test_readme() {
+	function test_readme_php() {
 		// This test is designed to only run on trunk/master
 		$this->skipOnAutomatedBranches();
 
 		$readme = file_get_contents( ABSPATH . 'readme.html' );
 
-		preg_match( '#Recommendations.*PHP</a> version <strong>([0-9.]*)#s', $readme, $matches );
+		preg_match(
+			'#Recommendations.*PHP</a> version <strong>([0-9.]*)#s',
+			$readme,
+			$matches
+		);
 
-		$response = wp_remote_get( 'https://secure.php.net/supported-versions.php' );
-		if ( 200 != wp_remote_retrieve_response_code( $response ) ) {
+		$response = wp_remote_get(
+			'https://secure.php.net/supported-versions.php'
+		);
+		if ( 200 !== wp_remote_retrieve_response_code( $response ) ) {
 			$this->fail( 'Could not contact PHP.net to check versions.' );
 		}
 		$php = wp_remote_retrieve_body( $response );
 
-		preg_match_all( '#<tr class="stable">\s*<td>\s*<a [^>]*>\s*([0-9.]*)#s', $php, $phpmatches );
+		preg_match_all(
+			'#<tr class="stable">\s*<td>\s*<a [^>]*>\s*([0-9.]*)#s',
+			$php,
+			$phpmatches
+		);
 
-		$this->assertContains( $matches[1], $phpmatches[1], "readme.html's Recommended PHP version is too old. Remember to update the WordPress.org Requirements page, too." );
+		$this->assertContains(
+			$matches[1],
+			$phpmatches[1],
+			"readme.html's Recommended PHP version is too old."
+		);
+	}
 
-		preg_match( '#Recommendations.*MySQL</a> version <strong>([0-9.]*)#s', $readme, $matches );
+	function test_readme_mysql() {
+		// This test is designed to only run on trunk/master
+		$this->skipOnAutomatedBranches();
 
-		$response = wp_remote_get( "https://dev.mysql.com/doc/relnotes/mysql/{$matches[1]}/en/" );
-		if ( 200 != wp_remote_retrieve_response_code( $response ) ) {
-			$this->fail( 'Could not contact dev.MySQL.com to check versions.' );
+		$readme = file_get_contents( ABSPATH . 'readme.html' );
+
+		preg_match(
+			'#Recommendations.*MySQL</a> version <strong>([0-9.]*)#s',
+			$readme,
+			$matches
+		);
+
+		$response = wp_remote_get(
+			"https://dev.mysql.com/doc/relnotes/mysql/{$matches[1]}/en/"
+		);
+		if ( 200 !== wp_remote_retrieve_response_code( $response ) ) {
+			$this->fail( 'Could not contact dev.mysql.com to check versions.' );
 		}
 		$mysql = wp_remote_retrieve_body( $response );
 
-		preg_match( '#(\d{4}-\d{2}-\d{2}), General Availability#', $mysql, $mysqlmatches );
+		preg_match(
+			'#(\d{4}-\d{2}-\d{2}), General Availability#',
+			$mysql,
+			$mysqlmatches
+		);
 
-		// Per https://www.mysql.com/support/, Oracle actively supports MySQL releases for 5 years from GA release
+		// Per https://www.mysql.com/support/, Oracle actively supports MySQL
+		// releases for 5 years from GA release
 		$mysql_eol = strtotime( $mysqlmatches[1] . ' +5 years' );
 
-		$this->assertLessThan( $mysql_eol, time(), "readme.html's Recommended MySQL version is too old. Remember to update the WordPress.org Requirements page, too." );
+		$this->assertLessThan(
+			$mysql_eol,
+			time(),
+			"readme.html's Recommended MySQL version is too old."
+		);
 	}
 }
