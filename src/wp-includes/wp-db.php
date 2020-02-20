0bf641bab21c162cc569db29dbd76e981b9aa831
James Nylen <jnylen@gmail.com>
2020-02-19 19:40:49 -0600

    Resolve conflicts

--- src/wp-includes/wp-db.php
+++ src/wp-includes/wp-db.php
@@ -528,10 +528,6 @@ class wpdb {
 	 * @since WP-3.9.0
 	 * @var array
 	 */
-<<<<<<< HEAD
-	protected $incompatible_modes = array( 'NO_ZERO_DATE', 'ONLY_FULL_GROUP_BY',
-		'STRICT_TRANS_TABLES', 'STRICT_ALL_TABLES', 'TRADITIONAL' );
-=======
 	protected $incompatible_modes = array(
 		'NO_ZERO_DATE',
 		'ONLY_FULL_GROUP_BY',
@@ -540,7 +536,6 @@ class wpdb {
 		'TRADITIONAL',
 		'ANSI',
 	);
->>>>>>> b26b0ccf8c... Database: Add `ANSI` to the list of incompatible SQL modes.
 
 	/**
 	 * Whether to use mysqli over mysql.
