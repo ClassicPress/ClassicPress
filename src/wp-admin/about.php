308f81f2eb68361302391558dd5d94c7dc576686
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2020-04-29 18:35:19 +0000

    WP-r47700: Update the About page for WordPress 4.9.14
    Conflicts:
      src/wp-admin/about.php
    ----
    Merges https://core.trac.wordpress.org/changeset/47700 / WordPress/wordpress-develop@15e1189eac to ClassicPress.

--- src/wp-admin/about.php
+++ src/wp-admin/about.php
@@ -126,6 +126,26 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 				printf(
 					/* translators: %s: WordPress version number */
 					__( '<strong>WordPress version %s</strong> addressed some security issues.' ),
+					'4.9.14'
+				);
+				?>
+				<?php
+				printf(
+					/* translators: %s: HelpHub URL */
+					__( 'For more information, see <a href="%s">the release notes</a>.' ),
+					sprintf(
+						/* translators: %s: WordPress version */
+						esc_url( __( 'https://wordpress.org/support/wordpress-version/version-%s/' ) ),
+						sanitize_title( '4.9.14' )
+					)
+				);
+				?>
+			</p>
+			<p>
+				<?php
+				printf(
+					/* translators: %s: WordPress version number */
+					__( '<strong>WordPress version %s</strong> addressed some security issues.' ),
 					'4.9.13'
 				);
 				?>
@@ -134,7 +154,7 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 					/* translators: %s: HelpHub URL */
 					__( 'For more information, see <a href="%s">the release notes</a>.' ),
 					sprintf(
-					/* translators: %s: WordPress version */
+						/* translators: %s: WordPress version */
 						esc_url( __( 'https://wordpress.org/support/wordpress-version/version-%s/' ) ),
 						sanitize_title( '4.9.13' )
 					)
