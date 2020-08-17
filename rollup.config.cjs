0ca7c1811c47bf5ffbdbc242fb08890eb8c97788
James Nylen <jnylen@gmail.com>
2020-08-17 04:33:26 +0000

    Reformat/update rollup config

--- rollup.config.cjs
+++ rollup.config.cjs
@@ -2,17 +2,20 @@
 
 const config = [];
 
-const pluginCommonJS = require( 'rollup-plugin-commonjs' );
+const createPluginCommonJS = require( '@rollup/plugin-commonjs' );
+
+const pluginCommonJS = createPluginCommonJS();
 
 [ 'audiovideo', 'grid', 'models', 'views' ].forEach( artifact => {
-    config.push( {
-        input: `src/wp-includes/js/media/${artifact}.manifest.js`,
-        output: {
-            file: `src/wp-includes/js/media-${artifact}.js`,
-            format: 'iife',
-        },
-        plugins: [ pluginCommonJS() ],
-    } );
+	config.push( {
+		input: `src/wp-includes/js/media/${artifact}.manifest.js`,
+		output: {
+			file: `src/wp-includes/js/media-${artifact}.js`,
+			format: 'iife',
+			strict: false,
+		},
+		plugins: [ pluginCommonJS ],
+	} );
 } );
 
 module.exports = config;
