fdbc108eb9fc8dd60fd6fff33fe1578d17fcfebf
John Blackbourn <johnbillion@git.wordpress.org>
2020-12-23 09:37:50 +0000

    WP-r49844: Mail: Introduce a `pre_wp_mail` filter to allow short-circuiting the `wp_mail()` function without having to override the pluggable function.
    WP:Props DvanKooten, swissspidy, SergeyBiryukov, jtsternberg, ericlewis, Mte90, birgire, ayeshrajans
    
    Fixes https://core.trac.wordpress.org/ticket/35069
    
    Conflicts:
    - src/wp-includes/pluggable.php
    
    ---
    
    Merges https://core.trac.wordpress.org/changeset/49844 / WordPress/wordpress-develop@99cfb00d13 to ClassicPress.

--- src/wp-includes/pluggable.php
+++ src/wp-includes/pluggable.php
@@ -178,8 +178,28 @@ function wp_mail( $to, $subject, $message, $headers = '', $attachments = array()
 	 *
 	 * @since WP-2.2.0
 	 *
+<<<<<<< HEAD
 	 * @param array $args A compacted array of wp_mail() arguments, including the "to" email,
 	 *                    subject, message, headers, and attachments values.
+=======
+	 * The default content type is `text/plain` which does not allow using HTML.
+	 * However, you can set the content type of the email by using the
+	 * {@see 'wp_mail_content_type'} filter.
+	 *
+	 * The default charset is based on the charset used on the blog. The charset can
+	 * be set using the {@see 'wp_mail_charset'} filter.
+	 *
+	 * @since 1.2.1
+	 *
+	 * @global PHPMailer\PHPMailer\PHPMailer $phpmailer
+	 *
+	 * @param string|string[] $to          Array or comma-separated list of email addresses to send message.
+	 * @param string          $subject     Email subject.
+	 * @param string          $message     Message contents.
+	 * @param string|string[] $headers     Optional. Additional headers.
+	 * @param string|string[] $attachments Optional. Paths to files to attach.
+	 * @return bool Whether the email was sent successfully.
+>>>>>>> 99cfb00d13 (Mail: Introduce a `pre_wp_mail` filter to allow short-circuiting the `wp_mail()` function without having to override the pluggable function.)
 	 */
 	$atts = apply_filters( 'wp_mail', compact( 'to', 'subject', 'message', 'headers', 'attachments' ) );
 
@@ -187,9 +207,41 @@ function wp_mail( $to, $subject, $message, $headers = '', $attachments = array()
 		$to = $atts['to'];
 	}
 
+<<<<<<< HEAD
 	if ( !is_array( $to ) ) {
 		$to = explode( ',', $to );
 	}
+=======
+		/**
+		 * Filters whether to preempt sending an email.
+		 *
+		 * Returning a non-null value will short-circuit {@see wp_mail()}, returning
+		 * that value instead. A boolean return value should be used to indicate whether
+		 * the email was successfully sent.
+		 *
+		 * @since 5.7.0
+		 *
+		 * @param null|bool $return Short-circuit return value.
+		 * @param array     $atts {
+		 *     Array of the `wp_mail()` arguments.
+		 *
+		 *     @type string|string[] $to          Array or comma-separated list of email addresses to send message.
+		 *     @type string          $subject     Email subject.
+		 *     @type string          $message     Message contents.
+		 *     @type string|string[] $headers     Additional headers.
+		 *     @type string|string[] $attachments Paths to files to attach.
+		 * }
+		 */
+		$pre_wp_mail = apply_filters( 'pre_wp_mail', null, $atts );
+
+		if ( null !== $pre_wp_mail ) {
+			return $pre_wp_mail;
+		}
+
+		if ( isset( $atts['to'] ) ) {
+			$to = $atts['to'];
+		}
+>>>>>>> 99cfb00d13 (Mail: Introduce a `pre_wp_mail` filter to allow short-circuiting the `wp_mail()` function without having to override the pluggable function.)
 
 	if ( isset( $atts['subject'] ) ) {
 		$subject = $atts['subject'];
