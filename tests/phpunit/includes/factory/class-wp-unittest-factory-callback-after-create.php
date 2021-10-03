85a8f9817ed7219bea0f5c09216d921831b6aead
mattyrob <mattyrobuk@gmail.com>
2021-10-03 13:02:12 +0100

    Fix merge conflicts from backport of 46986

--- tests/phpunit/includes/factory/class-wp-unittest-factory-callback-after-create.php
+++ tests/phpunit/includes/factory/class-wp-unittest-factory-callback-after-create.php
@@ -7,9 +7,6 @@ class WP_UnitTest_Factory_Callback_After_Create {
 		$this->callback = $callback;
 	}
 
-<<<<<<< HEAD
-	function call( $object ) {
-=======
 	/**
 	 * Calls the set callback on a given object.
 	 *
@@ -18,7 +15,6 @@ class WP_UnitTest_Factory_Callback_After_Create {
 	 * @return mixed The possibly altered object.
 	 */
 	public function call( $object ) {
->>>>>>> 0d81dcbfb1 (Docs: Add `@method` notation for `WP_UnitTest_Factory_For_Term::create_and_get()` for consistency with other factories.)
 		return call_user_func( $this->callback, $object );
 	}
 }
