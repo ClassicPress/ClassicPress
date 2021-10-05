2d0313563c0d0ae81cf7625079968bee83b80b0f
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2021-10-05 20:51:08 +0100

    WP-r48952: Tests: Add a polyfill for `assertEqualsWithDelta()` to `WP_UnitTestCase` and use it where appropriate.
    `assertEqualsWithDelta()` was added in PHPUnit 7.5, while WordPress still supports PHPUnit 5.4.x as the minimum version.
    
    See https://core.trac.wordpress.org/ticket/38266.
    
    Conflicts:
    - tests/phpunit/includes/testcase.php
    - tests/phpunit/tests/date/currentTime.php
    - tests/phpunit/tests/date/dateI18n.php
    - tests/phpunit/tests/date/getFeedBuildDate.php
    - tests/phpunit/tests/date/query.php
    - tests/phpunit/tests/multisite/site.php
    - tests/phpunit/tests/post.php
    - tests/phpunit/tests/rest-api/rest-posts-controller.php
    
    ---
    
    Merges https://core.trac.wordpress.org/changeset/48952 / WordPress/wordpress-develop@5bad67bccf to ClassicPress.

--- tests/phpunit/includes/testcase.php
+++ tests/phpunit/includes/testcase.php
@@ -16,12 +16,47 @@ class WP_UnitTestCase extends WP_UnitTestCase_Base {
 	/**
 	 * Asserts that a condition is not false.
 	 *
+<<<<<<< HEAD
 	 * @param bool   $condition
 	 * @param string $message
+=======
+	 * This method has been backported from a more recent PHPUnit version,
+	 * as tests running on PHP 5.2 use PHPUnit 3.6.x.
+	 *
+	 * @since 4.7.4
+	 *
+	 * @param bool   $condition Condition to check.
+	 * @param string $message   Optional. Message to display when the assertion fails.
+>>>>>>> 5bad67bccf (Tests: Add a polyfill for `assertEqualsWithDelta()` to `WP_UnitTestCase` and use it where appropriate.)
 	 *
 	 * @throws PHPUnit_Framework_AssertionFailedError
 	 */
 	public static function assertNotFalse( $condition, $message = '' ) {
 		self::assertThat( $condition, self::logicalNot( self::isFalse() ), $message );
+	}
+
+	/**
+	 * Asserts that two variables are equal (with delta).
+	 *
+	 * This method has been backported from a more recent PHPUnit version,
+	 * as tests running on PHP 5.6 use PHPUnit 5.7.x.
+	 *
+	 * @since 5.6.0
+	 *
+	 * @param mixed  $expected First value to compare.
+	 * @param mixed  $actual   Second value to compare.
+	 * @param float  $delta    Allowed numerical distance between two values to consider them equal.
+	 * @param string $message  Optional. Message to display when the assertion fails.
+	 *
+	 * @throws ExpectationFailedException
+	 * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
+	 */
+	public static function assertEqualsWithDelta( $expected, $actual, $delta, $message = '' ) {
+		$constraint = new PHPUnit_Framework_Constraint_IsEqual(
+			$expected,
+			$delta
+		);
+
+		static::assertThat( $actual, $constraint, $message );
 	}
 }
