aada6708ac15d7a20abb5fc36d9aca78323aa5e8
mattyrob <mattyrobuk@gmail.com>
2021-10-06 17:20:06 +0100

    Fix merge conflict from backport of 51561

--- tests/phpunit/includes/testcase.php
+++ tests/phpunit/includes/testcase.php
@@ -4,25 +4,5 @@
  *
  * All ClassicPress unit tests should inherit from this class.
  */
-<<<<<<< HEAD
 
-class WP_UnitTestCase extends WP_UnitTestCase_Base {
-	use AssertAttributeHelper;
-	use AssertClosedResource;
-	use AssertEqualsSpecializations;
-	use AssertFileDirectory;
-	use AssertFileEqualsSpecializations;
-	use AssertionRenames;
-	use AssertIsType;
-	use AssertNumericType;
-	use AssertObjectEquals;
-	use AssertStringContains;
-	use EqualToSpecializations;
-	use ExpectException;
-	use ExpectExceptionMessageMatches;
-	use ExpectExceptionObject;
-	use ExpectPHPException;
-}
-=======
 abstract class WP_UnitTestCase extends WP_UnitTestCase_Base {}
->>>>>>> 31a6cd2f78 (Build/Test Tools: Change the inheritance order of the abstract test classes.)
