c205895f22d6e7c3cfbd37cf19981e6bcbfb1232
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2021-10-09 18:18:05 +0100

    WP-r48794: Code Modernization: Fix PHP 8 deprecation notices for optional function parameters declared before required parameters.
    As it already was not possible to pass the required parameters without also passing the optional one anyway, removing the default value for the (not so) optional parameters should not affect backward compatibility.
    
    This change affects three functions in core:
    
    * `get_comment_delimited_block_content()`
    * `do_enclose()`
    * `_wp_delete_tax_menu_item()`
    
    WP:Props jrf, ayeshrajans, desrosj.
    Fixes https://core.trac.wordpress.org/ticket/50343.
    
    Conflicts:
    - src/wp-includes/blocks.php
    - src/wp-includes/functions.php
    - tests/phpunit/tests/rest-api/rest-attachments-controller.php
    
    ---
    
    Merges https://core.trac.wordpress.org/changeset/48794 / WordPress/wordpress-develop@9c220ffaf7 to ClassicPress.

--- tests/phpunit/tests/rest-api/rest-attachments-controller.php
+++ tests/phpunit/tests/rest-api/rest-attachments-controller.php
@@ -1302,6 +1302,7 @@ class WP_Test_REST_Attachments_Controller extends WP_Test_REST_Post_Type_Control
 		$this->assertArrayNotHasKey( 'https://api.w.org/action-publish', $links );
 	}
 
+<<<<<<< HEAD
 	public function tear_down() {
 		parent::tear_down();
 		if ( file_exists( $this->test_file ) ) {
@@ -1315,6 +1316,9 @@ class WP_Test_REST_Attachments_Controller extends WP_Test_REST_Post_Type_Control
 	}
 
 	protected function check_post_data( $attachment, $data, $context = 'view', $links ) {
+=======
+	protected function check_post_data( $attachment, $data, $context = 'view', $links = array() ) {
+>>>>>>> 9c220ffaf7 (Code Modernization: Fix PHP 8 deprecation notices for optional function parameters declared before required parameters.)
 		parent::check_post_data( $attachment, $data, $context, $links );
 
 		$this->assertArrayNotHasKey( 'content', $data );
