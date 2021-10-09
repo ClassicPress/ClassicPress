70043bc9597cf5cc248fcffc2a3fd8979b838b5e
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2021-10-09 18:41:42 +0100

    WP-r48979: Tests: Revert https://core.trac.wordpress.org/changeset/48973.
    These tests ensure that a `_doing_it_wrong()` notice is thrown when `wpdb::prepare()` is called incorrectly, but also that the function will still handle the provided input as correctly as possible.
    
    Disabling these tests on PHP 8 hides a problem, i.e. the function will no longer throw a notice and handle things correctly, it will now cause a white screen of death due to a fatal error.
    
    That is a backward compatibility break, and `wpdb::prepare()` should be updated instead to maintain the original behaviour on PHP 8.
    
    WP:Props jrf, ayeshrajans.
    See https://core.trac.wordpress.org/ticket/50913, https://core.trac.wordpress.org/ticket/50639.
    
    ---
    
    Merges https://core.trac.wordpress.org/changeset/48979 / WordPress/wordpress-develop@aadc1d70f2 to ClassicPress.

--- tests/phpunit/tests/db.php
+++ tests/phpunit/tests/db.php
@@ -367,7 +367,6 @@ class Tests_DB extends WP_UnitTestCase {
 
 	/**
 	 * @expectedIncorrectUsage wpdb::prepare
-	 * @requires PHP < 8.0
 	 */
 	function test_prepare_sprintf_invalid_args() {
 		global $wpdb;
@@ -390,7 +389,6 @@ class Tests_DB extends WP_UnitTestCase {
 
 	/**
 	 * @expectedIncorrectUsage wpdb::prepare
-	 * @requires PHP < 8.0
 	 */
 	function test_prepare_vsprintf_invalid_args() {
 		global $wpdb;
@@ -408,7 +406,6 @@ class Tests_DB extends WP_UnitTestCase {
 	 * @see https://core.trac.wordpress.org/ticket/42040
 	 * @dataProvider data_prepare_incorrect_arg_count
 	 * @expectedIncorrectUsage wpdb::prepare
-	 * @requires PHP < 8.0
 	 */
 	public function test_prepare_incorrect_arg_count( $query, $args, $expected ) {
 		global $wpdb;
