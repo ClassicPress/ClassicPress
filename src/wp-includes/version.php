564805c73c9362f11731f18ce690b2e35c502c9e
Andrew Ozz <azaozz@git.wordpress.org>
2021-10-11 08:57:08 +0100

    WP-r48158: TinyMCE: Update to 4.9.10. Changelog: https://www.tiny.cloud/docs-4x/changelog/#version4910april232020.
    WP:Props hareesh-pillai, azaozz.
    Fixes https://core.trac.wordpress.org/ticket/50431.
    
    Conflicts:
    - src/wp-includes/js/tinymce/skins/lightgray/skin.min.css
    
    ---
    
    Merges https://core.trac.wordpress.org/changeset/48158 / WordPress/wordpress-develop@67839d5a62 to ClassicPress.

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -54,7 +54,7 @@ $wp_db_version = 38590;
  *
  * @global string $tinymce_version
  */
-$tinymce_version = '4960-20190918';
+$tinymce_version = '49100-20200624';
 
 /**
  * Holds the required PHP version
