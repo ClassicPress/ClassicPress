8d8ebd65e878e77a60d1d48eee21f3a9134942f3
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2021-10-11 08:12:53 +0100

    WP-r48790: Code Modernization: Change `create_function()` in `phpunit/includes/plural-form-function.php` to closure.
    `create_function()` has been deprecated in PHP >= 7.2 and removed in PHP 8.
    
    The only instance left in core was used in a test that was being skipped on PHP >= 7.2. This allows the test to run again.
    
    Follow-up to https://core.trac.wordpress.org/changeset/41722, https://core.trac.wordpress.org/changeset/41730.
    
    WP:Props jrf.
    Fixes https://core.trac.wordpress.org/ticket/50899.
    
    Conflicts:
    - tests/phpunit/includes/plural-form-function.php
    - tests/phpunit/tests/pomo/pluralForms.php
    
    ---
    
    Merges https://core.trac.wordpress.org/changeset/48790 / WordPress/wordpress-develop@f0733600c9 to ClassicPress.

--- tests/phpunit/includes/plural-form-function.php
+++ tests/phpunit/includes/plural-form-function.php
@@ -7,10 +7,23 @@
  * @param string $expression
  */
 function tests_make_plural_form_function( $nplurals, $expression ) {
+<<<<<<< HEAD
 	$expression = str_replace( 'n', '$n', $expression );
 	$func_body = "
 		\$index = (int)($expression);
 		return (\$index < $nplurals)? \$index : $nplurals - 1;";
 
 	return create_function( '$n', $func_body );
+=======
+	$closure = function ( $n ) use ( $nplurals, $expression ) {
+		$expression = str_replace( 'n', $n, $expression );
+
+		// phpcs:ignore Squiz.PHP.Eval -- This is test code, not production.
+		$index = (int) eval( 'return ' . $expression . ';' );
+
+		return ( $index < $nplurals ) ? $index : $nplurals - 1;
+	};
+
+	return $closure;
+>>>>>>> f0733600c9 (Code Modernization: Change `create_function()` in `phpunit/includes/plural-form-function.php` to closure.)
 }
--- tests/phpunit/tests/pomo/pluralForms.php
+++ tests/phpunit/tests/pomo/pluralForms.php
@@ -74,11 +74,15 @@ class PluralFormsTest extends WP_UnitTestCase {
 	 * @group external-http
 	 */
 	public function test_regression( $lang, $nplurals, $expression ) {
+<<<<<<< HEAD
 		if ( version_compare( phpversion(), '7.2', '>=' ) ) {
 			$this->markTestSkipped( 'Lambda functions are deprecated in PHP 7.2' );
 		}
 
 		require_once dirname( dirname( dirname( __FILE__ ) ) ) . '/includes/plural-form-function.php';
+=======
+		require_once dirname( dirname( __DIR__ ) ) . '/includes/plural-form-function.php';
+>>>>>>> f0733600c9 (Code Modernization: Change `create_function()` in `phpunit/includes/plural-form-function.php` to closure.)
 
 		$parenthesized = self::parenthesize_plural_expression( $expression );
 		$old_style = tests_make_plural_form_function( $nplurals, $parenthesized );
