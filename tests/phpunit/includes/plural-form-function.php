d26d16248de3499aa2908924317a4889bec5b668
mattyrob <mattyrobuk@gmail.com>
2021-10-11 08:19:05 +0100

    Fix merge conflicts from backport of 48790

--- tests/phpunit/includes/plural-form-function.php
+++ tests/phpunit/includes/plural-form-function.php
@@ -7,14 +7,6 @@
  * @param string $expression
  */
 function tests_make_plural_form_function( $nplurals, $expression ) {
-<<<<<<< HEAD
-	$expression = str_replace( 'n', '$n', $expression );
-	$func_body = "
-		\$index = (int)($expression);
-		return (\$index < $nplurals)? \$index : $nplurals - 1;";
-
-	return create_function( '$n', $func_body );
-=======
 	$closure = function ( $n ) use ( $nplurals, $expression ) {
 		$expression = str_replace( 'n', $n, $expression );
 
@@ -25,5 +17,4 @@ function tests_make_plural_form_function( $nplurals, $expression ) {
 	};
 
 	return $closure;
->>>>>>> f0733600c9 (Code Modernization: Change `create_function()` in `phpunit/includes/plural-form-function.php` to closure.)
 }
