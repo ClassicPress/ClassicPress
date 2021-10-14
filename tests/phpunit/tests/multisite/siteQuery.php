db2a75c7ee108b8d281c5b3f50b6750a3c7a59bb
James Nylen <jnylen@gmail.com>
2021-10-14 17:07:22 -0500

    Remove WP-only test
    The 'sites_pre_query' filter was added in a WP changeset that
    ClassicPress has not backported:
    
    https://core.trac.wordpress.org/changeset/44983

--- tests/phpunit/tests/multisite/siteQuery.php
+++ tests/phpunit/tests/multisite/siteQuery.php
@@ -739,40 +739,8 @@ class Tests_Multisite_Site_Query extends WP_UnitTestCase {
 				'count'      => true,
 				)
 			);
-			$this->assertSame( $number_of_queries + 1, $wpdb->num_queries );
-		}
-
-		/**
-		 * @ticket 45749
-		 * @ticket 47599
-		 */
-		public function test_sites_pre_query_filter_should_bypass_database_query() {
-			global $wpdb;
-
-			add_filter( 'sites_pre_query', array( __CLASS__, 'filter_sites_pre_query' ), 10, 2 );
-
-			$num_queries = $wpdb->num_queries;
-
-			$q       = new WP_Site_Query();
-			$results = $q->query( array() );
-
-			remove_filter( 'sites_pre_query', array( __CLASS__, 'filter_sites_pre_query' ), 10, 2 );
-
-			// Make sure no queries were executed.
-			$this->assertSame( $num_queries, $wpdb->num_queries );
-
-			// We manually inserted a non-existing site and overrode the results with it.
-			$this->assertSame( array( 555 ), $results );
-
-			// Make sure manually setting total_users doesn't get overwritten.
-			$this->assertSame( 1, $q->found_sites );
-		}
-
-		public static function filter_sites_pre_query( $sites, $query ) {
-			$query->found_sites = 1;
-
-			return array( 555 );
-		}
+		$this->assertSame( $number_of_queries + 1, $wpdb->num_queries );
 	}
+}
 
 endif;
