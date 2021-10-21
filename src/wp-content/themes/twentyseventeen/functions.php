c6dec295aa0aaaf59ec54abd7979756170c69f9b
SergeyBiryukov <SergeyBiryukov@602fd350-edb4-49c9-b593-d223f7449a82>
2021-10-21 18:05:11 +0100

    WP-r46165: Bundled Themes: Declare HTML5 support for scripts and styles.
    Fixes https://core.trac.wordpress.org/ticket/42804.
    
    Conflicts:
    - src/wp-content/themes/twentyfifteen/functions.php
    - src/wp-content/themes/twentyfourteen/functions.php
    - src/wp-content/themes/twentynineteen/functions.php
    - src/wp-content/themes/twentyseventeen/functions.php
    - src/wp-content/themes/twentysixteen/functions.php
    - src/wp-content/themes/twentythirteen/functions.php
    
    ---
    
    Merges https://core.trac.wordpress.org/changeset/46165 / WordPress/wordpress-develop@baaaef5b63 to ClassicPress.

--- src/wp-content/themes/twentyseventeen/functions.php
+++ src/wp-content/themes/twentyseventeen/functions.php
@@ -68,12 +68,26 @@ function twentyseventeen_setup() {
 	 * Switch default core markup for search form, comment form, and comments
 	 * to output valid HTML5.
 	 */
+<<<<<<< HEAD
 	add_theme_support( 'html5', array(
 		'comment-form',
 		'comment-list',
 		'gallery',
 		'caption',
 	) );
+=======
+	add_theme_support(
+		'html5',
+		array(
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'caption',
+			'script',
+			'style',
+		)
+	);
+>>>>>>> baaaef5b63 (Bundled Themes: Declare HTML5 support for scripts and styles.)
 
 	/*
 	 * Enable support for Post Formats.
