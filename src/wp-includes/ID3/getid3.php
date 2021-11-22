dfa908dfb9829e96fb9d15e8a3f48d0482ef785f
Jonathan Desrosiers <desrosj@git.wordpress.org>
2021-11-22 16:44:13 +0000

    WP-r47735: General: Fix various issues flagged by the PHPCompatibilityWP PHPCS ruleset.
    As part of the continued effort to improve PHP compatibility, the following improvments are being made:
    
    - Removing deprecated PHP `safe_mode` checks not found in bundled external libraries.
    - Change the remaining `while` loops using `each()` to `foreach` loops.
    - Prevent false positives from being flagged for the `sodium_compat` library being caused by loading this in a non-standard way.
    - Add inline comments to not flag deprecated PHP directives in the getID3 library.
    
    WP:Props desrosj, earnjam, dryanpress.
    See https://core.trac.wordpress.org/ticket/49922.
    
    Conflicts:
    - src/wp-admin/includes/schema.php
    - src/wp-includes/ID3/getid3.php
    
    ---
    
    Merges https://core.trac.wordpress.org/changeset/47735 / WordPress/wordpress-develop@8e2de8212b to ClassicPress.

--- src/wp-includes/ID3/getid3.php
+++ src/wp-includes/ID3/getid3.php
@@ -283,7 +283,11 @@ class getID3
 		}
 
 		// Check safe_mode off
+<<<<<<< HEAD
 		if (preg_match('#(1|ON)#i', ini_get('safe_mode'))) { // phpcs:ignore PHPCompatibility.IniDirectives.RemovedIniDirectives.safe_modeDeprecatedRemoved
+=======
+		if (preg_match('#(1|ON)#i', ini_get('safe_mode'))) { // phpcs:ignore  // phpcs:ignore PHPCompatibility.FunctionUse.RemovedFunctions.get_magic_quotes_runtimeDeprecated
+>>>>>>> 8e2de8212b (General: Fix various issues flagged by the PHPCompatibilityWP PHPCS ruleset.)
 			$this->warning('WARNING: Safe mode is on, shorten support disabled, md5data/sha1data for ogg vorbis disabled, ogg vorbos/flac tag writing disabled.');
 		}
 
@@ -296,6 +300,7 @@ class getID3
 			$this->startup_error .= 'WARNING: php.ini contains "mbstring.func_overload = '.ini_get('mbstring.func_overload').'", getID3 cannot run with this setting (bitmask 2 (string functions) cannot be set). Recommended to disable entirely.'."\n";
 		}
 
+<<<<<<< HEAD
 		// WORDPRESS CHANGE FROM UPSTREAM
 		// Comment out deprecated function
 		/*
@@ -310,6 +315,21 @@ class getID3
 		if (function_exists('magic_quotes_gpc')) {
 			if (get_magic_quotes_gpc()) {
 				$this->startup_error .= 'magic_quotes_gpc must be disabled before running getID3(). Surround getid3 block by set_magic_quotes_gpc(0) and set_magic_quotes_gpc(1).'."\n";
+=======
+		// check for magic quotes in PHP < 7.4.0 (when these functions became deprecated)
+		if (version_compare(PHP_VERSION, '7.4.0', '<')) {
+			// Check for magic_quotes_runtime
+			if (function_exists('get_magic_quotes_runtime')) {
+				if (get_magic_quotes_runtime()) { // phpcs:ignore PHPCompatibility.FunctionUse.RemovedFunctions.get_magic_quotes_runtimeDeprecated
+					$this->startup_error .= 'magic_quotes_runtime must be disabled before running getID3(). Surround getid3 block by set_magic_quotes_runtime(0) and set_magic_quotes_runtime(1).'."\n";
+				}
+			}
+			// Check for magic_quotes_gpc
+			if (function_exists('get_magic_quotes_gpc')) {
+				if (get_magic_quotes_gpc()) { // phpcs:ignore PHPCompatibility.FunctionUse.RemovedFunctions.get_magic_quotes_gpcDeprecated
+					$this->startup_error .= 'magic_quotes_gpc must be disabled before running getID3(). Surround getid3 block by set_magic_quotes_gpc(0) and set_magic_quotes_gpc(1).'."\n";
+				}
+>>>>>>> 8e2de8212b (General: Fix various issues flagged by the PHPCompatibilityWP PHPCS ruleset.)
 			}
 		}
 		**/
@@ -1553,7 +1573,11 @@ class getID3
 			// page sequence numbers likely happens for OggSpeex and OggFLAC as well, but
 			// currently vorbiscomment only works on OggVorbis files.
 
+<<<<<<< HEAD
 			if (preg_match('#(1|ON)#i', ini_get('safe_mode'))) { // phpcs:ignore PHPCompatibility.IniDirectives.RemovedIniDirectives.safe_modeDeprecatedRemoved
+=======
+			if (preg_match('#(1|ON)#i', ini_get('safe_mode'))) { // phpcs:ignore  // phpcs:ignore PHPCompatibility.FunctionUse.RemovedFunctions.get_magic_quotes_runtimeDeprecated
+>>>>>>> 8e2de8212b (General: Fix various issues flagged by the PHPCompatibilityWP PHPCS ruleset.)
 
 				$this->warning('Failed making system call to vorbiscomment.exe - '.$algorithm.'_data is incorrect - error returned: PHP running in Safe Mode (backtick operator not available)');
 				$this->info[$algorithm.'_data'] = false;
