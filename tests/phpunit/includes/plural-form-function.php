96b80f5744e3c6c1a71185ef8f344e34bc09075e
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2021-12-06 16:37:50 +0000

    WP-r48790: Code Modernization: Change `create_function()` in `phpunit/includes/plural-form-function.php` to closure.
    `create_function()` has been deprecated in PHP >= 7.2 and removed in PHP 8.
    
    The only instance left in core was used in a test that was being skipped on PHP >= 7.2. This allows the test to run again.
    
    Follow-up to https://core.trac.wordpress.org/changeset/41722, https://core.trac.wordpress.org/changeset/41730.
    
    WP:Props jrf.
    Fixes https://core.trac.wordpress.org/ticket/50899.
    
    Conflicts:
    - tests/phpunit/includes/plural-form-function.php
    - tests/phpunit/tests/pomo/pluralForms.php
    
    ---
    
    Merges https://core.trac.wordpress.org/changeset/48790 / WordPress/wordpress-develop@f0733600c9 to ClassicPress.

--- tests/phpunit/includes/plural-form-function.php
+++ tests/phpunit/includes/plural-form-function.php
@@ -7,10 +7,18 @@
  * @param string $expression
  */
 function tests_make_plural_form_function( $nplurals, $expression ) {
-	$expression = str_replace( 'n', '$n', $expression );
-	$func_body = "
-		\$index = (int)($expression);
-		return (\$index < $nplurals)? \$index : $nplurals - 1;";
+	$closure = function ( $n ) use ( $nplurals, $expression ) {
+		$expression = str_replace( 'n', $n, $expression );
 
+<<<<<<< HEAD
 	return create_function( '$n', $func_body );
+=======
+		// phpcs:ignore Squiz.PHP.Eval -- This is test code, not production.
+		$index = (int) eval( 'return ' . $expression . ';' );
+
+		return ( $index < $nplurals ) ? $index : $nplurals - 1;
+	};
+
+	return $closure;
+>>>>>>> f0733600c9 (Code Modernization: Change `create_function()` in `phpunit/includes/plural-form-function.php` to closure.)
 }
