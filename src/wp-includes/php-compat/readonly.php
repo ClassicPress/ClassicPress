cc454cdfae2156e862a4eac2d4eb06671d42b57e
mattyrob <mattyrobuk@gmail.com>
2021-12-14 19:19:00 +0000

    Correct documentation indication changes in WP

--- src/wp-includes/php-compat/readonly.php
+++ src/wp-includes/php-compat/readonly.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Conditionally declares a `readonly()` function, which was renamed
- * to `wp_readonly()` in WordPress 5.9.0.
+ * to `wp_readonly()` in WordPress WP-5.9.0.
  *
  * In order to avoid PHP parser errors, this function was extracted
  * to this separate file and is only included conditionally on PHP 8.1.
@@ -9,7 +9,7 @@
  * Including this file on PHP >= 8.1 results in a fatal error.
  *
  * @package WordPress
- * @since 5.9.0
+ * @since WP-5.9.0
  */
 
 /**
@@ -19,8 +19,8 @@
  *
  * This function is deprecated, and cannot be used on PHP >= 8.1.
  *
- * @since 4.9.0
- * @deprecated 5.9.0 Use `wp_readonly` introduced in 5.9.0.
+ * @since WP-4.9.0
+ * @deprecated WP-5.9.0 Use `wp_readonly` introduced in WP-5.9.0.
  *
  * @see wp_readonly()
  *
@@ -30,6 +30,6 @@
  * @return string HTML attribute or empty string
  */
 function readonly( $readonly, $current = true, $echo = true ) {
-	_deprecated_function( __FUNCTION__, '5.9.0', 'wp_readonly()' );
+	_deprecated_function( __FUNCTION__, 'WP-5.9.0', 'wp_readonly()' );
 	return wp_readonly( $readonly, $current, $echo );
 }
