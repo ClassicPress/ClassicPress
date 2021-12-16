a99797edc0cebfe68a22b8178b8e9cce804225e0
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2021-12-16 17:26:22 +0000

    WP-r44420: Plugins: Use newer "Updating PHP" page URL in the notice displayed when a plugin requires a higher PHP version.
    WP:Props afragen.
    Fixes https://core.trac.wordpress.org/ticket/43986. See https://core.trac.wordpress.org/ticket/45686.
    
    Conflicts:
    - src/wp-admin/includes/dashboard.php
    
    ---
    
    Merges https://core.trac.wordpress.org/changeset/44420 / WordPress/wordpress-develop@6d8e3c5864 to ClassicPress.

--- src/wp-admin/includes/plugin-install.php
+++ src/wp-admin/includes/plugin-install.php
@@ -727,7 +727,7 @@ function install_plugin_information() {
 		printf(
 			/* translators: "Updating PHP" page URL */
 			__( '<strong>Error:</strong> This plugin <strong>requires a newer version of PHP</strong>, so unfortunately you cannot install it. <a href="%s" target="_blank">Click here to learn more about updating PHP</a>.' ),
-			esc_url( __( 'https://wordpress.org/support/upgrade-php/' ) )
+			esc_url( __( 'https://wordpress.org/support/update-php/' ) )
 		);
 		echo '</p></div>';
 	}
