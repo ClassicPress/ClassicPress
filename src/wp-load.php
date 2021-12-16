b28fba57a570f2a9c91355332014dde110ee8ecb
pattonwebz <will@pattonwebz.com>
2021-12-16 20:37:41 +0000

    PHPCS: fix missing space before closing parenthesis and swap unnecessary double quotes

--- src/wp-load.php
+++ src/wp-load.php
@@ -31,7 +31,7 @@ error_reporting( E_CORE_ERROR | E_CORE_WARNING | E_COMPILE_ERROR | E_ERROR | E_W
  *
  * If neither set of conditions is true, initiate loading the setup process.
  */
-if ( file_exists( ABSPATH . 'wp-config.php') ) {
+if ( file_exists( ABSPATH . 'wp-config.php' ) ) {
 
 	/** The config file resides in ABSPATH */
 	require_once( ABSPATH . 'wp-config.php' );
@@ -87,7 +87,7 @@ if ( file_exists( ABSPATH . 'wp-config.php') ) {
 		__( "You can create a %s file through a web interface, but this doesn't work for all server setups. The safest way is to manually create the file." ),
 		'<code>wp-config.php</code>'
 	) . '</p>';
-	$die .= '<p><a href="' . $path . '" class="button button-large">' . __( "Create a Configuration File" ) . '</a>';
+	$die .= '<p><a href="' . $path . '" class="button button-large">' . __( 'Create a Configuration File' ) . '</a>';
 
 	wp_die( $die, __( 'ClassicPress &rsaquo; Error' ) );
 }
