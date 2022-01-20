0661224619130c93179f7581127a880ef115c151
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2022-01-20 09:43:09 +0000

    WP-r48912: Date/Time: Make sure `get_the_date()` and related functions return correct time if the format was specified as `false`.
    Technically, the `$format` argument should always be a string, but passing `false` used to work before https://core.trac.wordpress.org/changeset/47808, so this restores backward compatibility.
    
    The list of affected functions:
    * `get_the_date()`
    * `get_the_time()`
    * `get_comment_date()`
    * `get_comment_time()`
    
    WP:Props wittich, Rarst, akabarikalpesh, SergeyBiryukov.
    Fixes https://core.trac.wordpress.org/ticket/51184.
    
    Conflicts:
    - src/wp-includes/comment-template.php
    - src/wp-includes/general-template.php
    
    ---
    
    Merges https://core.trac.wordpress.org/changeset/48912 / WordPress/wordpress-develop@3309309f6b to ClassicPress.

--- src/wp-includes/comment-template.php
+++ src/wp-includes/comment-template.php
@@ -538,11 +538,21 @@ function get_comment_class( $class = '', $comment_id = null, $post_id = null ) {
  */
 function get_comment_date( $format = '', $comment_ID = 0 ) {
 	$comment = get_comment( $comment_ID );
+<<<<<<< HEAD
 	if ( '' == $format ) {
 		$date = mysql2date( get_option( 'date_format' ), $comment->comment_date );
 	} else {
 		$date = mysql2date( $format, $comment->comment_date );
 	}
+=======
+
+	if ( ! is_string( $format ) || '' === $format ) {
+		$format = get_option( 'date_format' );
+	}
+
+	$date = mysql2date( $format, $comment->comment_date );
+
+>>>>>>> 3309309f6b (Date/Time: Make sure `get_the_date()` and related functions return correct time if the format was specified as `false`.)
 	/**
 	 * Filters the returned comment date.
 	 *
@@ -1012,11 +1022,18 @@ function get_comment_time( $format = '', $gmt = false, $translate = true ) {
 	$comment = get_comment();
 
 	$comment_date = $gmt ? $comment->comment_date_gmt : $comment->comment_date;
+<<<<<<< HEAD
 	if ( '' == $format ) {
 		$date = mysql2date( get_option( 'time_format' ), $comment_date, $translate );
 	} else {
-		$date = mysql2date( $format, $comment_date, $translate );
+=======
+
+	if ( ! is_string( $format ) || '' === $format ) {
+		$format = get_option( 'time_format' );
 	}
+
+>>>>>>> 3309309f6b (Date/Time: Make sure `get_the_date()` and related functions return correct time if the format was specified as `false`.)
+		$date = mysql2date( $format, $comment_date, $translate );
 
 	/**
 	 * Filters the returned comment time.
