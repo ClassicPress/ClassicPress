9f38358b5d2a593fd89f8dbe17e56e32b51a140f
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2022-01-20 09:46:23 +0000

    WP-r48918: Date/Time: In `get_the_date()` and related functions, pass the original, unmodified `$format` value to the filters.
    Additionally, simplify the `$format` argument checks for consistency with similar checks in `get_the_modified_date()` and `get_the_modified_time()`.
    
    Follow-up to https://core.trac.wordpress.org/changeset/48912.
    
    WP:Props Rarst.
    See https://core.trac.wordpress.org/ticket/51184.
    
    ---
    
    Merges https://core.trac.wordpress.org/changeset/48918 / WordPress/wordpress-develop@d33fa375f6 to ClassicPress.

--- src/wp-includes/general-template.php
+++ src/wp-includes/general-template.php
@@ -2256,11 +2256,9 @@ function get_the_date( $format = '', $post = null ) {
 		return false;
 	}
 
-	if ( ! is_string( $format ) || '' === $format ) {
-		$format = get_option( 'date_format' );
-	}
+	$_format = ! empty( $format ) ? $format : get_option( 'date_format' );
 
-	$the_date = get_post_time( $format, false, $post, true );
+	$the_date = get_post_time( $_format, false, $post, true );
 
 	/**
 	 * Filters the date a post was published.
@@ -2325,10 +2323,10 @@ function get_the_modified_date( $format = '', $post = null ) {
 	if ( ! $post ) {
 		// For backward compatibility, failures go through the filter below.
 		$the_time = false;
-	} elseif ( empty( $format ) ) {
-		$the_time = get_post_modified_time( get_option( 'date_format' ), false, $post, true );
 	} else {
-		$the_time = get_post_modified_time( $format, false, $post, true );
+		$_format = ! empty( $format ) ? $format : get_option( 'date_format' );
+
+		$the_time = get_post_modified_time( $_format, false, $post, true );
 	}
 
 	/**
@@ -2384,11 +2382,9 @@ function get_the_time( $format = '', $post = null ) {
 		return false;
 	}
 
-	if ( ! is_string( $format ) || '' === $format ) {
-		$format = get_option( 'time_format' );
-	}
+	$_format = ! empty( $format ) ? $format : get_option( 'time_format' );
 
-	$the_time = get_post_time( $format, false, $post, true );
+	$the_time = get_post_time( $_format, false, $post, true );
 
 	/**
 	 * Filters the time a post was written.
@@ -2573,10 +2569,10 @@ function get_the_modified_time( $format = '', $post = null ) {
 	if ( ! $post ) {
 		// For backward compatibility, failures go through the filter below.
 		$the_time = false;
-	} elseif ( empty( $format ) ) {
-		$the_time = get_post_modified_time( get_option( 'time_format' ), false, $post, true );
 	} else {
-		$the_time = get_post_modified_time( $format, false, $post, true );
+		$_format = ! empty( $format ) ? $format : get_option( 'time_format' );
+
+		$the_time = get_post_modified_time( $_format, false, $post, true );
 	}
 
 	/**
