1c2286b345085fd20e3221a5a134b662ab741933
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2022-01-20 09:11:41 +0000

    WP-r45911: Date/Time: Update XML-RPC tests for invalid date input to account for a more consistent `mysql2date()` error handling in https://core.trac.wordpress.org/changeset/45908.
    The tests were meant to check for the lack of a fatal error in case of invalid date input, not specifically mark a particular outcome as the correct one.
    
    WP:Props Rarst.
    Fixes https://core.trac.wordpress.org/ticket/28992.
    
    Conflicts:
    - tests/phpunit/tests/xmlrpc/wp/newPost.php
    
    ---
    
    Merges https://core.trac.wordpress.org/changeset/45911 / WordPress/wordpress-develop@355d93d977 to ClassicPress.

--- tests/phpunit/tests/xmlrpc/wp/newPost.php
+++ tests/phpunit/tests/xmlrpc/wp/newPost.php
@@ -305,7 +305,7 @@ class Tests_XMLRPC_wp_newPost extends WP_XMLRPC_UnitTestCase {
 		$result = $this->myxmlrpcserver->wp_newPost( array( 1, 'author', 'author', $post ) );
 		$fetched_post = get_post( $result );
 		$this->assertStringMatchesFormat( '%d', $result );
-		$this->assertEquals( '1970-01-01 00:00:00', $fetched_post->post_date );
+		$this->assertEquals( current_time( 'Y-m-d' ), substr( $fetched_post->post_date, 0, 10 ) );
 	}
 
 	/**
@@ -313,12 +313,21 @@ class Tests_XMLRPC_wp_newPost extends WP_XMLRPC_UnitTestCase {
 	 */
 	function test_invalid_post_date_gmt_does_not_fatal() {
 		$this->make_user_by_role( 'author' );
+<<<<<<< HEAD
 		$date_string = 'invalid date';
 		$post = array( 'post_title' => 'test', 'post_content' => 'test', 'post_date_gmt' => $date_string );
+=======
+		$date_string  = 'invalid_date';
+		$post         = array(
+			'post_title'    => 'test',
+			'post_content'  => 'test',
+			'post_date_gmt' => $date_string,
+		);
+>>>>>>> 355d93d977 (Date/Time: Update XML-RPC tests for invalid date input to account for a more consistent `mysql2date()` error handling in [45908].)
 		$result = $this->myxmlrpcserver->wp_newPost( array( 1, 'author', 'author', $post ) );
 		$fetched_post = get_post( $result );
 		$this->assertStringMatchesFormat( '%d', $result );
-		$this->assertEquals( '1970-01-01 00:00:00', $fetched_post->post_date_gmt );
+		$this->assertEquals( '0000-00-00', substr( $fetched_post->post_date_gmt, 0, 10 ) );
 	}
 
 	/**
