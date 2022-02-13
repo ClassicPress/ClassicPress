7faac085d07b83d9016ec9652a2b264b303e632f
mattyrob <mattyrobuk@gmail.com>
2022-02-13 09:27:10 +0000

    Fix merge conflicts from backport of 48794

--- src/wp-includes/functions.php
+++ src/wp-includes/functions.php
@@ -621,15 +621,9 @@ function wp_extract_urls( $content ) {
  *
  * @since WP-1.5.0
  * @since WP-5.3.0 The `$content` parameter was made optional, and the `$post` parameter was
- *              updated to accept a post ID or a WP_Post object.
-<<<<<<< HEAD
+ *                 updated to accept a post ID or a WP_Post object.
  * @since WP-5.6.0 The `$content` parameter is no longer optional, but passing `null` to skip it
- *              is still supported.
-
-=======
- * @since 5.6.0 The `$content` parameter is no longer optional, but passing `null` to skip it
- *              is still supported.
->>>>>>> 9c220ffaf7 (Code Modernization: Fix PHP 8 deprecation notices for optional function parameters declared before required parameters.)
+ *                 is still supported.
  *
  * @global wpdb $wpdb ClassicPress database abstraction object.
  *
--- tests/phpunit/tests/rest-api/rest-attachments-controller.php
+++ tests/phpunit/tests/rest-api/rest-attachments-controller.php
@@ -1290,21 +1290,6 @@ class WP_Test_REST_Attachments_Controller extends WP_Test_REST_Post_Type_Control
 		$this->assertArrayNotHasKey( 'https://api.w.org/action-publish', $links );
 	}
 
-<<<<<<< HEAD
-	public function tearDown() {
-		parent::tearDown();
-		if ( file_exists( $this->test_file ) ) {
-			unlink( $this->test_file );
-		}
-		if ( file_exists( $this->test_file2 ) ) {
-			unlink( $this->test_file2 );
-		}
-
-		$this->remove_added_uploads();
-	}
-
-=======
->>>>>>> 9c220ffaf7 (Code Modernization: Fix PHP 8 deprecation notices for optional function parameters declared before required parameters.)
 	protected function check_post_data( $attachment, $data, $context = 'view', $links = array() ) {
 		parent::check_post_data( $attachment, $data, $context, $links );
 
