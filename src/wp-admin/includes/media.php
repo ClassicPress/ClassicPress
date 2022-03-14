8531397d3c4c4c01678ddcf92d1b92e34e678819
mattyrob <mattyrobuk@gmail.com>
2022-03-14 19:46:54 +0000

    WP-r52837: Media: Store attachment’s file size in metadata (#944)
    * WP-r52837: Media: Store attachment’s file size in metadata.
    
    Store the file size of all newly uploaded attachments, as part of the metadata stored in post meta. Storing file size means, developers will not have to resort to doing `filesize` function calls, that can be time consuming on assets on offloaded to services like Amazon’s S3.
    
    This change also introduces a new helper function called, `wp_filesize`. This is a wrapper around the `filesize` php function, that adds some helpful filters and ensures the return value is an integer.
    
    WP:Props Cybr, Spacedmonkey, SergeyBiryukov, johnwatkins0, swissspidy, desrosj, joemcgill, azaozz, antpb, adamsilverstein, uday17035.
    Fixes https://core.trac.wordpress.org/ticket/49412.
    
    Conflicts:
    - src/wp-admin/includes/image.php
    - src/wp-admin/includes/media.php
    - src/wp-includes/post.php
    - tests/phpunit/tests/file.php
    - tests/phpunit/tests/image/functions.php
    
    ---
    
    Merges https://core.trac.wordpress.org/changeset/52837 / WordPress/wordpress-develop@7fc225aa87 to ClassicPress.
    
    * Fix merge conflicts from backport of 52837
    
    Co-authored-by: Jonny Harris <spacedmonkey@git.wordpress.org>

--- src/wp-admin/includes/media.php
+++ src/wp-admin/includes/media.php
@@ -2904,10 +2904,11 @@ function attachment_submitbox_metadata() {
 	<?php
 		$file_size = false;
 
-		if ( isset( $meta['filesize'] ) )
+		if ( isset( $meta['filesize'] ) ) {
 			$file_size = $meta['filesize'];
-		elseif ( file_exists( $file ) )
-			$file_size = filesize( $file );
+		} elseif ( file_exists( $file ) ) {
+			$file_size = wp_filesize( $file );
+		}
 
 		if ( ! empty( $file_size ) ) : ?>
 			<div class="misc-pub-section misc-pub-filesize">
