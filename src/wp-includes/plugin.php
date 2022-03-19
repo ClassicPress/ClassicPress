570f1e1b00354bea60c8fefd0dc836e8ad380d61
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2022-03-19 14:08:27 +0000

    WP-r46146: Code Modernisation: Simplify some logic in `apply_filters()`.
    WP:Props jrf.
    See https://core.trac.wordpress.org/ticket/47678.
    
    Conflicts:
    - src/wp-includes/plugin.php
    
    ---
    
    Merges https://core.trac.wordpress.org/changeset/46146 / WordPress/wordpress-develop@209666d332 to ClassicPress.

--- src/wp-includes/plugin.php
+++ src/wp-includes/plugin.php
@@ -176,13 +176,17 @@ function has_filter($tag, $function_to_check = false) {
 function apply_filters( $tag, $value ) {
 	global $wp_filter, $wp_current_filter;
 
-	$args = array();
+	$args = func_get_args();
 
 	// Do 'all' actions first.
 	if ( isset($wp_filter['all']) ) {
 		$wp_current_filter[] = $tag;
+<<<<<<< HEAD
 		$args = func_get_args();
 		_wp_call_all_hook($args);
+=======
+		_wp_call_all_hook( $args );
+>>>>>>> 209666d332 (Code Modernisation: Simplify some logic in `apply_filters()`.)
 	}
 
 	if ( !isset($wp_filter[$tag]) ) {
@@ -194,10 +198,14 @@ function apply_filters( $tag, $value ) {
 	if ( !isset($wp_filter['all']) )
 		$wp_current_filter[] = $tag;
 
+<<<<<<< HEAD
 	if ( empty($args) )
 		$args = func_get_args();
 
 	// don't pass the tag name to WP_Hook
+=======
+	// Don't pass the tag name to WP_Hook.
+>>>>>>> 209666d332 (Code Modernisation: Simplify some logic in `apply_filters()`.)
 	array_shift( $args );
 
 	$filtered = $wp_filter[ $tag ]->apply_filters( $value, $args );
