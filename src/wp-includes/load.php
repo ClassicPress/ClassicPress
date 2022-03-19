85aec49b781c621052b24390a9247dcbf8c7fbb8
mattyrob <mattyrobuk@gmail.com>
2022-03-19 13:18:03 +0000

    Fix merge conflicts from backport of 47612

--- src/wp-includes/load.php
+++ src/wp-includes/load.php
@@ -21,32 +21,6 @@ function wp_get_server_protocol() {
 }
 
 /**
-<<<<<<< HEAD
- * Turn register globals off.
- *
- * @since WP-2.1.0
- * @access private
- */
-function wp_unregister_GLOBALS() {
-	if ( !ini_get( 'register_globals' ) )
-		return;
-
-	if ( isset( $_REQUEST['GLOBALS'] ) )
-		die( 'GLOBALS overwrite attempt detected' );
-
-	// Variables that shouldn't be unset
-	$no_unset = array( 'GLOBALS', '_GET', '_POST', '_COOKIE', '_REQUEST', '_SERVER', '_ENV', '_FILES', 'table_prefix' );
-
-	$input = array_merge( $_GET, $_POST, $_COOKIE, $_SERVER, $_ENV, $_FILES, isset( $_SESSION ) && is_array( $_SESSION ) ? $_SESSION : array() );
-	foreach ( $input as $k => $v )
-		if ( !in_array( $k, $no_unset ) && isset( $GLOBALS[$k] ) ) {
-			unset( $GLOBALS[$k] );
-		}
-}
-
-/**
-=======
->>>>>>> 2b5b39931c (Bootstrap/Load: Deprecate `wp_unregister_GLOBALS()`.)
  * Fix `$_SERVER` variables for various setups.
  *
  * @since WP-3.0.0
