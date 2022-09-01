efe7bd406f49d08925ae6a6c9004c4878b7e6f56
Gary Pendergast <pento@git.wordpress.org>
2022-09-01 13:09:54 +0100

    WP-r45601: Coding Standards: Fix all `WordPress.CodeAnalysis.AssignmentInCondition` issues.
    `WordPress.CodeAnalysis.AssignmentInCondition.FoundInWhileCondition` can be ignored, as this is allowed in Core.
    
    See https://core.trac.wordpress.org/ticket/47632.
    
    Conflicts:
    - phpcs.xml.dist
    
    ---
    
    Merges https://core.trac.wordpress.org/changeset/45601 / WordPress/wordpress-develop@027c9dae90 to ClassicPress.

--- src/wp-includes/revision.php
+++ src/wp-includes/revision.php
@@ -157,7 +157,7 @@ function wp_save_post_revision( $post_id ) {
 		 * @param WP_Post $post              The post object.
 		 *
 		 */
-		if ( isset( $last_revision ) && apply_filters( 'wp_save_post_revision_check_for_changes', $check_for_changes = true, $last_revision, $post ) ) {
+		if ( isset( $last_revision ) && apply_filters( 'wp_save_post_revision_check_for_changes', true, $last_revision, $post ) ) {
 			$post_has_changed = false;
 
 			foreach ( array_keys( _wp_post_revision_fields( $post ) ) as $field ) {
