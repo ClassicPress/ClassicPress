b3c867426009ab0527e0bd0630e31e0a3f2bbc46
mattyrob <mattyrobuk@gmail.com>
2022-09-03 15:42:49 +0100

    Fix @deprecated version number

--- src/wp-includes/post-template.php
+++ src/wp-includes/post-template.php
@@ -1038,7 +1038,7 @@ function post_custom( $key = '' ) {
  *
  * @since WP-1.2.0
  *
- * @deprecated 6.0.2 Use get_post_meta() to retrieve post meta and render manually.
+ * @deprecated WP-6.0.2 Use get_post_meta() to retrieve post meta and render manually.
  *
  */
 function the_meta() {
