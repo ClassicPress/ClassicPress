4e02ee2a23b9b2b4699e497e9ae80c01a5a55300
mattyrob <mattyrobuk@gmail.com>
2022-10-15 18:29:35 +0100

    Fix merge conflicts in 47548

--- tests/phpunit/tests/theme/support.php
+++ tests/phpunit/tests/theme/support.php
@@ -74,18 +74,6 @@ class Tests_Theme_Support extends WP_UnitTestCase {
 		$this->assertFalse( current_theme_supports( 'post-thumbnails' ) );
 	}
 
-<<<<<<< HEAD
-	public function test_post_thumbnails_types_true() {
-		// array of arguments, with the key of 'types' holding the post types.
-		add_theme_support( 'post-thumbnails', array( 'types' => true ) );
-		$this->assertTrue( current_theme_supports( 'post-thumbnails' ) );
-		$this->assertTrue( current_theme_supports( 'post-thumbnails', rand_str() ) ); // any type
-		remove_theme_support( 'post-thumbnails' );
-		$this->assertFalse( current_theme_supports( 'post-thumbnails' ) );
-	}
-
-=======
->>>>>>> d8f5ce0ae5 (Tests: Remove `test_post_thumbnails_types_true()`.)
 	/**
 	 * @see https://core.trac.wordpress.org/ticket/24932
 	 */
