994b7009c5f65316653257dc9f59472971c65380
Jake Spurlock <whyisjake@git.wordpress.org>
2022-10-16 17:04:50 +0100

    WP-r48055: Cache API: Add `wp_cache_get_multiple()` to core functions.
    * `update_object_term_cache`
    * `update_meta_cache`
    * `_get_non_cached_ids`
    
    See https://core.trac.wordpress.org/changeset/47938.
    
    Fixes https://core.trac.wordpress.org/ticket/50352.
    
    WP:Props spacedmonkey, tillkruss, lukecavanagh.
    
    ---
    
    Merges https://core.trac.wordpress.org/changeset/48055 / WordPress/wordpress-develop@def933ced7 to ClassicPress.

--- src/wp-includes/functions.php
+++ src/wp-includes/functions.php
@@ -5658,9 +5658,10 @@ function wp_debug_backtrace_summary( $ignore_class = null, $skip_frames = 0, $pr
  */
 function _get_non_cached_ids( $object_ids, $cache_key ) {
 	$clean = array();
-	foreach ( $object_ids as $id ) {
+	$cache_values = wp_cache_get_multiple( $object_ids, $cache_key );
+	foreach ( $cache_values as $id => $cache_value ) {
 		$id = (int) $id;
-		if ( ! wp_cache_get( $id, $cache_key ) ) {
+		if ( ! $cache_value ) {
 			$clean[] = $id;
 		}
 	}
