ed1bae7d904f6d63b87b713b447bbc0ff1e41b17
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2022-10-19 18:51:42 +0100

    WP-r54569: Grouped backports to the 4.9 branch.
    - Posts, Post types: Apply KSES to post-by-email content,
    - General: Validate host on "Are you sure?" screen,
    - Posts, Post types: Remove emails from post-by-email logs,
    - Media: Refactor search by filename within the admin,
    - Pings/trackbacks: Apply KSES to all trackbacks,
    - Comments: Apply kses when editing comments,
    - Customize: Escape blogname option in underscores templates,
    - REST API: Lockdown post parameter of the terms endpoint,
    - Mail: Reset PHPMailer properties between use,
    - Query: Validate relation in `WP_Date_Query`,
    - Widgets: Escape RSS error messages for display.
    
    Merges https://core.trac.wordpress.org/changeset/54521, https://core.trac.wordpress.org/changeset/54522, https://core.trac.wordpress.org/changeset/54523, https://core.trac.wordpress.org/changeset/54524, https://core.trac.wordpress.org/changeset/54525, https://core.trac.wordpress.org/changeset/54526, https://core.trac.wordpress.org/changeset/54527, https://core.trac.wordpress.org/changeset/54528, https://core.trac.wordpress.org/changeset/54529, https://core.trac.wordpress.org/changeset/54530, https://core.trac.wordpress.org/changeset/54541 to the 4.9 branch.
    WP:Props voldemortensen, johnbillion, paulkevan, peterwilsoncc, xknown, dd32, audrasjb, martinkrcho, vortfu, davidbaumwald, tykoted, timothyblynjacobs, johnjamesjacoby, ehtis, matveb, talldanwp.
    
    Conflicts:
    - src/wp-includes/deprecated.php
    - src/wp-includes/post.php
    
    ---
    
    Merges https://core.trac.wordpress.org/changeset/54569 / WordPress/wordpress-develop@1167cf4f38 to ClassicPress.

--- src/wp-includes/deprecated.php
+++ src/wp-includes/deprecated.php
@@ -3941,6 +3941,7 @@ function wp_ajax_press_this_add_category() {
 }
 
 /**
+<<<<<<< HEAD
  * Turn register globals off.
  *
  * @since WP-2.1.0
@@ -3951,3 +3952,21 @@ function wp_unregister_GLOBALS() {  // phpcs:ignore WordPress.NamingConventions.
 	// register_globals was deprecated in PHP 5.3 and removed entirely in PHP 5.4.
 	_deprecated_function( __FUNCTION__, 'WP-5.5.0' );
 }
+=======
+ * Filter the SQL clauses of an attachment query to include filenames.
+ *
+ * @since 4.7.0
+ * @deprecated 6.0.3
+ * @access private
+ *
+ * @param array $clauses An array including WHERE, GROUP BY, JOIN, ORDER BY,
+ *                       DISTINCT, fields (SELECT), and LIMITS clauses.
+ * @return array The unmodified clauses.
+ */
+function _filter_query_attachment_filenames( $clauses ) {
+	_deprecated_function( __FUNCTION__, '6.0.3', 'add_filter( "wp_allow_query_attachment_by_filename", "__return_true" )');
+	remove_filter( 'posts_clauses', __FUNCTION__ );
+	return $clauses;
+}
+
+>>>>>>> 1167cf4f38 (Grouped backports to the 4.9 branch.)
