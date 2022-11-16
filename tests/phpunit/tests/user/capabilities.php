f8539ff99fbc27c9cb24554ebcc0f8599b7b1be9
Laurence Bahiirwa <laurence@mediauganda.com>
2022-11-16 09:48:13 +0300

    Fix Merge conflicts

--- tests/phpunit/tests/user/capabilities.php
+++ tests/phpunit/tests/user/capabilities.php
@@ -236,13 +236,10 @@ class Tests_User_Capabilities extends WP_UnitTestCase {
 			'install_languages'           => array( 'administrator' ),
 			'update_languages'            => array( 'administrator' ),
 			'deactivate_plugins'          => array( 'administrator' ),
-<<<<<<< HEAD
 			'export_others_personal_data' => array( 'administrator' ),
 			'erase_others_personal_data'  => array( 'administrator' ),
 			'manage_privacy_options'      => array( 'administrator' ),
-=======
-			'upgrade_php'            => array( 'administrator' ),
->>>>>>> 5f56921131 (General: Introduce dashboard widget to inform administrators about outdated PHP versions.)
+			'upgrade_php'                 => array( 'administrator' ),
 
 			'edit_categories'             => array( 'administrator', 'editor' ),
 			'delete_categories'           => array( 'administrator', 'editor' ),
@@ -274,13 +271,10 @@ class Tests_User_Capabilities extends WP_UnitTestCase {
 			'install_languages'           => array(),
 			'update_languages'            => array(),
 			'deactivate_plugins'          => array(),
-<<<<<<< HEAD
 			'export_others_personal_data' => array( '' ),
 			'erase_others_personal_data'  => array( '' ),
 			'manage_privacy_options'      => array(),
-=======
-			'upgrade_php'            => array(),
->>>>>>> 5f56921131 (General: Introduce dashboard widget to inform administrators about outdated PHP versions.)
+			'upgrade_php'                 => array(),
 
 			'customize'                   => array( 'administrator' ),
 			'delete_site'                 => array( 'administrator' ),
