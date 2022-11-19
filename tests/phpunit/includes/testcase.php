35636dd525f3b2b9ae87d1a43bf6c6b5cdb7196f
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2022-11-19 19:03:00 +0000

    WP-r48952: Tests: Add a polyfill for `assertEqualsWithDelta()` to `WP_UnitTestCase` and use it where appropriate.
    `assertEqualsWithDelta()` was added in PHPUnit 7.5, while WordPress still supports PHPUnit 5.4.x as the minimum version.
    
    See https://core.trac.wordpress.org/ticket/38266.
    
    Conflicts:
    - tests/phpunit/includes/phpunit7/testcase.php
    - tests/phpunit/includes/testcase.php
    - tests/phpunit/tests/date/currentTime.php
    - tests/phpunit/tests/date/dateI18n.php
    - tests/phpunit/tests/date/getFeedBuildDate.php
    - tests/phpunit/tests/multisite/site.php
    - tests/phpunit/tests/post.php
    - tests/phpunit/tests/rest-api/rest-posts-controller.php
    
    ---
    
    Merges https://core.trac.wordpress.org/changeset/48952 / WordPress/wordpress-develop@5bad67bccf to ClassicPress.

--- tests/phpunit/includes/testcase.php
+++ tests/phpunit/includes/testcase.php
@@ -5,4 +5,49 @@
  * All ClassicPress unit tests should inherit from this class.
  */
 
+<<<<<<< HEAD
 abstract class WP_UnitTestCase extends WP_UnitTestCase_Base {}
+=======
+	/**
+	 * Asserts that a condition is not false.
+	 *
+	 * This method has been backported from a more recent PHPUnit version,
+	 * as tests running on PHP 5.2 use PHPUnit 3.6.x.
+	 *
+	 * @since 4.7.4
+	 *
+	 * @param bool   $condition Condition to check.
+	 * @param string $message   Optional. Message to display when the assertion fails.
+	 *
+	 * @throws PHPUnit_Framework_AssertionFailedError
+	 */
+	public static function assertNotFalse( $condition, $message = '' ) {
+		self::assertThat( $condition, self::logicalNot( self::isFalse() ), $message );
+	}
+
+	/**
+	 * Asserts that two variables are equal (with delta).
+	 *
+	 * This method has been backported from a more recent PHPUnit version,
+	 * as tests running on PHP 5.6 use PHPUnit 5.7.x.
+	 *
+	 * @since 5.6.0
+	 *
+	 * @param mixed  $expected First value to compare.
+	 * @param mixed  $actual   Second value to compare.
+	 * @param float  $delta    Allowed numerical distance between two values to consider them equal.
+	 * @param string $message  Optional. Message to display when the assertion fails.
+	 *
+	 * @throws ExpectationFailedException
+	 * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
+	 */
+	public static function assertEqualsWithDelta( $expected, $actual, $delta, $message = '' ) {
+		$constraint = new PHPUnit_Framework_Constraint_IsEqual(
+			$expected,
+			$delta
+		);
+
+		static::assertThat( $actual, $constraint, $message );
+	}
+}
+>>>>>>> 5bad67bccf (Tests: Add a polyfill for `assertEqualsWithDelta()` to `WP_UnitTestCase` and use it where appropriate.)
--- tests/phpunit/tests/date/dateI18n.php
+++ tests/phpunit/tests/date/dateI18n.php
@@ -16,11 +16,15 @@ class Tests_Date_I18n extends WP_UnitTestCase {
 		$datetime     = new DateTime( 'now', new DateTimeZone( $timezone ) );
 		$wp_timestamp = $datetime->getTimestamp() + $datetime->getOffset();
 
-		$this->assertEquals( $wp_timestamp, date_i18n( 'U', 'invalid' ), '', 5 );
+		$this->assertEqualsWithDelta( $wp_timestamp, date_i18n( 'U', 'invalid' ), 5, 'The dates should be equal' );
 	}
 
 	public function test_should_format_date() {
+<<<<<<< HEAD
 		$this->assertEqualsWithDelta( strtotime( date( 'Y-m-d H:i:s' ) ), strtotime( date_i18n( 'Y-m-d H:i:s' ) ), 2, 'The dates should be equal' );
+=======
+		$this->assertEqualsWithDelta( strtotime( gmdate( 'Y-m-d H:i:s' ) ), strtotime( date_i18n( 'Y-m-d H:i:s' ) ), 2, 'The dates should be equal' );
+>>>>>>> 5bad67bccf (Tests: Add a polyfill for `assertEqualsWithDelta()` to `WP_UnitTestCase` and use it where appropriate.)
 	}
 
 	public function test_should_use_custom_timestamp() {
@@ -40,7 +44,11 @@ class Tests_Date_I18n extends WP_UnitTestCase {
 	public function test_date_should_be_in_gmt_with_custom_timezone_setting() {
 		update_option( 'timezone_string', 'America/Regina' );
 
+<<<<<<< HEAD
 		$this->assertEqualsWithDelta( strtotime( date( 'Y-m-d H:i:s', time() + get_option( 'gmt_offset' ) * HOUR_IN_SECONDS ) ), strtotime( date_i18n( 'Y-m-d H:i:s' ) ), 2, 'The dates should be equal' );
+=======
+		$this->assertEqualsWithDelta( strtotime( gmdate( DATE_RFC3339 ) ), strtotime( date_i18n( DATE_RFC3339, false, true ) ), 2, 'The dates should be equal' );
+>>>>>>> 5bad67bccf (Tests: Add a polyfill for `assertEqualsWithDelta()` to `WP_UnitTestCase` and use it where appropriate.)
 	}
 
 	public function test_date_should_be_in_gmt_with_custom_timezone_setting_and_timestamp() {
@@ -103,8 +111,13 @@ class Tests_Date_I18n extends WP_UnitTestCase {
 	public function test_date_i18n_handles_shorthand_formats( $short, $full ) {
 		update_option( 'timezone_string', 'America/Regina' );
 
+<<<<<<< HEAD
 		$this->assertEquals( strtotime( date_i18n( $full ) ), strtotime( date_i18n( $short ) ), 'The dates should be equal', 2 );
 		$this->assertEquals( $short, date_i18n( '\\' . $short ) );
+=======
+		$this->assertEqualsWithDelta( strtotime( date_i18n( $full ) ), strtotime( date_i18n( $short ) ), 2, 'The dates should be equal' );
+		$this->assertSame( $short, date_i18n( '\\' . $short ) );
+>>>>>>> 5bad67bccf (Tests: Add a polyfill for `assertEqualsWithDelta()` to `WP_UnitTestCase` and use it where appropriate.)
 	}
 
 	public function data_formats() {
@@ -130,9 +143,15 @@ class Tests_Date_I18n extends WP_UnitTestCase {
 		$timestamp    = $datetime->getTimestamp();
 		$wp_timestamp = $timestamp + $datetime->getOffset();
 
+<<<<<<< HEAD
 		$this->assertEquals( $wp_timestamp, date_i18n( 'U' ), 'The dates should be equal', 2 );
 		$this->assertEquals( $timestamp, date_i18n( 'U', false, true ), 'The dates should be equal', 2 );
 		$this->assertEquals( $wp_timestamp, date_i18n( 'U', $wp_timestamp ) );
+=======
+		$this->assertEqualsWithDelta( $wp_timestamp, date_i18n( 'U' ), 2, 'The dates should be equal' );
+		$this->assertEqualsWithDelta( $timestamp, date_i18n( 'U', false, true ), 2, 'The dates should be equal' );
+		$this->assertSame( $wp_timestamp, date_i18n( 'U', $wp_timestamp ) );
+>>>>>>> 5bad67bccf (Tests: Add a polyfill for `assertEqualsWithDelta()` to `WP_UnitTestCase` and use it where appropriate.)
 	}
 
 	/**
