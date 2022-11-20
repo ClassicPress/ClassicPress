c4af3e05a6a2364da8cddab2bedc29aa8ffc732f
mattyrob <mattyrobuk@gmail.com>
2022-11-20 11:35:33 +0000

    assertEqualsWithDelta is polyfilled

--- tests/phpunit/includes/testcase.php
+++ tests/phpunit/includes/testcase.php
@@ -5,29 +5,4 @@
  * All ClassicPress unit tests should inherit from this class.
  */
 
-abstract class WP_UnitTestCase extends WP_UnitTestCase_Base {
-	/**
-	 * Asserts that two variables are equal (with delta).
-	 *
-	 * This method has been backported from a more recent PHPUnit version,
-	 * as tests running on PHP 5.6 use PHPUnit 5.7.x.
-	 *
-	 * @since WP-5.6.0
-	 *
-	 * @param mixed  $expected First value to compare.
-	 * @param mixed  $actual   Second value to compare.
-	 * @param float  $delta    Allowed numerical distance between two values to consider them equal.
-	 * @param string $message  Optional. Message to display when the assertion fails.
-	 *
-	 * @throws ExpectationFailedException
-	 * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
-	 */
-	public static function assertEqualsWithDelta( $expected, $actual, $delta, $message = '' ): void {
-		$constraint = new PHPUnit_Framework_Constraint_IsEqual(
-			$expected,
-			$delta
-		);
-
-		static::assertThat( $actual, $constraint, $message );
-	}
-}
+abstract class WP_UnitTestCase extends WP_UnitTestCase_Base {}
