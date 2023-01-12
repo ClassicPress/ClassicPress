bbcb1a7e7c01dd78348a4c04574ca14f8297a683
Matt Robinson <mattyrobuk@gmail.com>
2023-01-12 16:52:39 +0000

    Introduce polyfill for str_contains() and other functions (#1212)
    * WP-r52039: General: Introduce polyfill for `str_contains()` added in PHP 8.0.
    
    PHP 8.0 introduced a new function: `str_contains()`. It performs a case-sensitive check indicating if given substring (needle) is contained in the string to search in (haystack).
    
    This polyfill makes this function available for use in Core.
    
    Ref:
    * PHP RFC https://wiki.php.net/rfc/str_contains
    * PHP manual https://www.php.net/manual/en/function.str-contains.php
    
    WP:Props ayeshrajans, costdev, desrosj, hellofromTonya, knutsp, pbearne.
    Fixes https://core.trac.wordpress.org/ticket/49652.
    
    Conflicts:
    - src/wp-includes/compat.php
    
    ---
    
    Merges https://core.trac.wordpress.org/changeset/52039 / WordPress/wordpress-develop@3cc8f1237a to ClassicPress.
    
    * Fix merge conflicts in 52039
    
    Co-authored-by: Tonya Mork <hellofromtonya@git.wordpress.org>

--- src/wp-includes/compat.php
+++ src/wp-includes/compat.php
@@ -501,3 +501,73 @@ if ( ! function_exists( 'is_iterable' ) ) {
 		return ( is_array( $var ) || $var instanceof Traversable );
 	}
 }
+
+if ( ! function_exists( 'array_key_first' ) ) {
+	/**
+	 * Polyfill for array_key_first() function added in PHP 7.3.
+	 *
+	 * Get the first key of the given array without affecting
+	 * the internal array pointer.
+	 *
+	 * @since WP-5.9.0
+	 *
+	 * @param array $arr An array.
+	 * @return string|int|null The first key of array if the array
+	 *                         is not empty; `null` otherwise.
+	 */
+	function array_key_first( array $arr ) {
+		foreach ( $arr as $key => $value ) {
+			return $key;
+		}
+	}
+}
+
+if ( ! function_exists( 'array_key_last' ) ) {
+	/**
+	 * Polyfill for `array_key_last()` function added in PHP 7.3.
+	 *
+	 * Get the last key of the given array without affecting the
+	 * internal array pointer.
+	 *
+	 * @since WP-5.9.0
+	 *
+	 * @param array $arr An array.
+	 * @return string|int|null The last key of array if the array
+	 *.                        is not empty; `null` otherwise.
+	 */
+	function array_key_last( array $arr ) {
+		if ( empty( $arr ) ) {
+			return null;
+		}
+		end( $arr );
+		return key( $arr );
+	}
+}
+
+if ( ! function_exists( 'str_contains' ) ) {
+	/**
+	 * Polyfill for `str_contains()` function added in PHP 8.0.
+	 *
+	 * Performs a case-sensitive check indicating if needle is
+	 * contained in haystack.
+	 *
+	 * @since WP-5.9.0
+	 *
+	 * @param string $haystack The string to search in.
+	 * @param string $needle   The substring to search for in the haystack.
+	 * @return bool True if `$needle` is in `$haystack`, otherwise false.
+	 */
+	function str_contains( $haystack, $needle ) {
+		return ( '' === $needle || false !== strpos( $haystack, $needle ) );
+	}
+}
+
+// IMAGETYPE_WEBP constant is only defined in PHP 7.1 or later.
+if ( ! defined( 'IMAGETYPE_WEBP' ) ) {
+	define( 'IMAGETYPE_WEBP', 18 );
+}
+
+// IMG_WEBP constant is only defined in PHP 7.0.10 or later.
+if ( ! defined( 'IMG_WEBP' ) ) {
+	define( 'IMG_WEBP', IMAGETYPE_WEBP ); // phpcs:ignore PHPCompatibility.Constants.NewConstants.imagetype_webpFound
+}
