fba430f32fb383d30e4ecff56eefb0ff32e2a897
Viktor Nagornyy <viktor@projectarmy.net>
2023-03-16 12:11:50 -0400

    Removed petitions widget
    Removed petitions widget.

--- src/wp-admin/includes/ajax-actions.php
+++ src/wp-admin/includes/ajax-actions.php
@@ -358,9 +358,6 @@ function wp_ajax_dashboard_widgets() {
 		case 'dashboard_primary':
 			wp_dashboard_primary();
 			break;
-		case 'dashboard_petitions':
-			cp_dashboard_petitions();
-			break;
 	}
 	wp_die();
 }
