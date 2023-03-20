4c1932a26359354b26950da8f6706f5489dc2bc3
xxsimoxx <simone@gieffeedizioni.it>
2023-03-20 15:35:47 +0100

    Restore Upload Plugin page

--- src/wp-admin/menu.php
+++ src/wp-admin/menu.php
@@ -259,6 +259,7 @@ $submenu['plugins.php'][5] = array( __( 'Installed Plugins' ), 'activate_plugins
 
 if ( ! is_multisite() ) {
 	/* translators: Add new plugin. */
+	$submenu['plugins.php'][7]  = array( _x( 'Upload', 'plugin' ), 'install_plugins', 'plugin-install.php?tab=upload' );
 	$submenu['plugins.php'][10] = array( _x( 'Add New', 'plugin' ), 'install_plugins', 'plugin-install.php' );
 	$submenu['plugins.php'][15] = array( __( 'Plugin File Editor' ), 'edit_plugins', 'plugin-editor.php' );
 }
--- src/wp-admin/plugins.php
+++ src/wp-admin/plugins.php
@@ -727,6 +727,7 @@ echo esc_html( $title );
 if ( ( ! is_multisite() || is_network_admin() ) && current_user_can( 'install_plugins' ) ) {
 	?>
 	<a href="<?php echo esc_url( self_admin_url( 'plugin-install.php' ) ); ?>" class="page-title-action"><?php echo esc_html_x( 'Add New', 'plugin' ); ?></a>
+	<a href="<?php echo self_admin_url( 'plugin-install.php?tab=upload' ); ?>" class="page-title-action"><?php echo esc_html_x( 'Upload', 'plugin' ); ?></a>
 	<?php
 }
 
