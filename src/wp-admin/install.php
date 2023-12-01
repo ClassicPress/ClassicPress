637e6904160202d0a95d3d19851ccc2182226d4b
mattyrob <mattyrobuk@gmail.com>
2023-12-01 09:17:20 +0000

    Fix coding standards

--- src/wp-admin/install.php
+++ src/wp-admin/install.php
@@ -417,35 +417,35 @@ switch ( $step ) {
 			$result = wp_install( $weblog_title, $user_name, $admin_email, $public, '', wp_slash( $admin_password ), $loaded_language );
 			?>
 
-<h1><?php _e( 'Success!' ); ?></h1>
+			<h1><?php _e( 'Success!' ); ?></h1>
 
-<p>
-	<?php
-	printf(
-		/* translators: link to Open Collective donation page */
-		__( 'ClassicPress has been installed. Consider making a donation to support <a href="%s">ClassicPress</a>, please. Thank you, and enjoy!' ),
-		'https://www.classicpress.net/donate/'
-	);
-	?>
-</p>
+			<p>
+				<?php
+				printf(
+					/* translators: link to Open Collective donation page */
+					__( 'ClassicPress has been installed. Consider making a donation to support <a href="%s">ClassicPress</a>, please. Thank you, and enjoy!' ),
+					'https://www.classicpress.net/donate/'
+				);
+				?>
+			</p>
 
-<table class="form-table install-success">
-	<tr>
-		<th><?php _e( 'Username' ); ?></th>
-		<td><?php echo esc_html( sanitize_user( $user_name, true ) ); ?></td>
-	</tr>
-	<tr>
-		<th><?php _e( 'Password' ); ?></th>
-		<td>
-			<?php if ( ! empty( $result['password'] ) && empty( $admin_password_check ) ) : ?>
-				<code><?php echo esc_html( $result['password'] ); ?></code><br>
-			<?php endif; ?>
-			<p><?php echo $result['password_message']; ?></p>
-		</td>
-	</tr>
-</table>
+			<table class="form-table install-success">
+				<tr>
+					<th><?php _e( 'Username' ); ?></th>
+					<td><?php echo esc_html( sanitize_user( $user_name, true ) ); ?></td>
+				</tr>
+				<tr>
+					<th><?php _e( 'Password' ); ?></th>
+					<td>
+						<?php if ( ! empty( $result['password'] ) && empty( $admin_password_check ) ) : ?>
+							<code><?php echo esc_html( $result['password'] ); ?></code><br>
+						<?php endif; ?>
+						<p><?php echo $result['password_message']; ?></p>
+					</td>
+				</tr>
+			</table>
 
-<p class="step"><a href="<?php echo esc_url( wp_login_url() ); ?>" class="button button-primary button-large"><?php _e( 'Log In' ); ?></a></p>
+			<p class="step"><a href="<?php echo esc_url( wp_login_url() ); ?>" class="button button-primary button-large"><?php _e( 'Log In' ); ?></a></p>
 
 			<?php
 		}
