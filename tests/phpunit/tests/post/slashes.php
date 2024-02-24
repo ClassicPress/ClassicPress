bed28aa5a3d07eeea677fda4dbb57ea91f1bed38
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2024-02-24 12:04:26 +0000

    WP-r53557: Code Modernization: Remove dynamic properties in `Tests_*_Slashes`.
    Dynamic (non-explicitly declared) properties are deprecated as of PHP 8.2 and are expected to become a fatal error in PHP 9.0.
    
    In this particular case, the test classes contain a `set_up()` method that sets a group of properties, which are ''used'' by the tests, but the values of these properties are never ''changed'' by the tests.
    
    In other words, setting these properties in the `set_up()` is an unnecessary overhead and the properties should be changed to class constants.
    
    Follow-up to [1041/tests], [1071/tests].
    
    WP:Props jrf.
    See https://core.trac.wordpress.org/ticket/56033.
    
    Conflicts:
    - tests/phpunit/tests/post/slashes.php
    - tests/phpunit/tests/user/slashes.php
    
    ---
    
    Merges https://core.trac.wordpress.org/changeset/53557 / WordPress/wordpress-develop@bafecbeab5 to ClassicPress.

--- tests/phpunit/tests/post/slashes.php
+++ tests/phpunit/tests/post/slashes.php
@@ -6,6 +6,7 @@
  * @ticket 21767
  */
 class Tests_Post_Slashes extends WP_UnitTestCase {
+<<<<<<< HEAD
 	public function set_up() {
 		parent::set_up();
 
@@ -22,6 +23,34 @@ class Tests_Post_Slashes extends WP_UnitTestCase {
 		$this->slash_5 = 'String with 5 slashes \\\\\\\\\\';
 		$this->slash_6 = 'String with 6 slashes \\\\\\\\\\\\';
 		$this->slash_7 = 'String with 7 slashes \\\\\\\\\\\\\\';
+=======
+
+	/*
+	 * It is important to test with both even and odd numbered slashes,
+	 * as KSES does a strip-then-add slashes in some of its function calls.
+	 */
+
+	const SLASH_1 = 'String with 1 slash \\';
+	const SLASH_2 = 'String with 2 slashes \\\\';
+	const SLASH_3 = 'String with 3 slashes \\\\\\';
+	const SLASH_4 = 'String with 4 slashes \\\\\\\\';
+	const SLASH_5 = 'String with 5 slashes \\\\\\\\\\';
+	const SLASH_6 = 'String with 6 slashes \\\\\\\\\\\\';
+	const SLASH_7 = 'String with 7 slashes \\\\\\\\\\\\\\';
+
+	protected static $author_id;
+	protected static $post_id;
+
+	public static function wpSetUpBeforeClass( WP_UnitTest_Factory $factory ) {
+		self::$author_id = $factory->user->create( array( 'role' => 'editor' ) );
+		self::$post_id   = $factory->post->create();
+	}
+
+	public function set_up() {
+		parent::set_up();
+
+		wp_set_current_user( self::$author_id );
+>>>>>>> bafecbeab5 (Code Modernization: Remove dynamic properties in `Tests_*_Slashes`.)
 	}
 
 	/**
@@ -32,32 +61,50 @@ class Tests_Post_Slashes extends WP_UnitTestCase {
 		$id = self::factory()->post->create();
 
 		$_POST               = array();
+<<<<<<< HEAD
 		$_POST['post_ID']    = $id;
 		$_POST['post_title'] = $this->slash_1;
 		$_POST['content']    = $this->slash_5;
 		$_POST['excerpt']    = $this->slash_7;
 		$_POST               = add_magic_quotes( $_POST ); // the edit_post() function will strip slashes
+=======
+		$_POST['post_ID']    = $post_id;
+		$_POST['post_title'] = self::SLASH_1;
+		$_POST['content']    = self::SLASH_5;
+		$_POST['excerpt']    = self::SLASH_7;
+
+		$_POST = add_magic_quotes( $_POST ); // The edit_post() function will strip slashes.
+>>>>>>> bafecbeab5 (Code Modernization: Remove dynamic properties in `Tests_*_Slashes`.)
 
 		$post_id = edit_post();
 		$post    = get_post( $post_id );
 
-		$this->assertSame( $this->slash_1, $post->post_title );
-		$this->assertSame( $this->slash_5, $post->post_content );
-		$this->assertSame( $this->slash_7, $post->post_excerpt );
+		$this->assertSame( self::SLASH_1, $post->post_title );
+		$this->assertSame( self::SLASH_5, $post->post_content );
+		$this->assertSame( self::SLASH_7, $post->post_excerpt );
 
 		$_POST               = array();
+<<<<<<< HEAD
 		$_POST['post_ID']    = $id;
 		$_POST['post_title'] = $this->slash_2;
 		$_POST['content']    = $this->slash_4;
 		$_POST['excerpt']    = $this->slash_6;
 		$_POST               = add_magic_quotes( $_POST );
+=======
+		$_POST['post_ID']    = $post_id;
+		$_POST['post_title'] = self::SLASH_2;
+		$_POST['content']    = self::SLASH_4;
+		$_POST['excerpt']    = self::SLASH_6;
+
+		$_POST = add_magic_quotes( $_POST ); // The edit_post() function will strip slashes.
+>>>>>>> bafecbeab5 (Code Modernization: Remove dynamic properties in `Tests_*_Slashes`.)
 
 		$post_id = edit_post();
 		$post    = get_post( $post_id );
 
-		$this->assertSame( $this->slash_2, $post->post_title );
-		$this->assertSame( $this->slash_4, $post->post_content );
-		$this->assertSame( $this->slash_6, $post->post_excerpt );
+		$this->assertSame( self::SLASH_2, $post->post_title );
+		$this->assertSame( self::SLASH_4, $post->post_content );
+		$this->assertSame( self::SLASH_6, $post->post_excerpt );
 	}
 
 	/**
@@ -68,33 +115,33 @@ class Tests_Post_Slashes extends WP_UnitTestCase {
 		$id   = wp_insert_post(
 			array(
 				'post_status'  => 'publish',
-				'post_title'   => $this->slash_1,
-				'post_content' => $this->slash_3,
-				'post_excerpt' => $this->slash_5,
+				'post_title'   => self::SLASH_1,
+				'post_content' => self::SLASH_3,
+				'post_excerpt' => self::SLASH_5,
 				'post_type'    => 'post',
 				'slashed'      => false,
 			)
 		);
 		$post = get_post( $id );
 
-		$this->assertSame( wp_unslash( $this->slash_1 ), $post->post_title );
-		$this->assertSame( wp_unslash( $this->slash_3 ), $post->post_content );
-		$this->assertSame( wp_unslash( $this->slash_5 ), $post->post_excerpt );
+		$this->assertSame( wp_unslash( self::SLASH_1 ), $post->post_title );
+		$this->assertSame( wp_unslash( self::SLASH_3 ), $post->post_content );
+		$this->assertSame( wp_unslash( self::SLASH_5 ), $post->post_excerpt );
 
 		$id   = wp_insert_post(
 			array(
 				'post_status'  => 'publish',
-				'post_title'   => $this->slash_2,
-				'post_content' => $this->slash_4,
-				'post_excerpt' => $this->slash_6,
+				'post_title'   => self::SLASH_2,
+				'post_content' => self::SLASH_4,
+				'post_excerpt' => self::SLASH_6,
 				'post_type'    => 'post',
 			)
 		);
 		$post = get_post( $id );
 
-		$this->assertSame( wp_unslash( $this->slash_2 ), $post->post_title );
-		$this->assertSame( wp_unslash( $this->slash_4 ), $post->post_content );
-		$this->assertSame( wp_unslash( $this->slash_6 ), $post->post_excerpt );
+		$this->assertSame( wp_unslash( self::SLASH_2 ), $post->post_title );
+		$this->assertSame( wp_unslash( self::SLASH_4 ), $post->post_content );
+		$this->assertSame( wp_unslash( self::SLASH_6 ), $post->post_excerpt );
 	}
 
 	/**
@@ -106,31 +153,45 @@ class Tests_Post_Slashes extends WP_UnitTestCase {
 
 		wp_update_post(
 			array(
+<<<<<<< HEAD
 				'ID'           => $id,
 				'post_title'   => $this->slash_1,
 				'post_content' => $this->slash_3,
 				'post_excerpt' => $this->slash_5,
+=======
+				'ID'           => $post_id,
+				'post_title'   => self::SLASH_1,
+				'post_content' => self::SLASH_3,
+				'post_excerpt' => self::SLASH_5,
+>>>>>>> bafecbeab5 (Code Modernization: Remove dynamic properties in `Tests_*_Slashes`.)
 			)
 		);
 		$post = get_post( $id );
 
-		$this->assertSame( wp_unslash( $this->slash_1 ), $post->post_title );
-		$this->assertSame( wp_unslash( $this->slash_3 ), $post->post_content );
-		$this->assertSame( wp_unslash( $this->slash_5 ), $post->post_excerpt );
+		$this->assertSame( wp_unslash( self::SLASH_1 ), $post->post_title );
+		$this->assertSame( wp_unslash( self::SLASH_3 ), $post->post_content );
+		$this->assertSame( wp_unslash( self::SLASH_5 ), $post->post_excerpt );
 
 		wp_update_post(
 			array(
+<<<<<<< HEAD
 				'ID'           => $id,
 				'post_title'   => $this->slash_2,
 				'post_content' => $this->slash_4,
 				'post_excerpt' => $this->slash_6,
+=======
+				'ID'           => $post_id,
+				'post_title'   => self::SLASH_2,
+				'post_content' => self::SLASH_4,
+				'post_excerpt' => self::SLASH_6,
+>>>>>>> bafecbeab5 (Code Modernization: Remove dynamic properties in `Tests_*_Slashes`.)
 			)
 		);
 		$post = get_post( $id );
 
-		$this->assertSame( wp_unslash( $this->slash_2 ), $post->post_title );
-		$this->assertSame( wp_unslash( $this->slash_4 ), $post->post_content );
-		$this->assertSame( wp_unslash( $this->slash_6 ), $post->post_excerpt );
+		$this->assertSame( wp_unslash( self::SLASH_2 ), $post->post_title );
+		$this->assertSame( wp_unslash( self::SLASH_4 ), $post->post_content );
+		$this->assertSame( wp_unslash( self::SLASH_6 ), $post->post_excerpt );
 	}
 
 	/**
@@ -138,9 +199,9 @@ class Tests_Post_Slashes extends WP_UnitTestCase {
 	 */
 	public function test_wp_trash_untrash() {
 		$post = array(
-			'post_title'   => $this->slash_1,
-			'post_content' => $this->slash_3,
-			'post_excerpt' => $this->slash_5,
+			'post_title'   => self::SLASH_1,
+			'post_content' => self::SLASH_3,
+			'post_excerpt' => self::SLASH_5,
 		);
 		$id   = wp_insert_post( wp_slash( $post ) );
 
@@ -149,17 +210,17 @@ class Tests_Post_Slashes extends WP_UnitTestCase {
 
 		$post = get_post( $id );
 
-		$this->assertSame( $this->slash_1, $post->post_title );
-		$this->assertSame( $this->slash_3, $post->post_content );
-		$this->assertSame( $this->slash_5, $post->post_excerpt );
+		$this->assertSame( self::SLASH_1, $post->post_title );
+		$this->assertSame( self::SLASH_3, $post->post_content );
+		$this->assertSame( self::SLASH_5, $post->post_excerpt );
 
 		$untrashed = wp_untrash_post( $id );
 		$this->assertNotEmpty( $untrashed );
 
 		$post = get_post( $id );
 
-		$this->assertSame( $this->slash_1, $post->post_title );
-		$this->assertSame( $this->slash_3, $post->post_content );
-		$this->assertSame( $this->slash_5, $post->post_excerpt );
+		$this->assertSame( self::SLASH_1, $post->post_title );
+		$this->assertSame( self::SLASH_3, $post->post_content );
+		$this->assertSame( self::SLASH_5, $post->post_excerpt );
 	}
 }
--- tests/phpunit/tests/user/slashes.php
+++ tests/phpunit/tests/user/slashes.php
@@ -148,7 +148,11 @@ class Tests_User_Slashes extends WP_UnitTestCase {
 			array(
 				'user_login'   => 'slash_example_user_3',
 				'role'         => 'subscriber',
+<<<<<<< HEAD
 				'user_email'   => 'user3@example.com',
+=======
+				'email'        => 'user3@example.com',
+>>>>>>> bafecbeab5 (Code Modernization: Remove dynamic properties in `Tests_*_Slashes`.)
 				'first_name'   => self::SLASH_1,
 				'last_name'    => self::SLASH_3,
 				'nickname'     => self::SLASH_5,
@@ -169,7 +173,11 @@ class Tests_User_Slashes extends WP_UnitTestCase {
 			array(
 				'user_login'   => 'slash_example_user_4',
 				'role'         => 'subscriber',
+<<<<<<< HEAD
 				'user_email'   => 'user4@example.com',
+=======
+				'email'        => 'user3@example.com',
+>>>>>>> bafecbeab5 (Code Modernization: Remove dynamic properties in `Tests_*_Slashes`.)
 				'first_name'   => self::SLASH_2,
 				'last_name'    => self::SLASH_4,
 				'nickname'     => self::SLASH_6,
