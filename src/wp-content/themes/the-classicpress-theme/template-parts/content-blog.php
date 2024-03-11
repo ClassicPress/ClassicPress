ba1fab5f55fe2a4d715b270d6ba52d264756a84d
Tim Kaye <KTS915@users.noreply.github.com>
2024-03-11 11:27:57 -0400

    Add files via upload

---
+++ src/wp-content/themes/the-classicpress-theme/template-parts/content-blog.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Susty
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<div class="blog-post">
+
+		<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+
+		<p class="entry-meta">
+			<?php susty_wp_posted_on(); ?>
+			<?php susty_wp_posted_by(); ?>
+			<?php esc_html_e( ' | Category: ', 'susty' ); ?>
+			<?php the_category(', '); ?>
+		</p>
+
+		<div class="excerpt"><p><?php the_excerpt(); ?>
+			<?php if ( get_edit_post_link() ) : ?>
+				<?php
+				edit_post_link(
+					sprintf(
+						wp_kses(
+							/* translators: %s: Name of current post. Only visible to screen readers */
+							__( 'Edit <span class="screen-reader-text">%s</span>', 'susty' ),
+							array(
+								'span' => array(
+									'class' => array(),
+								),
+							)
+						),
+						get_the_title()
+					),
+					' <span class="edit-link">', '</span>'
+				);
+				?>
+			<?php endif; ?>
+		</p>
+		</div>
+
+		<!--a href="<?php //the_permalink(); ?>" class="button button-purple"><?php //esc_html_e( 'Continue Reading', 'susty' ); ?></a--> <?php
+
+		wp_link_pages( array(
+			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'susty' ),
+			'after'  => '</div>',
+		) ); ?>
+
+	</div>	
+	
+</article><!-- #post-<?php the_ID(); ?> -->
---
+++ src/wp-content/themes/the-classicpress-theme/template-parts/content-none.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Template part for displaying a message that posts cannot be found
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Susty
+ */
+
+?>
+
+<section>
+	<header>
+		<h1><?php esc_html_e( 'Nothing Found', 'susty' ); ?></h1>
+	</header><!-- .page-header -->
+
+	<div>
+		<?php
+		if ( is_home() && current_user_can( 'publish_posts' ) ) :
+
+			printf(
+				'<p>' . wp_kses(
+					/* translators: 1: link to WP admin new post page. */
+					__( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'susty' ),
+					array(
+						'a' => array(
+							'href' => array(),
+						),
+					)
+				) . '</p>',
+				esc_url( admin_url( 'post-new.php' ) )
+			);
+
+		elseif ( is_search() ) :
+			?>
+
+			<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'susty' ); ?></p>
+			<?php
+			get_search_form();
+
+		else :
+			?>
+
+			<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'susty' ); ?></p>
+			<?php
+			get_search_form();
+
+		endif;
+		?>
+	</div>
+</section>
