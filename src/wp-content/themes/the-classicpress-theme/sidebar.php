5c845d296d871e1100c06da331476a720ab31b92
Tim Kaye <KTS915@users.noreply.github.com>
2024-03-12 12:58:17 -0400

    Update sidebar.php to reflect new text domain

--- src/wp-content/themes/the-classicpress-theme/sidebar.php
+++ src/wp-content/themes/the-classicpress-theme/sidebar.php
@@ -5,7 +5,7 @@
 <aside id="sidebar" class="sidebar" role="complementary" aria-labelledby="sidebar-header">
 
 	<h2 id="sidebar-header" class="screen-reader-text">
-		<?php echo esc_attr_x( 'Sidebar', 'Sidebar aria label', 'susty' ); ?>
+		<?php echo esc_attr_x( 'Sidebar', 'Sidebar aria label', 'the-classicpress-theme' ); ?>
 	</h2>
 
 	<?php
