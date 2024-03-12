e2f45bbff3da2371e8e5684e8f22349b5b9ef618
Tim Kaye <KTS915@users.noreply.github.com>
2024-03-12 11:18:21 -0400

    Update functions.php to ensure custom logo is the appropriate height

--- src/wp-content/themes/the-classicpress-theme/functions.php
+++ src/wp-content/themes/the-classicpress-theme/functions.php
@@ -63,7 +63,7 @@ if ( ! function_exists( 'susty_setup' ) ) :
 		 * @link https://codex.wordpress.org/Theme_Logo
 		 */
 		add_theme_support( 'custom-logo', array(
-			'height'  => 250,
+			'height'  => 50,
 			'width'   => 250,
 			'flex-width'  => true,
 			'flex-height' => true,
