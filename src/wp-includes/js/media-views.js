ae2df6c880b5b381c3a9386a54e0a8411cbf31fa
Tim Kaye <KTS915@users.noreply.github.com>
2024-03-25 10:30:57 -0400

    Update media-views.js to replace jQuery 'return false' with JS `event.preventDefault();`

--- src/wp-includes/js/media-views.js
+++ src/wp-includes/js/media-views.js
@@ -4461,11 +4461,11 @@
 
 			// Keep tab focus within media modal while it's open.
 			if ( tabbables.at(-1) === event.target && ! event.shiftKey ) {
+				event.preventDefault();
 				tabbables[0].focus();
-				return false;
 			} else if ( tabbables[0] === event.target && event.shiftKey ) {
+				event.preventDefault();
 				tabbables.at(-1).focus();
-				return false;
 			}
 		},
 
