de57d395e584b9a5184726de3bdc21770b80158b
SIMONE FIORAVANTI <simospa@gieffeedizioni.it>
2024-04-11 17:30:21 +0200

    Bump $wp_version from 6.2.3 to 6.2.5 (#1387)
    Co-authored-by: xxsimoxx <simone@gieffeedizioni.it>

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -39,7 +39,7 @@ $cp_version = '2.0.0+dev';
  *
  * @global string $wp_version
  */
-$wp_version = '6.2.3';
+$wp_version = '6.2.5';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
