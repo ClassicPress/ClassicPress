9a803cce0d0c25d31a99941851d5cb3555598d4c
xxsimoxx <simone@gieffeedizioni.it>
2024-04-15 15:48:08 +0200

    Add unit test

--- tests/phpunit/tests/media/getAttachmentTaxonomies.php
+++ tests/phpunit/tests/media/getAttachmentTaxonomies.php
@@ -24,6 +24,25 @@ class Tests_Media_GetAttachmentTaxonomies extends WP_UnitTestCase {
 		$this->assertSame( $expected, $found );
 	}
 
+	public function test_should_return_default_taxonomies() {
+
+		$a          = self::factory()->attachment->create_object(
+			'image.jpg',
+			0,
+			array(
+				'post_mime_type' => 'image/jpeg',
+				'post_type'      => 'attachment',
+			)
+		);
+		$attachment = get_post( $a );
+
+		$found    = get_attachment_taxonomies( $attachment, 'names' );
+		$expected = array( 'media_category', 'media_post_tag' );
+
+		$this->assertSame( $expected, $found );
+	}
+
+
 	public function test_should_return_taxonomy_registered_for_specific_attachment_type() {
 		register_taxonomy( 'wptests_tax', 'attachment:image' );
 
