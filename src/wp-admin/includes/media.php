4736c3be46097c46a5ffc98b2ad2397aa619a7cc
Tim Kaye <KTS915@users.noreply.github.com>
2024-05-04 10:13:44 -0400

    Update media.php to escape translatable strings

--- src/wp-admin/includes/media.php
+++ src/wp-admin/includes/media.php
@@ -3849,9 +3849,9 @@ function cp_select_upload_media_category() {
 		);
 
 		if ( ! empty( $media_terms ) ) {
-			$media_select .= '<label for="upload-category"><strong>' . __( 'Please choose the upload media category' ) . '</strong></label><br>';
+			$media_select .= '<label for="upload-category"><strong>' . esc_html__( 'Please choose the upload media category' ) . '</strong></label><br>';
 			$media_select .= '<select id="upload-category" name="upload-category" class="attachment-filters">';
-			$media_select .= '<option value=""' . selected( in_array( $cat_subfolder, array( '', '/' ) ), true, false ) . '>&nbsp;' . __( 'Media category' ) . '&nbsp;</option>';
+			$media_select .= '<option value=""' . selected( in_array( $cat_subfolder, array( '', '/' ) ), true, false ) . '>&nbsp;' . esc_html__( 'Media category' ) . '&nbsp;</option>';
 			foreach ( $media_terms as $media_term ) {
 				$ancestor_ids = get_ancestors( $media_term->term_id, 'media_category' );
 				$level = $count = count( $ancestor_ids );
