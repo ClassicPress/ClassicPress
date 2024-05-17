3b1db50d6bad6618e663f08b7e625bad11d589de
Tim Kaye <KTS915@users.noreply.github.com>
2024-05-17 09:12:51 -0400

    Update class-wp-media-list-table.php to have separate columns for featured images

--- src/wp-admin/includes/class-wp-media-list-table.php
+++ src/wp-admin/includes/class-wp-media-list-table.php
@@ -182,16 +182,17 @@ class WP_Media_List_Table extends WP_List_Table {
 		if ( MEDIA_TRASH ) {
 			if ( $this->is_trash ) {
 				$actions['untrash'] = __( 'Restore' );
-				$actions['delete']  = __( 'Delete permanently' );
+				$actions['delete'] = __( 'Delete permanently' );
 			} else {
 				$actions['trash'] = __( 'Move to Trash' );
 			}
 		} else {
+			$actions['edit'] = __( 'Edit' );
 			$actions['delete'] = __( 'Delete permanently' );
 		}
 
 		if ( $this->detached ) {
-			$actions['attach'] = __( 'Attach' );
+			$actions['attach'] = __( 'Attach to post' );
 		}
 
 		return $actions;
@@ -356,7 +357,8 @@ class WP_Media_List_Table extends WP_List_Table {
 		}
 
 		/* translators: Column name. */
-		$posts_columns['used_in'] = _x( 'Used In', 'column name' );
+		$posts_columns['thumbnail'] = _x( 'Featured Image', 'column name' );
+		$posts_columns['used_in']   = _x( 'Used In', 'column name' );
 
 		if ( ! $this->detached ) {
 			$posts_columns['parent'] = _x( 'Uploaded to', 'column name' );
@@ -391,12 +393,13 @@ class WP_Media_List_Table extends WP_List_Table {
 	 */
 	protected function get_sortable_columns() {
 		return array(
-			'title'    => 'title',
-			'author'   => 'author',
-			'parent'   => 'parent',
-			'used_in'  => 'used_in',
-			'comments' => 'comment_count',
-			'date'     => array( 'date', true ),
+			'title'     => 'title',
+			'author'    => 'author',
+			'parent'    => 'parent',
+			'thumbnail' => 'thumbnail',
+			'used_in'   => 'used_in',
+			'comments'  => 'comment_count',
+			'date'      => array( 'date', true ),
 		);
 	}
 
@@ -556,6 +559,48 @@ class WP_Media_List_Table extends WP_List_Table {
 	}
 
 	/**
+	 * Handles the thumbnail column output.
+	 *
+	 * @since CP-2.2.0
+	 *
+	 * @param WP_Post $post The current WP_Post object.
+	 */
+	public function column_thumbnail( $post ) {
+		$user_can_edit = current_user_can( 'edit_post', $post->ID );
+
+		// Get all post types except attachments and revisions.
+		$parent_types = get_post_types();
+		unset( $parent_types['attachment'] );
+		unset( $parent_types['revision'] );
+
+		// Set output variable.
+		$output = '';
+
+		foreach ( $parent_types as $parent_type ) {
+
+			// Get all posts where this attachment is the featured image.
+			$relationship_ids = cp_get_object_relationship_ids( $post->ID, 'thumbnail', $parent_type );
+
+			if ( ! empty( $relationship_ids ) ) {
+				foreach ( $relationship_ids as $relationship_id ) {
+					$ancestor          = get_post( $relationship_id );
+					$ancestor_type_obj = get_post_type_object( $ancestor->post_type );
+					$title             = _draft_or_post_title( $relationship_id );
+
+					if ( $ancestor_type_obj->show_ui && current_user_can( 'edit_post', $relationship_id ) ) {
+						$link = '<strong><a href="' . esc_url( get_edit_post_link( $relationship_id ) ) . '">' . esc_html( $title ) . '</a></strong>';
+					} else {
+						$link = $title;
+					}
+
+					$output .= $link . ' ' . get_the_time( __( 'Y/m/d' ) ) . '<br>';
+				}
+			}
+		}
+		return $output;
+	}
+
+	/**
 	 * Handles the used-in column output.
 	 *
 	 * @since CP-2.2.0
@@ -575,9 +620,6 @@ class WP_Media_List_Table extends WP_List_Table {
 
 		foreach ( $parent_types as $parent_type ) {
 
-			// Get all posts where this attachment is the featured image.
-			$relationship_ids = cp_get_object_relationship_ids( $post->ID, 'thumbnail', $parent_type );
-
 			// Get all posts where this attachment is used in the content.
 			$parent_ids = cp_get_object_relationship_ids( $post->ID, 'attachment', $parent_type );
 
@@ -593,37 +635,8 @@ class WP_Media_List_Table extends WP_List_Table {
 							$link = $title;
 						}
 
-						// Check for the thumbnail ID (if any) for this post.
-						if ( in_array( $parent_id, $relationship_ids, true ) ) {
-							$usage_context = __( '(as Featured Image and in content)' );
-
-							// Remove from relationship_ids array to avoid duplicates.
-							$key = array_search( $parent_id, $relationship_ids );
-							unset( $relationship_ids[ $key ] );
-						} else {
-							$usage_context = __( '(in content)' );
-						}
-
-						$output .= $link . ' ' . get_the_time( __( 'Y/m/d' ) ) . ' ' . $usage_context . '<br>';
+						$output .= $link . ' ' . get_the_time( __( 'Y/m/d' ) ) . '<br>';
 					}
-				}
-			}
-
-			if ( ! empty( $relationship_ids ) ) {
-				foreach ( $relationship_ids as $relationship_id ) {
-					$ancestor          = get_post( $relationship_id );
-					$ancestor_type_obj = get_post_type_object( $ancestor->post_type );
-					$title             = _draft_or_post_title( $relationship_id );
-
-					if ( $ancestor_type_obj->show_ui && current_user_can( 'edit_post', $relationship_id ) ) {
-						$link = '<strong><a href="' . esc_url( get_edit_post_link( $relationship_id ) ) . '">' . esc_html( $title ) . '</a></strong>';
-					} else {
-						$link = $title;
-					}
-
-					$usage_context = __( '(as Featured Image)' );
-
-					$output .= $link . ' ' . get_the_time( __( 'Y/m/d' ) ) . ' ' . $usage_context . '<br>';
 				}
 			}
 		}
