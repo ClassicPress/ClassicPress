108f9374096342aa302e6e8d0f9a589f10ba6bbf
mattyrob <mattyrobuk@gmail.com>
2024-07-06 09:55:31 +0100

    Implement $wp_filesystem for menu and `get_pepper()`

--- src/wp-content/plugins/cp-pepper/cp-pepper.php
+++ src/wp-content/plugins/cp-pepper/cp-pepper.php
@@ -84,28 +84,6 @@ class PepperPassword {
 	}
 
 	/**
-	 * Check that pepper file is readable.
-	 *
-	 * @since 1.0.0
-	 *
-	 * @return bool Whether the file that store the pepper is readable
-	 */
-	private function can_read() {
-		return is_readable( $this->pepper_file );
-	}
-
-	/**
-	 * Check that pepper file is writable.
-	 *
-	 * @since 1.0.0
-	 *
-	 * @return bool Whether the file that store the pepper is writable
-	 */
-	private function can_write() {
-		return is_writable( $this->pepper_file );
-	}
-
-	/**
 	 * Save the pepper.
 	 *
 	 * @since 1.0.0
@@ -127,13 +105,33 @@ $current_pepper = \'' . $pepper . '\';
 	 *
 	 * @since 1.0.0
 	 *
-	 * @return string|false Pepper string on success, false on failure.
+	 * @return string Pepper string on success, empty on failure.
 	 */
 	private function get_pepper() {
-		$lines = file( $this->pepper_file );
-		$match = preg_match( '/\$current_pepper = \'([a-zA-Z0-9]*)\';/', $lines[2], $matches );
+		ob_start();
+		if ( false === ( $creds = request_filesystem_credentials( admin_url(), '', false, false, null ) ) ) {
+			return; // Await filesystem access
+		}
+
+		if ( ! WP_Filesystem( $creds ) ) {
+			request_filesystem_credentials( admin_url(), '', true, false, null );
+			return;
+		}
+		ob_end_flush();
+
+		global $wp_filesystem;
+		if ( $wp_filesystem->exists( $this->pepper_file ) ) {
+			$pepper = $wp_filesystem->get_contents( $this->pepper_file );
+		}
+
+		if ( empty( $pepper ) ) {
+			return '';
+		}
+
+		$match = preg_match( '/\$current_pepper = \'([a-zA-Z0-9]*)\';/', $pepper, $matches );
+
 		if ( $match !== 1 ) {
-			return false;
+			return '';
 		}
 		return $matches[1];
 	}
@@ -150,15 +148,26 @@ $current_pepper = \'' . $pepper . '\';
 			wp_die( esc_html__( 'Unauthorized.' ) );
 		};
 
+		if ( false === ( $creds = request_filesystem_credentials( admin_url( 'options-general.php?page=' . self::SLUG ), '', false, false, null ) ) ) {
+			return; // Await filesystem access
+		}
+
+		if ( ! WP_Filesystem( $creds ) ) {
+			request_filesystem_credentials( admin_url( 'options-general.php?page=' . self::SLUG ), '', true, false, null );
+			return;
+		}
+
+		global $wp_filesystem;
+
 		echo '<div class="wrap">';
 		echo '<div class="cp-pepper-general">';
 		echo '<h1>' . esc_html__( 'ClassicPress Pepper' ) . '</h1>';
 
-		if ( $this->can_write() === false ) {
+		if ( false === $wp_filesystem->is_writable( dirname( $this->pepper_file ) ) ) {
 			echo '<div class="notice notice-error is-dismissible">';
 			echo '<p>' . esc_html( 'Error: Cannot write the pepper file.' ) . '</p>';
 			echo '<p><code>' . esc_html( $this->pepper_file ) . '</code></p>';
-			if ( $this->can_read() ) {
+			if ( $wp_filesystem->is_readable( $this->pepper_file ) ) {
 				echo '<p>' . esc_html( 'You can edit the file manually to add or chenge the pepper.' ) . '</p>';
 			}
 			echo '</div></div></div>';
