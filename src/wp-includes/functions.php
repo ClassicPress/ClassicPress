c58566c93286fdedd1017cfd98de56b44ca784b8
SIMONE FIORAVANTI <simospa@gieffeedizioni.it>
2024-08-01 17:28:00 +0200

    Add new functions to check if serving a REST API request (#1499)
    * WP-r57312: Bootstrap/Load: Introduce functions to check whether WordPress is serving a REST API request.
    
    This changeset introduces two functions:
    * `wp_is_serving_rest_request()` returns a boolean for whether WordPress is serving an actual REST API request.
    * `wp_is_rest_endpoint()` returns a boolean for whether a WordPress REST API endpoint is currently being used. While this is always the case if `wp_is_serving_rest_request()` returns `true`, the function additionally covers the scenario of internal REST API requests, i.e. where WordPress calls a REST API endpoint within the same request.
    
    Both functions should only be used after the `parse_request` action.
    
    All relevant manual checks have been adjusted to use one of the new functions, depending on the use-case. They were all using the same constant check so far, while in fact some of them were intending to check for an actual REST API request while others were intending to check for REST endpoint usage.
    
    A new filter `wp_is_rest_endpoint` can be used to alter the return value of the `wp_is_rest_endpoint()` function.
    
    WP:Props lots.0.logs, TimothyBlynJacobs, flixos90, joehoyle, peterwilsoncc, swissspidy, SergeyBiryukov, pento, mikejolley, iandunn, hellofromTonya, Cybr, petitphp.
    Fixes https://core.trac.wordpress.org/ticket/42061.
    
    Conflicts:
    - src/wp-includes/deprecated.php
    - src/wp-includes/functions.php
    - src/wp-includes/load.php
    - src/wp-includes/rest-api/endpoints/class-wp-rest-attachments-controller.php
    - src/wp-includes/rest-api/endpoints/class-wp-rest-url-details-controller.php
    - src/wp-includes/script-loader.php
    
    ---
    
    Merges https://core.trac.wordpress.org/changeset/57312 / WordPress/wordpress-develop@d17afcc9dd to ClassicPress.
    
    * Fix merge conflicts
    
    * Fix AJAX error
    
    ---------
    
    Co-authored-by: Felix Arntz <flixos90@git.wordpress.org>
    Co-authored-by: xxsimoxx <simone@gieffeedizioni.it>

--- src/wp-includes/functions.php
+++ src/wp-includes/functions.php
@@ -3685,7 +3685,7 @@ function wp_die( $message = '', $title = '', $args = array() ) {
 		 * @param callable $callback Callback function name.
 		 */
 		$callback = apply_filters( 'wp_die_json_handler', '_json_wp_die_handler' );
-	} elseif ( defined( 'REST_REQUEST' ) && REST_REQUEST && wp_is_jsonp_request() ) {
+	} elseif ( wp_is_serving_rest_request() && wp_is_jsonp_request() ) {
 		/**
 		 * Filters the callback for killing WordPress execution for JSONP REST requests.
 		 *
@@ -4403,8 +4403,8 @@ function _wp_json_prepare_data( $data ) {
  * @param int   $status_code Optional. The HTTP status code to output. Default null.
  * @param int   $options     Optional. Options to be passed to json_encode(). Default 0.
  */
-function wp_send_json( $response, $status_code = null, $options = 0 ) {
-	if ( defined( 'REST_REQUEST' ) && REST_REQUEST ) {
+function wp_send_json( $response, $status_code = null, $flags = 0 ) {
+	if ( wp_is_serving_rest_request() ) {
 		_doing_it_wrong(
 			__FUNCTION__,
 			sprintf(
@@ -4424,7 +4424,7 @@ function wp_send_json( $response, $status_code = null, $options = 0 ) {
 		}
 	}
 
-	echo wp_json_encode( $response, $options );
+	echo wp_json_encode( $response, $flags );
 
 	if ( wp_doing_ajax() ) {
 		wp_die(
@@ -4675,6 +4675,24 @@ function _mce_set_direction( $mce_init ) {
 function _mce_set_element_format( $mce_init ) {
 	$mce_init['element_format'] = 'html';
 	return $mce_init;
+}
+
+/**
+ * Determines whether WordPress is currently serving a REST API request.
+ *
+ * The function relies on the 'REST_REQUEST' global. As such, it only returns true when an actual REST _request_ is
+ * being made. It does not return true when a REST endpoint is hit as part of another request, e.g. for preloading a
+ * REST response. See {@see wp_is_rest_endpoint()} for that purpose.
+ *
+ * This function should not be called until the {@see 'parse_request'} action, as the constant is only defined then,
+ * even for an actual REST request.
+ *
+ * @since 6.5.0
+ *
+ * @return bool True if it's a WordPress REST API request, false otherwise.
+ */
+function wp_is_serving_rest_request() {
+	return defined( 'REST_REQUEST' ) && REST_REQUEST;
 }
 
 /**
