6471af8fb19c59a861c00b7b826d975c4a79fd03
xxsimoxx <simone@gieffeedizioni.it>
2024-08-01 08:33:50 +0200

    Add polyfills

--- tests/phpunit/tests/compat/wordpress.php
+++ tests/phpunit/tests/compat/wordpress.php
@@ -41,6 +41,8 @@ class Tests_Compat_wordpress extends WP_UnitTestCase {
 		$this->assertTrue( function_exists( 'register_block_pattern_category' ) );
 		$this->assertTrue( function_exists( 'unregister_block_pattern_category' ) );
 		$this->assertTrue( function_exists( 'wp_is_block_theme' ) );
+		$this->assertTrue( function_exists( 'parse_blocks' ) );
+		$this->assertTrue( function_exists( 'get_dynamic_block_names' ) );
 	}
 
 	/**
