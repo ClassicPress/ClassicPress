4f6a7bac969ee4c8ed183e209d7e0b78fbdc6de9
xxsimoxx <simone@gieffeedizioni.it>
2024-08-27 14:40:14 +0200

    Fix actions and filters

--- src/wp-includes/classicpress/class-cp-customization-frontend.php
+++ src/wp-includes/classicpress/class-cp-customization-frontend.php
@@ -23,8 +23,9 @@ class CP_Customization_Frontend {
 		remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
 		remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
 		remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
-		remove_filter( 'admin_print_scripts', 'print_emoji_detection_script' );
-		remove_action( 'wp_head', 'print_emoji_detection_script' );
+		remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+		remove_action( 'embed_head', 'print_emoji_detection_script' );
+		remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
 		remove_action( 'wp_print_styles', 'print_emoji_styles' );
 		remove_action( 'admin_print_styles', 'print_emoji_styles' );
 	}
