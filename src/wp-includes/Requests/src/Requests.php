550a427ec4a2335dc244955ce3b7d9a5bf3022c5
Elisabetta Carrara <elisabetta.marina.clelia@gmail.com>
2024-10-18 11:14:07 +0200

    Fixed library version conflict

--- src/wp-includes/Requests/src/Requests.php
+++ src/wp-includes/Requests/src/Requests.php
@@ -148,11 +148,7 @@ class Requests {
 	 *
 	 * @var string
 	 */
-<<<<<<< HEAD
-	const VERSION = '2.0.5';
-=======
 	const VERSION = '2.0.9';
->>>>>>> 2130a90861 (External Libraries: Update Requests to `2.0.9`.)
 
 	/**
 	 * Selected transport name
