0ede1e1d3baef4d22019fbc8a0f55a383087fa70
William Patton <will@pattonwebz.com>
2024-11-03 15:12:10 +0000

    Add a test to validate that passwords for user do not get swapped when other valid passwords are passed

--- tests/phpunit/tests/pluggable/wpCheckPassword.php
+++ tests/phpunit/tests/pluggable/wpCheckPassword.php
@@ -40,4 +40,35 @@ class Tests_Pluggable_wpCheckPassword extends WP_UnitTestCase {
 		$this->assertTrue( wp_check_password( $valid_password, $hash ) );
 		$this->assertTrue( wp_check_password( $valid_password, $hash ) );
 	}
+
+	public function test_wp_check_password_does_not_reset_application_password() {
+		$valid_password = 'validpassword';
+		$hash           = wp_hash_password( $valid_password );
+
+		// create a user and set their normal password and an application
+		// password
+		$user_id = $this->factory()->user->create(
+			array(
+				'user_pass' => $hash,
+			)
+		);
+
+		// check the user password
+		$this->assertTrue( wp_check_password( $valid_password, $hash, $user_id ) );
+
+		// verify user password hash matches the one we set
+		$user = get_user_by( 'id', $user_id );
+		$this->assertSame( $hash, $user->user_pass );
+
+		// create an application password for the user
+		$app_pass      = WP_Application_Passwords::create_new_application_password( $user_id, array( 'name' => 'testapp' ) );
+		$app_pass_hash = wp_hash_password( $app_pass[0] );
+
+		// check the application password
+		$this->assertTrue( wp_check_password( $app_pass[0], $app_pass_hash, $user_id ) );
+
+		// check if the user password hash has not changed from the one we set
+		$user = get_user_by( 'id', $user_id );
+		$this->assertSame( $hash, $user->user_pass );
+	}
 }
