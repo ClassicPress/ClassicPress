191ec0843aed36cef3e1609bd5d30ec983818adf
mattyrob <mattyrobuk@gmail.com>
2024-11-14 19:33:38 +0000

    Improve and extend test

--- tests/phpunit/tests/comment/cpWalker.php
+++ tests/phpunit/tests/comment/cpWalker.php
@@ -24,7 +24,7 @@ class Tests_Comment_Walker_Functions extends WP_UnitTestCase {
 	 * @see https://github.com/ClassicPress/ClassicPress/issues/1645
 	 */
 	public function test_comment_wrapper_of_html5_comment() {
-		$comment = self::factory()->comment->create_and_get(
+		$comment_1 = self::factory()->comment->create_and_get(
 			array(
 				'comment_post_ID'  => $this->post_id,
 				'comment_author'   => 'A ClassicPress Commenter',
@@ -33,8 +33,20 @@ class Tests_Comment_Walker_Functions extends WP_UnitTestCase {
 			)
 		);
 
+		$comment_2 = self::factory()->comment->create_and_get(
+			array(
+				'comment_post_ID'  => $this->post_id,
+				'comment_author'   => 'A Different ClassicPress Commenter',
+				'comment_content'  => 'Hi, this is another comment used for testing!',
+				'comment_approved' => 1,
+			)
+		);
+
 		$comment_walker = new Walker_Comment();
 		$test_comment = new Walker_Comment_Helper();
+
+		$this->assertTrue( method_exists( 'Walker_Comment', 'comment' ) );
+		$this->assertTrue( method_exists( 'Walker_Comment', 'html5_comment' ) );
 
 		$args = array(
 			'style'       => 'div',
@@ -42,7 +54,15 @@ class Tests_Comment_Walker_Functions extends WP_UnitTestCase {
 			'max_depth'   => 1,
 		);
 
-		$this->assertSame( $test_comment->exposed_comment( $comment, 1, $args ), $test_comment->exposed_html5_comment( $comment, 1, $args ) );
+		// Render 2 comments using HTML5 function
+		$html5_comment  = $test_comment->exposed_html5_comment( $comment_1, 1, $args );
+		$html5_comment .= $test_comment->exposed_html5_comment( $comment_2, 1, $args );
+
+		// Render 2 comments using wrapper on the original function name
+		$original_comment  = $test_comment->exposed_comment( $comment_1, 1, $args );
+		$original_comment .= $test_comment->exposed_comment( $comment_2, 1, $args );
+
+		$this->assertSame( $original_comment, $html5_comment );
 	}
 }
 
@@ -52,9 +72,8 @@ class Walker_Comment_Helper extends Walker_Comment {
 	 * Expose parent protected `comment()` function for testing
 	 */
 	public function exposed_comment( $comment, $args, $depth ) {
-		// use output buffer to capture rendered comment, render twice to account for odd/even rendering
+		// use output buffer to capture rendered comment.
 		ob_start();
-		$this->comment( $comment, $args, $depth );
 		$this->comment( $comment, $args, $depth );
 		return ob_get_clean();
 	}
@@ -63,9 +82,8 @@ class Walker_Comment_Helper extends Walker_Comment {
 	 * Expose parent protected `html5_comment()` function for testing
 	 */
 	public function exposed_html5_comment( $comment, $args, $depth ) {
-		// use output buffer to capture rendered comment, render twice to account for odd/even rendering
+		// use output buffer to capture rendered comment.
 		ob_start();
-		$this->html5_comment( $comment, $args, $depth );
 		$this->html5_comment( $comment, $args, $depth );
 		return ob_get_clean();
 	}
