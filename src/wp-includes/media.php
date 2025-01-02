186692f87a76eb24391f6b0279c7a8b8aef8b71f
Matt Robinson <mattyrobuk@gmail.com>
2025-01-02 19:12:57 +0000

    Add Support for AVIF image format (#1656)
    * WP-r57524: Media: enable AVIF support.
    
    Add support for uploading, editing and saving AVIF images when supported by the server.
    
    Add 'image/avif' to supported mime types. Correctly identify AVIF images and sizes even when PHP doesn't support AVIF. Resize uploaded AVIF files (when supported) and use for front end markup.
    
    WP:Props adamsilverstein, lukefiretoss, ayeshrajans, navjotjsingh, Tyrannous, jb510, gregbenz, nickpagz, JavierCasares, mukesh27, yguyon, swissspidy.
    Fixes https://core.trac.wordpress.org/ticket/51228.
    
    ---
    
    Merges https://core.trac.wordpress.org/changeset/57524 / WordPress/wordpress-develop@575ce15b5d to ClassicPress.
    
    * WP-r57525: Media: fix AVIF tests.
    
    Follow up to r57524. Properly add AVIF images for unit tests.
    
    Fixes https://core.trac.wordpress.org/ticket/51228.
    
    ---
    
    Merges https://core.trac.wordpress.org/changeset/57525 / WordPress/wordpress-develop@7e34c4b7d3 to ClassicPress.
    
    * WP-r58400: Networks and sites: use get_allowed_mime_types to populate multisite’s upload_filetypes.
    
    Ensure new multisite installs are up to date with the current mime types supported in core.
    
    Note that this will only affect newly created networks, since this is only used to populate the schema for new networks, not change the allowed mime types for existing networks
    
    WP:Props spacedmonkey, costdev, pavanpatil1, joemcgill, rajinsharwar, tb1909.
    Fixes https://core.trac.wordpress.org/ticket/53167.
    
    ---
    
    Merges https://core.trac.wordpress.org/changeset/58400 / WordPress/wordpress-develop@7f70d68353 to ClassicPress.
    
    * `grunt precommit` changes
    
    * Allow webp and avif upload even if not server editable
    
    * Improve UX for uneditable image types
    
    * Update manifest file for build step
    
    ---------
    
    Co-authored-by: Adam Silverstein <adamsilverstein@git.wordpress.org>
    
    CP:Props mattyrob, xxsimoxx, KTS915

--- src/wp-includes/media.php
+++ src/wp-includes/media.php
@@ -4043,6 +4043,7 @@ function _wp_image_editor_choose( $args = array() ) {
 	require_once ABSPATH . WPINC . '/class-wp-image-editor.php';
 	require_once ABSPATH . WPINC . '/class-wp-image-editor-gd.php';
 	require_once ABSPATH . WPINC . '/class-wp-image-editor-imagick.php';
+	require_once ABSPATH . WPINC . '/class-avif-info.php';
 	/**
 	 * Filters the list of image editing library classes.
 	 *
@@ -4142,6 +4143,11 @@ function wp_plupload_default_settings() {
 	// Check if WebP images can be edited.
 	if ( ! wp_image_editor_supports( array( 'mime_type' => 'image/webp' ) ) ) {
 		$defaults['webp_upload_error'] = true;
+	}
+
+	// Check if AVIF images can be edited.
+	if ( ! wp_image_editor_supports( array( 'mime_type' => 'image/avif' ) ) ) {
+		$defaults['avif_upload_error'] = true;
 	}
 
 	/**
@@ -4833,6 +4839,11 @@ function wp_enqueue_media( $args = array() ) {
 		// Headings.
 		'filterAttachments'           => __( 'Filter media' ),
 		'attachmentsList'             => __( 'Media list' ),
+
+		// Server editable image data
+		'webp_editable'    => wp_image_editor_supports( array( 'mime_type' => 'image/webp' ) ),
+		'avif_editable'    => wp_image_editor_supports( array( 'mime_type' => 'image/avif' ) ),
+		'heic_editable'    => wp_image_editor_supports( array( 'mime_type' => 'image/heic' ) ),
 	);
 
 	/**
@@ -5482,6 +5493,7 @@ function wp_show_heic_upload_error( $plupload_settings ) {
  *
  * @since 5.7.0
  * @since 5.8.0 Added support for WebP images.
+ * @since 6.5.0 Added support for AVIF images.
  *
  * @param string $filename   The file path.
  * @param array  $image_info Optional. Extended image information (passed by reference).
@@ -5516,7 +5528,11 @@ function wp_getimagesize( $filename, ?array &$image_info = null ) {
 		}
 	}
 
-	if ( false !== $info ) {
+	if (
+		! empty( $info ) &&
+		// Some PHP versions return 0x0 sizes from `getimagesize` for unrecognized image formats, including AVIFs.
+		! ( empty( $info[0] ) && empty( $info[1] ) )
+	) {
 		return $info;
 	}
 
@@ -5543,8 +5559,73 @@ function wp_getimagesize( $filename, ?array &$image_info = null ) {
 		}
 	}
 
+	// For PHP versions that don't support AVIF images, extract the image size info from the file headers.
+	if ( 'image/avif' === wp_get_image_mime( $filename ) ) {
+		$avif_info = wp_get_avif_info( $filename );
+
+		$width  = $avif_info['width'];
+		$height = $avif_info['height'];
+
+		// Mimic the native return format.
+		if ( $width && $height ) {
+			return array(
+				$width,
+				$height,
+				IMAGETYPE_AVIF,
+				sprintf(
+					'width="%d" height="%d"',
+					$width,
+					$height
+				),
+				'mime' => 'image/avif',
+			);
+		}
+	}
+
 	// The image could not be parsed.
 	return false;
+}
+
+/**
+ * Extracts meta information about an AVIF file: width, height, bit depth, and number of channels.
+ *
+ * @since 6.5.0
+ *
+ * @param string $filename Path to an AVIF file.
+ * @return array {
+ *    An array of AVIF image information.
+ *
+ *    @type int|false $width        Image width on success, false on failure.
+ *    @type int|false $height       Image height on success, false on failure.
+ *    @type int|false $bit_depth    Image bit depth on success, false on failure.
+ *    @type int|false $num_channels Image number of channels on success, false on failure.
+ * }
+ */
+function wp_get_avif_info( $filename ) {
+	$results = array(
+		'width'        => false,
+		'height'       => false,
+		'bit_depth'    => false,
+		'num_channels' => false,
+	);
+
+	if ( 'image/avif' !== wp_get_image_mime( $filename ) ) {
+		return $results;
+	}
+
+	// Parse the file using libavifinfo's PHP implementation.
+	require_once ABSPATH . WPINC . '/class-avif-info.php';
+
+	$handle = fopen( $filename, 'rb' );
+	if ( $handle ) {
+		$parser  = new Avifinfo\Parser( $handle );
+		$success = $parser->parse_ftyp() && $parser->parse_file();
+		fclose( $handle );
+		if ( $success ) {
+			$results = $parser->features->primary_item_features;
+		}
+	}
+	return $results;
 }
 
 /**
