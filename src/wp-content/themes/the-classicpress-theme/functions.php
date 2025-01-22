28d9401575454ecf6f4cfaba653f208cf35c4812
Ciprian Popescu <wolffe@users.noreply.github.com>
2025-01-22 15:11:13 +0000

    Update recommended featured image size

--- src/wp-content/themes/the-classicpress-theme/functions.php
+++ src/wp-content/themes/the-classicpress-theme/functions.php
@@ -210,7 +210,7 @@ if ( function_exists( 'register_sidebar' ) ) {
  * Modify Featured Image Text
  */
 function filter_featured_image_admin_text( $content, $post_id, $thumbnail_id ) {
-	$help_text = '<p><i>' . __( 'Ideal size is 800 x 471 pixels.', 'the-classicpress-theme' ) . '</i></p>';
+	$help_text = '<p><i>' . __( 'Ideal size is 1200 x 628 pixels or higher.', 'the-classicpress-theme' ) . '</i></p>';
 	return $help_text . $content;
 }
 add_filter( 'admin_post_thumbnail_html', 'filter_featured_image_admin_text', 10, 3 );
