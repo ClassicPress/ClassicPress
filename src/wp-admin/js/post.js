b2362f4c51be726dba10c6b9d07c2d74bf8ec8ad
Tim Kaye <KTS915@users.noreply.github.com>
2025-01-28 10:59:14 -0500

    Update post.js to remove scroll to last position

--- src/wp-admin/js/post.js
+++ src/wp-admin/js/post.js
@@ -1263,20 +1263,6 @@ jQuery( function($) {
 		}
 	} );
 
-	/* Scroll to last position within document before saving */
-	// Restore scroll position if it exists in sessionStorage
-	setTimeout( function() {
-		if ( sessionStorage.getItem( 'scrollPosition' ) ) {
-			window.scrollTo( 0, parseInt( sessionStorage.getItem( 'scrollPosition' ) ) );
-		}
-	}, 500);
-
-	// Store scroll position when the window is about to unload
-	window.addEventListener( 'beforeunload', function() {
-		var scrollPosition = document.documentElement.scrollTop;
-		sessionStorage.setItem( 'scrollPosition', scrollPosition );
-	} );
-
 	// If the last status was auto-draft and the save is triggered, edit the current URL.
 	if ( $( '#original_post_status' ).val() === 'auto-draft' && window.history.replaceState ) {
 		var location;
