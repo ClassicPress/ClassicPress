7183e7c43881b044aa9ec957b73cb8275cb7e538
Jonathan Desrosiers <desrosj@git.wordpress.org>
2025-02-10 08:56:18 +0100

    WP-r59783: General: Introduce polyfills for new array related functions in PHP 8.4.
    PHP 8.4 introduced four new functions to provide a common way to more easily perform common operations on arrays.
    
    - `array_find()`: https://www.php.net/manual/en/function.array-find.php
    - `array_find_key()`: https://www.php.net/manual/en/function.array-find-key.php
    - `array_all()`: https://www.php.net/manual/en/function.array-all.php
    - `array_any()`: https://www.php.net/manual/en/function.array-any.php
    
    These functions are now polyfilled making them available on all supported versions of PHP (currently 7.2+).
    
    WP:Props Soean, swissspidy, TobiasBg, ayeshrajans, mukesh27, joemcgill.
    Fixes https://core.trac.wordpress.org/ticket/62558.
    
    Conflicts:
    - src/wp-includes/compat.php
    
    ---
    
    Merges https://core.trac.wordpress.org/changeset/59783 / WordPress/wordpress-develop@6fea443d8f to ClassicPress.

--- src/wp-includes/compat.php
+++ src/wp-includes/compat.php
@@ -285,6 +285,7 @@ if ( ! function_exists( 'str_ends_with' ) ) {
 	}
 }
 
+<<<<<<< HEAD
 // IMAGETYPE_WEBP constant is only defined in PHP 7.1 or later.
 if ( ! defined( 'IMAGETYPE_WEBP' ) ) {
 	define( 'IMAGETYPE_WEBP', 18 );
@@ -293,6 +294,98 @@ if ( ! defined( 'IMAGETYPE_WEBP' ) ) {
 // IMG_WEBP constant is only defined in PHP 7.0.10 or later.
 if ( ! defined( 'IMG_WEBP' ) ) {
 	define( 'IMG_WEBP', IMAGETYPE_WEBP );
+=======
+if ( ! function_exists( 'array_find' ) ) {
+	/**
+	 * Polyfill for `array_find()` function added in PHP 8.4.
+	 *
+	 * Searches an array for the first element that passes a given callback.
+	 *
+	 * @since 6.8.0
+	 *
+	 * @param array    $array    The array to search.
+	 * @param callable $callback The callback to run for each element.
+	 * @return mixed|null The first element in the array that passes the `$callback`, otherwise null.
+	 */
+	function array_find( array $array, callable $callback ) { // phpcs:ignore Universal.NamingConventions.NoReservedKeywordParameterNames.arrayFound
+		foreach ( $array as $key => $value ) {
+			if ( $callback( $value, $key ) ) {
+				return $value;
+			}
+		}
+
+		return null;
+	}
+}
+
+if ( ! function_exists( 'array_find_key' ) ) {
+	/**
+	 * Polyfill for `array_find_key()` function added in PHP 8.4.
+	 *
+	 * Searches an array for the first key that passes a given callback.
+	 *
+	 * @since 6.8.0
+	 *
+	 * @param array    $array    The array to search.
+	 * @param callable $callback The callback to run for each element.
+	 * @return int|string|null The first key in the array that passes the `$callback`, otherwise null.
+	 */
+	function array_find_key( array $array, callable $callback ) { // phpcs:ignore Universal.NamingConventions.NoReservedKeywordParameterNames.arrayFound
+		foreach ( $array as $key => $value ) {
+			if ( $callback( $value, $key ) ) {
+				return $key;
+			}
+		}
+
+		return null;
+	}
+}
+
+if ( ! function_exists( 'array_any' ) ) {
+	/**
+	 * Polyfill for `array_any()` function added in PHP 8.4.
+	 *
+	 * Checks if any element of an array passes a given callback.
+	 *
+	 * @since 6.8.0
+	 *
+	 * @param array    $array    The array to check.
+	 * @param callable $callback The callback to run for each element.
+	 * @return bool True if any element in the array passes the `$callback`, otherwise false.
+	 */
+	function array_any( array $array, callable $callback ): bool { // phpcs:ignore Universal.NamingConventions.NoReservedKeywordParameterNames.arrayFound
+		foreach ( $array as $key => $value ) {
+			if ( $callback( $value, $key ) ) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+}
+
+if ( ! function_exists( 'array_all' ) ) {
+	/**
+	 * Polyfill for `array_all()` function added in PHP 8.4.
+	 *
+	 * Checks if all elements of an array pass a given callback.
+	 *
+	 * @since 6.8.0
+	 *
+	 * @param array    $array    The array to check.
+	 * @param callable $callback The callback to run for each element.
+	 * @return bool True if all elements in the array pass the `$callback`, otherwise false.
+	 */
+	function array_all( array $array, callable $callback ): bool { // phpcs:ignore Universal.NamingConventions.NoReservedKeywordParameterNames.arrayFound
+		foreach ( $array as $key => $value ) {
+			if ( ! $callback( $value, $key ) ) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+>>>>>>> 6fea443d8f (General: Introduce polyfills for new array related functions in PHP 8.4.)
 }
 
 // IMAGETYPE_AVIF constant is only defined in PHP 8.x or later.
