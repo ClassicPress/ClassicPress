6f713eb8d876dc06dc2138d1bcf7e7fe9febafee
xxsimoxx <simone@gieffeedizioni.it>
2025-03-03 09:53:05 +0100

    Fix WP-CLI

--- src/wp-includes/class-fix-wpcli.php
+++ src/wp-includes/class-fix-wpcli.php
@@ -19,7 +19,7 @@ class Fix_WPCLI {
 	 * @since CP-1.5.0
 	 */
 	public function __construct() {
-		WP_CLI::add_hook( 'after_wp_load', array( __CLASS__, 'add_cp_version_to_scope' ) );
+		WP_CLI::add_hook( 'after_wp_config_load', array( __CLASS__, 'add_cp_version_to_scope' ) );
 		WP_CLI::add_hook( 'before_invoke:core check-update', array( __CLASS__, 'correct_core_check_update' ) );
 	}
 
