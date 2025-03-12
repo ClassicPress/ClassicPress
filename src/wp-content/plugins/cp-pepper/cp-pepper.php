a89aaca309036cbae12efb86b662f9cdfd3ff63d
Gabriela <citrionea@gmail.com>
2025-03-12 14:48:58 -0300

    Bump version

--- src/wp-content/plugins/cp-pepper/cp-pepper.php
+++ src/wp-content/plugins/cp-pepper/cp-pepper.php
@@ -3,7 +3,7 @@
  * Plugin Name:       ClassicPress Pepper for Passwords
  * Plugin URI:        https://github.com/ClassicPress/ClassicPress
  * Description:       For enhanced security add a `pepper` to password hashing.
- * Version:           1.0.2
+ * Version:           2.0.0
  * Requires at least: 4.9.15
  * Requires PHP:      7.4
  * Requires CP:       2.2
