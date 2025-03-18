26f57e4b74852cd129fdefa91f05ead60d2d9397
mattyrob <mattyrobuk@gmail.com>
2025-03-18 10:50:33 +0000

    Remove unnecessary test

--- tests/phpunit/tests/widgets/wpWidgetMediaImage.php
+++ tests/phpunit/tests/widgets/wpWidgetMediaImage.php
@@ -617,19 +617,4 @@ class Tests_Widgets_wpWidgetMediaImage extends WP_UnitTestCase {
 
 		$this->assertTrue( wp_script_is( 'media-image-widget' ) );
 	}
-
-	/**
-	 * Test render_control_template_scripts method.
-	 *
-	 * @covers WP_Widget_Media_Image::render_control_template_scripts
-	 */
-	public function test_render_control_template_scripts() {
-		$widget = new WP_Widget_Media_Image();
-
-		ob_start();
-		$widget->render_control_template_scripts();
-		$output = ob_get_clean();
-
-		$this->assertStringContainsString( '<script type="text/html" id="tmpl-wp-media-widget-image-preview">', $output );
-	}
 }
