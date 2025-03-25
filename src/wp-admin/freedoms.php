af7dad26f4a4aa1edb651ff06d4ccc5ff29d457e
Guido07111975 <gjvdleest@yahoo.com>
2025-03-25 23:55:24 +0100

    Update freedoms.php
    Fix: copied from the WP file

--- src/wp-admin/freedoms.php
+++ src/wp-admin/freedoms.php
@@ -70,4 +70,4 @@ require_once ABSPATH . 'wp-admin/admin-header.php';
 </div>
 
 </div>
-<?php require ABSPATH . 'wp-admin/admin-footer.php'; ?>
+<?php require_once ABSPATH . 'wp-admin/admin-footer.php'; ?>
