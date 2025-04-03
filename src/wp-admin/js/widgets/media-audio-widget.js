38da4d090fed80aa306eb1a0e7d1fab366b1439a
Tim Kaye <KTS915@users.noreply.github.com>
2025-04-03 11:42:14 -0400

    Refactor Media audio widget to vanilla JavaScript and PHP (#1821)
    * Update media-audio-widget.js to build audio widget with PHP instead of backbone.js
    
    * Update class-wp-widget-media-audio.php to build media widget with PHP instead of backbone.js
    
    * Update media-audio-widget.js to replace a div with a fieldset
    
    * Update class-wp-widget-media-audio.php to correct conditionals
    
    * Update class-wp-widget-media-audio.php to improve conditionals
    
    * Update class-wp-widget-media-audio.php to make code more efficient
    
    * Update class-wp-widget-media-audio.php to correct typo
    
    * Update class-wp-widget-media-audio.php
    
    * Update media-audio-widget.js
    
    * Update class-wp-widget-media-video.php to comply with coding standards
    
    * Update media-video-widget.js to comply with coding standards
    
    * Update media-video-widget.js
    
    * Update media-video-widget.js
    
    * Update class-wp-widget-media-video.php
    
    * Update media-video-widget.js to revert erroneous commits
    
    * Update class-wp-widget-media-video.php to revert erroneous commits
    
    * Update media-audio-widget.js to comply with coding standards
    
    * Update media-audio-widget.js to eliminate trailing spaces
    
    * Remove unnecessary QUnit tests
    
    * Update core file header
    
    * Use parent `update()` function
    
    * Remove unnecessary PHPUnit test
    
    ---------
    
    Co-authored-by: mattyrob <mattyrobuk@gmail.com>
    
    CP:Props KTS915, mattyrob

--- src/wp-admin/js/widgets/media-audio-widget.js
+++ src/wp-admin/js/widgets/media-audio-widget.js
@@ -1,154 +1,352 @@
-/**
- * @output wp-admin/js/widgets/media-audio-widget.js
- */
-
 /* eslint consistent-this: [ "error", "control" ] */
-(function( component ) {
-	'use strict';
 
-	var AudioWidgetModel, AudioWidgetControl, AudioDetailsMediaFrame;
+/* global AUDIO_WIDGET, console */
+
+/*
+ * @since CP-2.5.0
+ */
+document.addEventListener( 'DOMContentLoaded', function() {
 
 	/**
-	 * Custom audio details frame that removes the replace-audio state.
+	 * Open the media select frame to chose an item.
 	 *
-	 * @class    wp.mediaWidgets.controlConstructors~AudioDetailsMediaFrame
-	 * @augments wp.media.view.MediaFrame.AudioDetails
+	 * @return {void}
 	 */
-	AudioDetailsMediaFrame = wp.media.view.MediaFrame.AudioDetails.extend(/** @lends wp.mediaWidgets.controlConstructors~AudioDetailsMediaFrame.prototype */{
+	function selectMedia( widget ) {
+		var mediaUploader,
+			embedded = false;
 
-		/**
-		 * Create the default states.
-		 *
-		 * @return {void}
-		 */
-		createStates: function createStates() {
-			this.states.add([
-				new wp.media.controller.AudioDetails({
-					media: this.media
-				}),
-
-				new wp.media.controller.MediaLibrary({
-					type: 'audio',
-					id: 'add-audio-source',
-					title: wp.media.view.l10n.audioAddSourceTitle,
-					toolbar: 'add-audio-source',
-					media: this.media,
-					menu: false
-				})
-			]);
+		if ( mediaUploader ) {
+			mediaUploader.open();
+			return;
 		}
-	});
 
-	/**
-	 * Audio widget model.
-	 *
-	 * See WP_Widget_Audio::enqueue_admin_scripts() for amending prototype from PHP exports.
-	 *
-	 * @class    wp.mediaWidgets.modelConstructors.media_audio
-	 * @augments wp.mediaWidgets.MediaWidgetModel
-	 */
-	AudioWidgetModel = component.MediaWidgetModel.extend({});
+		mediaUploader = wp.media( {
+			title: AUDIO_WIDGET.add_audio,
+			button: {
+				text: AUDIO_WIDGET.add_to_widget
+			},
+			multiple: false,
+			library: {
+				type: 'audio'
+			},
+			states: [
+				new wp.media.controller.Library( {
+					title: AUDIO_WIDGET.add_audio,
+					library: wp.media.query({ type: 'audio' }),
+					multiple: false,
+					priority: 20
+				} ),
+				new wp.media.controller.Embed( {
+					id: 'embed',
+					title: AUDIO_WIDGET.insert_from_url,
+					priority: 30
+				} )
+			]
+		} );
 
-	/**
-	 * Audio widget control.
-	 *
-	 * See WP_Widget_Audio::enqueue_admin_scripts() for amending prototype from PHP exports.
-	 *
-	 * @class    wp.mediaWidgets.controlConstructors.media_audio
-	 * @augments wp.mediaWidgets.MediaWidgetControl
-	 */
-	AudioWidgetControl = component.MediaWidgetControl.extend(/** @lends wp.mediaWidgets.controlConstructors.media_audio.prototype */{
+		// This runs once the modal is open
+		mediaUploader.on( 'ready', function() {
+			var separator = document.createElement( 'div' ),
+				addButton = mediaUploader.el.querySelector( '.media-button-select' ),
+				libraryButton = mediaUploader.el.querySelector( '#menu-item-library' );
 
-		/**
-		 * Show display settings.
-		 *
-		 * @type {boolean}
-		 */
-		showDisplaySettings: false,
+			separator.className = 'separator';
+			separator.setAttribute( 'role', 'presentation' );
+			libraryButton.after( separator );
 
-		/**
-		 * Map model props to media frame props.
-		 *
-		 * @param {Object} modelProps - Model props.
-		 * @return {Object} Media frame props.
-		 */
-		mapModelToMediaFrameProps: function mapModelToMediaFrameProps( modelProps ) {
-			var control = this, mediaFrameProps;
-			mediaFrameProps = component.MediaWidgetControl.prototype.mapModelToMediaFrameProps.call( control, modelProps );
-			mediaFrameProps.link = 'embed';
-			return mediaFrameProps;
-		},
-
-		/**
-		 * Render preview.
-		 *
-		 * @return {void}
-		 */
-		renderPreview: function renderPreview() {
-			var control = this, previewContainer, previewTemplate, attachmentId, attachmentUrl;
-			attachmentId = control.model.get( 'attachment_id' );
-			attachmentUrl = control.model.get( 'url' );
-
-			if ( ! attachmentId && ! attachmentUrl ) {
-				return;
+			// Prevent duplicate separator lines
+			if ( mediaUploader.el.querySelectorAll( '.separator' ).length > 1 ) {
+				mediaUploader.el.querySelectorAll( '.separator' )[1].remove();
 			}
 
-			previewContainer = control.$el.find( '.media-widget-preview' );
-			previewTemplate = wp.template( 'wp-media-widget-audio-preview' );
+			// Insert audio from URL
+			mediaUploader.el.querySelector( '#menu-item-embed' ).addEventListener( 'click', function() {
+				var embed = document.createElement( 'div' );
 
-			previewContainer.html( previewTemplate({
-				model: {
-					attachment_id: control.model.get( 'attachment_id' ),
-					src: attachmentUrl
-				},
-				error: control.model.get( 'error' )
-			}));
-			wp.mediaelement.initialize();
-		},
+				embed.className = 'media-embed';
+				embed.innerHTML = '<span class="embed-url">' +
+					'<input id="embed-url-field" type="url" aria-label="' + AUDIO_WIDGET.insert_from_url + '" placeholder="https://">' +
+					'<span class="spinner"></span>' +
+					'</span>';
 
-		/**
-		 * Open the media audio-edit frame to modify the selected item.
-		 *
-		 * @return {void}
-		 */
-		editMedia: function editMedia() {
-			var control = this, mediaFrame, metadata, updateCallback;
+				if ( embedded === false ) {
+					mediaUploader.el.querySelector( '.media-frame-content' ).append( embed );
+					embedded = true;
+				}
+				if ( mediaUploader.el.querySelector( '.uploader-inline' ) ) {
+					mediaUploader.el.querySelector( '.uploader-inline' ).remove();
+				}
+				if ( mediaUploader.el.querySelector( '.attachments-browser' ) ) {
+					mediaUploader.el.querySelector( '.attachments-browser' ).remove();
+				}
 
-			metadata = control.mapModelToMediaFrameProps( control.model.toJSON() );
+				mediaUploader.el.querySelector( '#embed-url-field' ).addEventListener( 'change', function( e ) {
+					var fileType  = e.target.value.split( '.' ).pop(),
+						error     = document.createElement( 'div' ),
+						audio     = document.createElement( 'audio' ),
+						source    = document.createElement( 'source' ),
+						buttons   = document.createElement( 'div' ),
+						message   = mediaUploader.el.querySelector( '#message' );
 
-			// Set up the media frame.
-			mediaFrame = new AudioDetailsMediaFrame({
-				frame: 'audio',
-				state: 'audio-details',
-				metadata: metadata
-			});
-			wp.media.frame = mediaFrame;
-			mediaFrame.$el.addClass( 'media-widget' );
+					// Update values in hidden fields
+					widget.querySelector( '[data-property="url"]' ).value = e.target.value;
+					if ( widget.querySelector( '[data-property="' + fileType + '"]' ) ) {
+						if ( message ) {
+							message.remove();
+						}
+						widget.querySelector( '[data-property="' + fileType + '"]' ).value = e.target.value;
 
-			updateCallback = function( mediaFrameProps ) {
+						audio.className = 'wp_audio_shortcode';
+						audio.style.width = '100%';
+						audio.controls = true;
 
-				// Update cached attachment object to avoid having to re-fetch. This also triggers re-rendering of preview.
-				control.selectedAttachment.set( mediaFrameProps );
+						// Create source element
+						source.src = e.target.value;
 
-				control.model.set( _.extend(
-					control.model.defaults(),
-					control.mapMediaToModelProps( mediaFrameProps ),
-					{ error: false }
-				) );
-			};
+						// Append source to audio
+						audio.append( source );
 
-			mediaFrame.state( 'audio-details' ).on( 'update', updateCallback );
-			mediaFrame.state( 'replace-audio' ).on( 'replace', updateCallback );
-			mediaFrame.on( 'close', function() {
-				mediaFrame.detach();
-			});
+						// Add Edit and Replace buttons
+						buttons.className = 'media-widget-buttons';
+						buttons.innerHTML = '<button type="button" class="button edit-media">' + AUDIO_WIDGET.edit_audio + '</button>' +
+							'<button type="button" class="button change-media select-media">' + AUDIO_WIDGET.replace_audio + '</button>';
 
-			mediaFrame.open();
+						// Insert audio according to whether this is a new insertion or replacement
+						if ( widget.querySelector( '.attachment-media-view' ) !== null ) {
+							widget.querySelector( '.media_audio' ).append( audio );
+							widget.querySelector( '.attachment-media-view' ).replaceWith( buttons );
+						} else { // replacement
+							widget.querySelector( '.wp-audio' ).replaceWith( audio );
+						}
+
+						// Activate Add to widget button
+						if ( addButton.disabled ) {
+							addButton.removeAttribute( 'disabled' );
+						}
+
+						addButton.addEventListener( 'click', function() {
+							document.querySelector( '.media-modal-close' ).click();
+
+							// Activate Save/Publish button
+							if ( document.body.className.includes( 'widgets-php' ) ) {
+								widget.classList.add( 'widget-dirty' );
+							}
+							widget.dispatchEvent( new Event( 'change' ) );
+						} );
+					} else {
+						if ( message == null ) {
+							error.id = 'message';
+							error.className = 'notice-error is-dismissible';
+							error.innerHTML = '<p style="color:#fff;background:red;padding:0.5em 1em;"></p>';
+							error.querySelector( 'p' ).textContent = AUDIO_WIDGET.unsupported_file_type;
+							e.target.before( error );
+						}
+					}
+				} );
+
+			} );
+
+			libraryButton.addEventListener( 'click', function() {
+				embedded = false;
+			} );
+		} );
+
+		// Insert audio from media library
+		mediaUploader.on( 'select', function() {
+			var attachment = mediaUploader.state().get( 'selection' ).first().toJSON(),
+				fileType   = attachment.url.split( '.' ).pop(),
+				audio      = document.createElement( 'audio' ),
+				source     = document.createElement( 'source' ),
+				fieldset   = document.createElement( 'fieldset' );
+
+			audio.className = 'wp_audio_shortcode';
+			audio.style.width = '100%';
+			audio.controls = true;
+
+			// Create source element
+			source.src = attachment.url;
+			source.type = attachment.mime;
+
+			// Append source to audio
+			audio.append( source );
+
+			// Add Edit and Replace buttons
+			fieldset.className = 'media-widget-buttons';
+			fieldset.innerHTML = '<button type="button" class="button edit-media">' + AUDIO_WIDGET.edit_audio + '</button>' +
+				'<button type="button" class="button change-media select-media">' + AUDIO_WIDGET.replace_audio + '</button>';
+
+			// Insert audio according to whether this is a new insertion or replacement
+			if ( widget.querySelector( '.attachment-media-view' ) !== null ) {
+				widget.querySelector( '.media_audio' ).prepend( audio );
+				widget.querySelector( '.attachment-media-view' ).replaceWith( fieldset );
+			} else { // replacement
+				widget.querySelector( '.media_audio' ).replaceWith( audio );
+			}
+
+			// Update values in hidden fields
+			widget.querySelector( '[data-property="attachment_id"]' ).value = attachment.id;
+			widget.querySelector( '[data-property="url"]' ).value = attachment.url;
+
+			if ( widget.querySelector( '[data-property="' + fileType + '"]' ) ) {
+				widget.querySelector( '[data-property="' + fileType + '"]' ).value = attachment.url;
+			} else {
+				console.error( AUDIO_WIDGET.unsupported_file_type );
+			}
+
+			// Activate Save/Publish button
+			if ( document.body.className.includes( 'widgets-php' ) ) {
+				widget.classList.add( 'widget-dirty' );
+			}
+			widget.dispatchEvent( new Event( 'change' ) );
+		} );
+
+		mediaUploader.open();
+	}
+
+	/**
+	 * Open the media frame to modify the selected item.
+	 *
+	 * @abstract
+	 * @return {void}
+	 */
+	function editMedia( widget ) {
+		var	frame,
+			audioURL = widget.querySelector( '[data-property="url"]' ).value,
+			audioID = widget.querySelector( '[data-property="attachment_id"]' ).value;
+
+        if ( audioURL === null ) {
+			audioURL = widget.querySelector( '[data-property="mp3"]' ).value;
 		}
-	});
+		if ( audioURL === null ) {
+			audioURL = widget.querySelector( '[data-property="ogg"]' ).value;
+		}
+		if ( audioURL === null ) {
+			audioURL = widget.querySelector( '[data-property="flac"]' ).value;
+		}
+		if ( audioURL === null ) {
+			audioURL = widget.querySelector( '[data-property="m4a"]' ).value;
+		}
+		if ( audioURL === null ) {
+			audioURL = widget.querySelector( '[data-property="wav"]' ).value;
+		}
+		if ( audioURL === null ) {
+            console.error( AUDIO_WIDGET.no_audio_selected );
+            return;
+        }
 
-	// Exports.
-	component.controlConstructors.media_audio = AudioWidgetControl;
-	component.modelConstructors.media_audio = AudioWidgetModel;
+        // Open the media modal in edit mode
+        frame = wp.media( {
+            frame: 'audio',
+            state: 'audio-details',
+            metadata: wp.media.attachment( audioID )
+        } );
 
-})( wp.mediaWidgets );
+		// This runs once the modal is open
+		frame.on( 'ready', function() {
+			frame.el.querySelector( '#audio-details-loop' ).checked = widget.querySelector( '[data-property="loop"]' ).value === '1' ? true : false;
+
+			updateFrame( frame, widget, audioURL );
+
+			frame.el.querySelector( '#menu-item-audio-details' ).addEventListener( 'click', function() {
+				updateFrame( frame, widget, audioURL );
+			} );
+		} );
+
+        frame.open();
+    }
+
+    function updateFrame( frame, widget, audioURL ) {
+		var audio = document.createElement( 'audio' ),
+			source = document.createElement( 'source' ),
+			span = document.createElement( 'span' ),
+			fileType = audioURL.split( '.' ).pop(),
+			autoplay = frame.el.querySelector( '#audio-details-autoplay' ),
+			loop = frame.el.querySelector( '#audio-details-loop' );
+
+		// Organize menu items in left-hand column
+		if ( frame.el.querySelector( '#menu-item-replace-audio' ) ) {
+			frame.el.querySelector( '#menu-item-replace-audio' ).remove();
+		}
+		if ( frame.el.querySelector( '#menu-item-select-poster-image' ) ) {
+			frame.el.querySelector( '#menu-item-select-poster-image' ).remove();
+		}
+
+		audio.className = 'wp_audio_shortcode';
+		audio.style.width = '100%';
+		audio.controls = true;
+
+		// Create source element
+		source.src = audioURL;
+		source.type = 'audio/' + fileType;
+
+		// Append source to audio
+		audio.append( source );
+		frame.el.querySelector( 'audio' ).replaceWith( audio );
+
+		span.className = 'setting';
+		span.innerHTML = '<label for="audio-details-' + fileType + '-source" class="name">' + fileType.toUpperCase() + '</label>' +
+			'<input type="text" id="audio-details-' + fileType + '-source" readonly="" data-setting="' + fileType + '" value="' + audioURL + '">' +
+			'<button type="button" class="button-link remove-setting">' + AUDIO_WIDGET.remove_audio_source + '</button>' +
+			'</span>';
+		if ( frame.el.querySelector( '[data-setting="' + fileType + '"]' ) == null ) {
+			frame.el.querySelector( '.wp_audio_shortcode' ).after( span );
+		}
+
+		// Remove current filetype button from alternate sources list
+		frame.el.querySelectorAll( '.add-media-source' ).forEach( function( mime ) {
+			if ( mime.textContent === fileType ) {
+				mime.remove();
+			}
+		} );
+
+		// Remove autoplay option
+		if ( autoplay ) {
+			autoplay.parentNode.remove();
+		}
+
+		// Set whether the audio should loop or not
+		loop.addEventListener( 'change', function() {
+			if ( loop.checked ) {
+				widget.querySelector( '[data-property="loop"]' ).value = '1';
+			} else {
+				widget.querySelector( '[data-property="loop"]' ).value = '';
+			}
+		} );
+
+		// Set preload option
+		frame.el.querySelectorAll( '.setting.preload .button' ).forEach( function( preload ) {
+			if ( preload.value === widget.querySelector( '[data-property="preload"]' ).value ) {
+				setTimeout( preload.click() );
+			}
+			preload.addEventListener( 'click', function() {
+				if ( preload.value === 'none' ) {
+					preload.value = '';
+				}
+				widget.querySelector( '[data-property="preload"]' ).value = preload.value;
+			} );
+		} );
+
+		// Activate Save button on widget after update
+		frame.el.querySelector( '.media-button-button' ).addEventListener( 'click', function() {
+			widget.dispatchEvent( new Event( 'change' ) );
+		} );
+	}
+
+	// Handle clicks on Add, Edit, and Replace Video buttons
+	document.addEventListener( 'click', function( e ) {
+		var base,
+			widget = e.target.closest( '.widget' );
+
+		if ( widget ) {
+			base = widget.querySelector( '.id_base' );
+			if ( base && base.value === 'media_audio' && e.target.tagName === 'BUTTON' ) {
+				if ( e.target.className.includes( 'select-media' ) ) {
+					selectMedia( widget );
+				} else if ( e.target.className.includes( 'edit-media' ) ) {
+					editMedia( widget );
+				}
+			}
+		}
+	} );
+} );
