33208cae6dd5b642356bdb324d91e4393c0ba702
Tim Kaye <KTS915@users.noreply.github.com>
2025-04-08 11:50:17 -0400

    Update user.php to revert previous change

--- src/wp-admin/includes/user.php
+++ src/wp-admin/includes/user.php
@@ -107,18 +107,18 @@ function edit_user( $user_id = 0 ) {
 	 *
 	 * @since CP-2.1.0
 	 */
-	$taxonomies = apply_filters( 'cp_user_taxonomies', get_object_taxonomies( 'user' ), $user );
+	$taxonomies = apply_filters( 'cp_user_taxonomies', get_object_taxonomies( 'user', 'objects' ), $user );
 
 	if ( ! empty( $taxonomies ) ) {
 		foreach ( $taxonomies as $taxonomy ) {
-			if ( isset( $_POST[ $taxonomy ] ) ) {
+			if ( isset( $_POST[ $taxonomy->name ] ) ) {
 				$term_slugs = array();
-				foreach ( $_POST[ $taxonomy ] as $term_slug ) {
+				foreach ( $_POST[ $taxonomy->name ] as $term_slug ) {
 					$term_slugs[] = wp_unslash( $term_slug );
 				}
-				wp_set_object_terms( $user->ID, array_map( 'sanitize_title', array_unique( $term_slugs ) ), $taxonomy );
+				wp_set_object_terms( $user->ID, array_map( 'sanitize_title', array_unique( $term_slugs ) ), $taxonomy->name );
 			} else {
-				wp_delete_object_term_relationships( $user->ID, $taxonomy );
+				wp_delete_object_term_relationships( $user->ID, $taxonomy->name );
 			}
 		}
 	}
