33ec15813c33177b481546f6688c7c9185e5f58e
Guido07111975 <gjvdleest@yahoo.com>
2025-04-29 20:29:00 +0200

    Update class-wp-plugin-install-list-table.php

--- src/wp-admin/includes/class-wp-plugin-install-list-table.php
+++ src/wp-admin/includes/class-wp-plugin-install-list-table.php
@@ -692,14 +692,6 @@ class WP_Plugin_Install_List_Table extends WP_List_Table {
 					}
 				} elseif ( ! $compatible_wp ) {
 					$incompatible_notice_message .= __( 'This plugin does not work with your version of ClassicPress.' );
-					if ( current_user_can( 'update_core' ) && $cp_needs_update ) {
-						$incompatible_notice_message .= sprintf(
-							/* translators: %s: URL to WordPress Updates screen. */
-							' ' . __( '<a href="%s">Please update ClassicPress</a>.' ),
-							self_admin_url( 'update-core.php' )
-						);
-						$incompatible_notice_message .= wp_update_php_annotation( '</p><p><em>', '</em>', false );
-					}
 				} elseif ( ! $compatible_php ) {
 					$incompatible_notice_message .= __( 'This plugin does not work with your version of PHP.' );
 					if ( current_user_can( 'update_php' ) ) {
