d80ba6213948d3f29b64d9357ba02e2c0cd75936
Tim Kaye <KTS915@users.noreply.github.com>
2025-05-03 11:08:27 -0400

    Update media-video-widget.js to avoid conflict with gallery widget

--- src/wp-admin/js/widgets/media-video-widget.js
+++ src/wp-admin/js/widgets/media-video-widget.js
@@ -1096,7 +1096,7 @@ document.addEventListener( 'DOMContentLoaded', function() {
 		} else if ( dialog.querySelector( '#widget-modal-media-content' ) ) {
 			widgetId = dialog.querySelector( '#widget-modal-media-content' ).dataset.widgetId;
 			widgetEl = document.getElementById( widgetId );
-			base     = widgetEl.querySelector( '.id_base' );
+			base     = widgetEl ? widgetEl.querySelector( '.id_base' ) : '';
 
 			// Only run on a media image widget
 			if ( base && base.value === 'media_video' ) {
