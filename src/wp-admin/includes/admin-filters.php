54d9a3cf559766eecac3650a0d286f132cef0784
Tim Kaye <KTS915@users.noreply.github.com>
2025-05-27 11:03:10 -0400

    Update admin-filters.php

--- src/wp-admin/includes/admin-filters.php
+++ src/wp-admin/includes/admin-filters.php
@@ -133,7 +133,7 @@ add_action( '_core_updated_successfully', '_redirect_to_about_wordpress' );
 
 // Upgrade hooks.
 add_action( 'upgrader_process_complete', array( 'Language_Pack_Upgrader', 'async_upgrade' ), 20 );
-add_action( 
+add_action(
 	'upgrader_process_complete',
 	function () {
 		wp_version_check( array(), true );
