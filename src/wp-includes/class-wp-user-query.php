9a7ca7abadf8c070f7e17b3542f959f052f184f6
Tim Kaye <KTS915@users.noreply.github.com>
2025-05-29 04:46:46 -0400

    Fix content attribution dropdown when a user is deleted (#1961)
    * Update class-wp-user-query.php
    
    * Update class-wp-user-query.php to apply @mattyrob's suggestion
    
    CCP:Props KTS915, Guido07111975, mattyrob

--- src/wp-includes/class-wp-user-query.php
+++ src/wp-includes/class-wp-user-query.php
@@ -777,11 +777,13 @@ class WP_User_Query {
 		/**
 		 * Enable grouping of users by taxonomy term on 'users.php' page.
 		 *
+		 * Do not run when deleting one or more users.
+		 *
 		 * @since CP-2.1.0
 		 *
 		 */
 		global $pagenow;
-		if ( $pagenow === 'users.php' ) {
+		if ( $pagenow === 'users.php' && ( isset( $_GET['action'] ) && 'delete' !== $_GET['action'] ) ) {
 
 			// Get user taxonomies
 			$taxonomies = get_taxonomies(
