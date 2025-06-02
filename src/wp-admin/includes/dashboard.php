2730f308140942d8d932ca59ae95233dde2a82b1
Guido07111975 <gjvdleest@yahoo.com>
2025-06-02 08:17:14 +0200

    Update dashboard.php
    As requested by Matt

--- src/wp-admin/includes/dashboard.php
+++ src/wp-admin/includes/dashboard.php
@@ -1082,6 +1082,7 @@ function wp_dashboard_recent_comments( $total_items = 5 ) {
 	$comments_query = array(
 		'number' => $total_items * 5,
 		'offset' => 0,
+		'post_type' => array_keys( get_post_types() ),
 	);
 
 	if ( ! current_user_can( 'edit_posts' ) ) {
