85e0d3c486175508212b0527a8380291bfbbb7ab
mattyrob <mattyrobuk@gmail.com>
2025-07-10 17:16:20 +0100

    Check for development builds the ClassicPress way

--- src/wp-admin/includes/update-core.php
+++ src/wp-admin/includes/update-core.php
@@ -350,7 +350,7 @@ function update_core( $from, $to ) {
 	 * when updating from older WordPress versions, in which case
 	 * the polyfills from wp-includes/compat.php may not be available.
 	 */
-	$development_build = ( false !== strpos( $old_wp_version . $wp_version, '-' ) ); // A dash in the version indicates a development release.
+	$development_build = ( false !== strpos( $old_wp_version . $wp_version, '+' ) ); // A plus in the version indicates a development or nightly release.
 	$php_compat        = version_compare( $php_version, $required_php_version, '>=' );
 
 	if ( file_exists( WP_CONTENT_DIR . '/db.php' ) && empty( $wpdb->is_mysql ) ) {
