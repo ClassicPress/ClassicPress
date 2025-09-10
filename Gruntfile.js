a07049055e541d362896e67add3b3d0ee1fdfb3f
Matt Robinson <mattyrobuk@gmail.com>
2025-09-10 18:11:15 +0100

    Add `mediaelement` as dependency and introduce build steps (#2013)
    * Add wp-mediaelement to build for CSS validation and minification
    
    * Add mediaelement as dependency, add build step and apply update
    
    * Re-apply max-width in controlled file
    
    * Fix lint comma dangle
    
    * Restore `mediaelement` as a dependency
    
    CP:Props mattyrob, xxsimoxx

--- Gruntfile.js
+++ Gruntfile.js
@@ -61,7 +61,8 @@ module.exports = function(grunt) {
 				dest: SOURCE_DIR,
 				src: [
 					'wp-admin/css/*.css',
-					'wp-includes/css/*.css'
+					'wp-includes/css/*.css',
+					'wp-includes/js/mediaelement/wp-*.css'
 				]
 			},
 			colors: {
@@ -121,6 +122,21 @@ module.exports = function(grunt) {
 				src: [
 					'wp-includes/js/dist/*.js',
 					'!wp-includes/js/dist/vendor/**'
+				]
+			},
+			'mediaelement-js': {
+				expand: true,
+				cwd: SOURCE_DIR,
+				src: [
+					'wp-includes/js/mediaelement/mediaelement-and-player.js',
+					'wp-includes/js/mediaelement/mediaelement-and-player.min.js',
+					'wp-includes/js/mediaelement/mediaelement.js',
+					'wp-includes/js/mediaelement/mediaelement.min.js',
+					'wp-includes/js/mediaelement/mediaelementplayer-legacy.css',
+					'wp-includes/js/mediaelement/mediaelementplayer-legacy.min.css',
+					'wp-includes/js/mediaelement/mediaelementplayer.css',
+					'wp-includes/js/mediaelement/mediaelementplayer.min.css',
+					'wp-includes/js/mediaelement/mejs-controls.svg'
 				]
 			},
 			dynamic: {
@@ -226,6 +242,46 @@ module.exports = function(grunt) {
 					{
 						src:  `./node_modules/sortablejs/Sortable.min.js`,
 						dest: `${SOURCE_DIR}wp-includes/js/sortable.min.js`
+					}
+				]
+			},
+			'mediaelement-js': {
+				files: [
+					{
+						src:  `./node_modules/mediaelement/build/mediaelement-and-player.js`,
+						dest: `${SOURCE_DIR}wp-includes/js/mediaelement/mediaelement-and-player.js`
+					},
+					{
+						src:  `./node_modules/mediaelement/build/mediaelement-and-player.min.js`,
+						dest: `${SOURCE_DIR}wp-includes/js/mediaelement/mediaelement-and-player.min.js`
+					},
+					{
+						src:  `./node_modules/mediaelement/build/mediaelement.js`,
+						dest: `${SOURCE_DIR}wp-includes/js/mediaelement/mediaelement.js`
+					},
+					{
+						src:  `./node_modules/mediaelement/build/mediaelement.min.js`,
+						dest: `${SOURCE_DIR}wp-includes/js/mediaelement/mediaelement.min.js`
+					},
+					{
+						src:  `./node_modules/mediaelement/build/mediaelementplayer-legacy.css`,
+						dest: `${SOURCE_DIR}wp-includes/js/mediaelement/mediaelementplayer-legacy.css`
+					},
+					{
+						src:  `./node_modules/mediaelement/build/mediaelementplayer-legacy.min.css`,
+						dest: `${SOURCE_DIR}wp-includes/js/mediaelement/mediaelementplayer-legacy.min.css`
+					},
+					{
+						src:  `./node_modules/mediaelement/build/mediaelementplayer.css`,
+						dest: `${SOURCE_DIR}wp-includes/js/mediaelement/mediaelementplayer.css`
+					},
+					{
+						src:  `./node_modules/mediaelement/build/mediaelementplayer.min.css`,
+						dest: `${SOURCE_DIR}wp-includes/js/mediaelement/mediaelementplayer.min.css`
+					},
+					{
+						src:  `./node_modules/mediaelement/build/mejs-controls.svg`,
+						dest: `${SOURCE_DIR}wp-includes/js/mediaelement/mejs-controls.svg`
 					}
 				]
 			},
@@ -1201,10 +1257,12 @@ module.exports = function(grunt) {
 		[
 			'clean:vendor-js',
 			'clean:package-js',
+			'clean:mediaelement-js',
 			'copy:vendor-js',
 			'webpack:dev',
 			'webpack:min',
-			'usebanner:js'
+			'usebanner:js',
+			'copy:mediaelement-js'
 		]
 	);
 
