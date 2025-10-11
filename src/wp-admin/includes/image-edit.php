904c5a413e6275ae88355f41abf92217b75c5b19
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2025-10-11 18:47:39 +0100

    WP-r60703: Code Modernization: Address no-op function deprecations in PHP 8.5.
    Several PHP functions that have not been doing anything since PHP 8.0/8.1, specifically:
    
    * `finfo_close()` since the `ext/fileinfo` migration in PHP 8.1
    * `xml_parser_free()` since the `ext/xml` migration in PHP 8.0
    * `curl_close()` since the `ext/curl` migration in PHP 8.0
    * `curl_share_close()` since the `ext/curl` migration in PHP 8.0
    * `imagedestroy()` since the `ext/gd` migration in PHP 8.0
    
    will be deprecated in PHP 8.5 and will thus be throwing warnings.
    
    This commit adds conditional checks to only call these functions on the relevant PHP versions.
    
    Reference: [https://wiki.php.net/rfc/deprecations_php_8_5#deprecate_no-op_functions_from_the_resource_to_object_conversion PHP RFC: Deprecations for PHP 8.5: Deprecate no-op functions from the resource to object conversion].
    
    WP:Props TobiasBg, SergeyBiryukov.
    See https://core.trac.wordpress.org/ticket/63061.
    
    Conflicts:
    - src/wp-includes/functions.php
    
    ---
    
    Merges https://core.trac.wordpress.org/changeset/60703 / WordPress/wordpress-develop@9b6c234bc6 to ClassicPress.

--- src/wp-admin/includes/image-edit.php
+++ src/wp-admin/includes/image-edit.php
@@ -541,7 +541,10 @@ function _rotate_image_resource( $img, $angle ) {
 		$rotated = imagerotate( $img, $angle, 0 );
 
 		if ( is_gd_image( $rotated ) ) {
+			if ( PHP_VERSION_ID < 80000 ) { // imagedestroy() has no effect as of PHP 8.0.
 			imagedestroy( $img );
+			}
+
 			$img = $rotated;
 		}
 	}
@@ -576,7 +579,10 @@ function _flip_image_resource( $img, $horz, $vert ) {
 		$sh = $horz ? -$h : $h;
 
 		if ( imagecopyresampled( $dst, $img, 0, 0, $sx, $sy, $w, $h, $sw, $sh ) ) {
+			if ( PHP_VERSION_ID < 80000 ) { // imagedestroy() has no effect as of PHP 8.0.
 			imagedestroy( $img );
+			}
+
 			$img = $dst;
 		}
 	}
@@ -602,7 +608,10 @@ function _crop_image_resource( $img, $x, $y, $w, $h ) {
 
 	if ( is_gd_image( $dst ) ) {
 		if ( imagecopy( $dst, $img, 0, 0, $x, $y, $w, $h ) ) {
+			if ( PHP_VERSION_ID < 80000 ) { // imagedestroy() has no effect as of PHP 8.0.
 			imagedestroy( $img );
+			}
+
 			$img = $dst;
 		}
 	}
