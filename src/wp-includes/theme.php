833eb28c41aa8aab7c1a4060d9676fc56e1c13f2
Tim Kaye <KTS915@users.noreply.github.com>
2025-12-19 11:42:26 -0500

    Delete core true stylesheet transient on switching themes

--- src/wp-includes/theme.php
+++ src/wp-includes/theme.php
@@ -3389,6 +3389,7 @@ function check_theme_switched() {
 			/** This action is documented in wp-includes/theme.php */
 			do_action( 'after_switch_theme', $stylesheet, $old_theme );
 		}
+		delete_transient( 'core_true_stylesheet' );
 
 		flush_rewrite_rules();
 
