c5c746ccf953c69bf751c8c51771c2d884b1a3e0
mattyrob <mattyrobuk@gmail.com>
2025-12-31 17:44:52 +0000

    Unify testing

--- tests/phpunit/tests/option/cacheOption.php
+++ tests/phpunit/tests/option/cacheOption.php
@@ -5,24 +5,14 @@
  */
 class Tests_Option_CacheOption extends WP_UnitTestCase {
 	private static $cp_settings;
+	private static $object_cache_file;
 
 	public static function wpSetUpBeforeClass() {
 		require_once ABSPATH . WPINC . '/classicpress/class-cp-settings.php';
 		self::$cp_settings = new CP_Settings();
-	}
-
-	public static function wpTearDownAfterClass() {
-		// make sure to tidy up if needed
-		if ( class_exists( 'Memcache' ) ) {
-			return;
-		}
 
 		$wp_content_dir    = defined( 'WP_CONTENT_DIR' ) ? WP_CONTENT_DIR : ABSPATH . 'wp-content';
-		$object_cache_file = $wp_content_dir . '/object-cache.php';
-
-		if ( file_exists( $object_cache_file ) ) {
-			unlink( $object_cache_file );
-		}
+		self::$object_cache_file = $wp_content_dir . '/object-cache.php';
 	}
 
 	/**
@@ -33,37 +23,23 @@ class Tests_Option_CacheOption extends WP_UnitTestCase {
 			$this->markTestSkipped( 'APCu extension is not available.' );
 		}
 
+		if ( file_exists( self::$object_cache_file ) ) {
+			$this->markTestSkipped( 'Leave original object cache in place for Memcached tests.' );
+		}
+
 		update_option( 'cp_object_cache', 1 );
 
 		$this->assertSame( 1, get_option( 'cp_object_cache' ) );
 
-		$wp_content_dir    = defined( 'WP_CONTENT_DIR' ) ? WP_CONTENT_DIR : ABSPATH . 'wp-content';
-		$object_cache_file = $wp_content_dir . '/object-cache.php';
 		self::$cp_settings->_cp_maybe_install_apcu_object_cache();
-
-		$this->assertTrue( file_exists( $object_cache_file ) );
-	}
-
-	/**
-	 * @covers ::_cp_maybe_install_apcu_object_cache
-	 */
-	public function test_object_cache_uninstalled() {
-		if ( ! extension_loaded( 'apcu' ) ) {
-			$this->markTestSkipped( 'APCu extension is not available.' );
-		}
-
-		if ( class_exists( 'Memcache' ) ) {
-			$this->markTestSkipped( 'Leave object cache in place for Memcached tests.' );
-		}
+		$this->assertTrue( file_exists( self::$object_cache_file ) );
 
 		update_option( 'cp_object_cache', 0 );
 
 		$this->assertSame( 0, get_option( 'cp_object_cache' ) );
 
-		$wp_content_dir    = defined( 'WP_CONTENT_DIR' ) ? WP_CONTENT_DIR : ABSPATH . 'wp-content';
-		$object_cache_file = $wp_content_dir . '/object-cache.php';
 		self::$cp_settings->_cp_maybe_install_apcu_object_cache();
 
-		$this->assertFalse( file_exists( $object_cache_file ) );
+		$this->assertFalse( file_exists( self::$object_cache_file ) );
 	}
 }
