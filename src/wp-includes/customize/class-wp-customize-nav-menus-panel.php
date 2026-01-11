0c664aeec8b0807311ccecf3ea90b62a14a45022
Tim Kaye <KTS915@users.noreply.github.com>
2026-01-11 16:23:40 -0500

    Add new PHP render_content method and refactor templates

--- src/wp-includes/customize/class-wp-customize-nav-menus-panel.php
+++ src/wp-includes/customize/class-wp-customize-nav-menus-panel.php
@@ -56,7 +56,82 @@ class WP_Customize_Nav_Menus_Panel extends WP_Customize_Panel {
 	}
 
 	/**
-	 * An Underscore (JS) template for this panel's content (but not its container).
+	 * PHP rendering for this panel's content (but not its container).
+	 *
+	 * @since CP-2.7.0
+	 */
+	public function render_content() {
+		$panels      = $this->manager->panels();
+		$menus_panel = isset ( $panels['nav_menus'] ) ? $panels['nav_menus'] : null;
+		if ( $menus_panel ) :
+			$cannot_expand = $menus_panel->description ? '' : ' cannot-expand';
+			?>
+
+			<li class="panel-meta customize-info accordion-section<?php echo $cannot_expand; ?>">
+				<button type="button" class="customize-panel-back" tabindex="-1">
+					<span class="screen-reader-text">
+						<?php
+						/* translators: Hidden accessibility text. */
+						esc_html_e( 'Back' );
+						?>
+					</span>
+				</button>
+				<div class="accordion-section-title">
+					<span class="preview-notice">
+						<?php
+						/* translators: %s: The site/panel title in the Customizer. */
+						printf( __( 'You are customizing %s' ), '<strong class="panel-title">' . esc_html( $menus_panel->title ) . '</strong>' );
+						?>
+					</span>
+					<button type="button" class="customize-help-toggle dashicons dashicons-editor-help" aria-expanded="false">
+						<span class="screen-reader-text">
+							<?php
+							/* translators: Hidden accessibility text. */
+							esc_html_e( 'Help' );
+							?>
+						</span>
+					</button>
+					<button type="button" class="customize-screen-options-toggle" aria-expanded="false">
+						<span class="screen-reader-text">
+							<?php
+							/* translators: Hidden accessibility text. */
+							esc_html_e( 'Menu Options' );
+							?>
+						</span>
+					</button>
+				</div>
+
+				<?php
+				if ( $menus_panel->description ) {
+					?>
+
+					<div class="description customize-panel-description">
+						<?php echo wp_kses_post( $menus_panel->description ); ?>
+					</div>
+
+					<?php
+				}
+				?>
+
+				<div id="screen-options-wrap">
+					<?php $this->render_screen_options(); ?>
+				</div>
+			</li>
+
+			<?php
+		endif;
+		// NOTE: The following is a workaround for an inability to treat (and thus label) a list of sections as a whole.
+		?>
+
+		<li class="customize-control-title customize-section-title-nav_menus-heading">
+			<?php esc_html_e( 'Menus' ); ?>
+		</li>
+
+		<?php
+	}
+
+	/**
+	 * Redundant Underscore (JS) template for this panel's content (but not its container).
 	 *
 	 * Class variables for this panel class are available in the `data` JS object;
 	 * export custom variables by overriding WP_Customize_Panel::json().
@@ -65,52 +140,5 @@ class WP_Customize_Nav_Menus_Panel extends WP_Customize_Panel {
 	 *
 	 * @see WP_Customize_Panel::print_template()
 	 */
-	protected function content_template() {
-		?>
-		<li class="panel-meta customize-info accordion-section <# if ( ! data.description ) { #> cannot-expand<# } #>">
-			<button type="button" class="customize-panel-back" tabindex="-1">
-				<span class="screen-reader-text">
-					<?php
-					/* translators: Hidden accessibility text. */
-					_e( 'Back' );
-					?>
-				</span>
-			</button>
-			<div class="accordion-section-title">
-				<span class="preview-notice">
-					<?php
-					/* translators: %s: The site/panel title in the Customizer. */
-					printf( __( 'You are customizing %s' ), '<strong class="panel-title">{{ data.title }}</strong>' );
-					?>
-				</span>
-				<button type="button" class="customize-help-toggle dashicons dashicons-editor-help" aria-expanded="false">
-					<span class="screen-reader-text">
-						<?php
-						/* translators: Hidden accessibility text. */
-						_e( 'Help' );
-						?>
-					</span>
-				</button>
-				<button type="button" class="customize-screen-options-toggle" aria-expanded="false">
-					<span class="screen-reader-text">
-						<?php
-						/* translators: Hidden accessibility text. */
-						_e( 'Menu Options' );
-						?>
-					</span>
-				</button>
-			</div>
-			<# if ( data.description ) { #>
-			<div class="description customize-panel-description">{{{ data.description }}}</div>
-			<# } #>
-			<div id="screen-options-wrap">
-				<?php $this->render_screen_options(); ?>
-			</div>
-		</li>
-		<?php
-		// NOTE: The following is a workaround for an inability to treat (and thus label) a list of sections as a whole.
-		?>
-		<li class="customize-control-title customize-section-title-nav_menus-heading"><?php _e( 'Menus' ); ?></li>
-		<?php
-	}
+	protected function content_template() {}
 }
