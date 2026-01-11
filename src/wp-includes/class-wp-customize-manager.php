58ecad5590f8beca6b9e247ffc077d905ffd5a33
Tim Kaye <KTS915@users.noreply.github.com>
2026-01-11 18:43:14 -0500

    Remove unused nav-menu-locations-control require
    Removed unnecessary require_once for nav-menu-locations-control.

--- src/wp-includes/class-wp-customize-manager.php
+++ src/wp-includes/class-wp-customize-manager.php
@@ -316,7 +316,6 @@ final class WP_Customize_Manager {
 		require_once ABSPATH . WPINC . '/customize/class-wp-customize-nav-menu-item-control.php';
 		require_once ABSPATH . WPINC . '/customize/class-wp-customize-nav-menu-location-control.php';
 		require_once ABSPATH . WPINC . '/customize/class-wp-customize-nav-menu-name-control.php';
-		require_once ABSPATH . WPINC . '/customize/class-wp-customize-nav-menu-locations-control.php';
 
 		require_once ABSPATH . WPINC . '/customize/class-wp-customize-nav-menus-panel.php';
 
