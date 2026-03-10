1417ce5f695d4182c775718133e6e63a018a21bb
Tim Kaye <KTS915@users.noreply.github.com>
2026-03-10 16:40:48 -0400

    Refactor customize-controls-proxy.js for clarity

--- src/wp-admin/js/customize-controls-proxy.js
+++ src/wp-admin/js/customize-controls-proxy.js
@@ -4,63 +4,82 @@
  * @since CP-2.8.0
  */
 
-/* global wp, updatedControls, _updatedControlsWatcher
- */
-window.updatedControls = window.updatedControls || {};
+/* global wp, updatedControls, _updatedControlsWatcher */
 
 if ( ! wp || ! wp.customize || ! wp.customize.bind ) {
-	var wp = window.wp = window.wp || {};
-	wp.customize = {
-		control: {
-			bind: function() {}
-		},
-		Menus: {
-			MenuItemControl: function() {}
-		}
-	};
-	wp.customize.bind = function() {};
+    var wp = window.wp = window.wp || {};
+    wp.customize = {
+        control: { bind: function() {} },
+        Menus: { MenuItemControl: function() {} }
+    };
+    wp.customize.bind = function() {};
 }
 
 window._previewSenders = {};
-
 window._customizePublishing = false;
+window._cpDirtySettings = {};
 
 window.sendSettingToPreview = function( id, value ) {
-	if ( window._customizePublishing ) {
-		return;
-	}
-	var iframe = document.querySelector( '#customize-preview iframe' );
-	if ( ! iframe || ! iframe.contentWindow ) {
-		return;
-	}
-	var src = iframe.src || '';
-	var match = src.match( /customize_messenger_channel=([^&]+)/ );
-	if ( ! match ) {
-		return;
-	}
-	var channel = match[1];
-	iframe.contentWindow.postMessage(
-		JSON.stringify( {
-			channel: channel,
-			type: 'setting',
-			data: [ id, value ]
-		} ),
-		location.origin
-	);
+    var iframe, src, match;
+
+    if ( window._customizePublishing ) { return; }
+
+    iframe = document.querySelector( '#customize-preview iframe' );
+    if ( ! iframe || ! iframe.contentWindow ) { return; }
+
+    src = iframe.src || '';
+    match = src.match( /customize_messenger_channel=([^&]+)/ );
+    if ( ! match ) { return; }
+
+    iframe.contentWindow.postMessage(
+        JSON.stringify( {
+            channel: match[1],
+            type:    'setting',
+            data:    [ id, value ]
+        } ),
+        location.origin
+    );
 };
 
+window.sendCustomizedToPreview = function() {
+    var iframe, src, match;
+
+    if ( window._customizePublishing ) { return; }
+
+    iframe = document.querySelector( '#customize-preview iframe' );
+    if ( ! iframe || ! iframe.contentWindow ) { return; }
+
+    src = iframe.src || '';
+    match = src.match( /customize_messenger_channel=([^&]+)/ );
+    if ( ! match ) { return; }
+
+    iframe.contentWindow.postMessage(
+        JSON.stringify( {
+            channel: match[1],
+            type:    'customized',
+            data:    window._cpDirtySettings
+        } ),
+        location.origin
+    );
+};
+
+window.updatedControls = window.updatedControls || {};
+
 window._updatedControlsWatcher = new Proxy( window.updatedControls, {
-	set: function( target, key, value ) {
-		target[ key ] = value;
+    set: function( target, key, value ) {
+        target[ key ] = value;
+        window._cpDirtySettings[ key ] = value;
 
-		if ( ! window._previewSenders[ key ] ) {
-			window._previewSenders[ key ] = _.debounce( function() {
-				window.sendSettingToPreview( key, target[ key ] );
-			}, 300 );
-		}
+        if ( ! window._previewSenders[ key ] ) {
+            window._previewSenders[ key ] = _.debounce( function() {
+                window.sendSettingToPreview( key, target[ key ] );
+            }, 300 );
+        }
 
-		window._previewSenders[ key ]();
+        window._previewSenders[ key ]();
+        window.sendCustomizedToPreview();
 
-		return true;
-	}
+        return true;
+    }
 } );
+window.updatedControls = window._updatedControlsWatcher;
