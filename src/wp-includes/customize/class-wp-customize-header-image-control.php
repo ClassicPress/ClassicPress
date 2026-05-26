65092a073e0097a2d943272457782e52559181c3
Tim Kaye <KTS915@users.noreply.github.com>
2026-05-26 17:13:39 -0400

    Update class-wp-customize-header-image-control.php

--- src/wp-includes/customize/class-wp-customize-header-image-control.php
+++ src/wp-includes/customize/class-wp-customize-header-image-control.php
@@ -243,7 +243,9 @@ class WP_Customize_Header_Image_Control extends WP_Customize_Image_Control {
 								?>
 
 								<div class="header-image-item">
-									<img src="<?php echo esc_url( $header['url'] ); ?>" alt="<?php echo esc_attr( $header['alt_text'] && $header['alt_text'][0] ? $header['alt_text'][0] : '' ); ?>">
+									<img src="<?php echo esc_url( $header['url'] ); ?>"
+										alt="<?php echo esc_attr( $header['alt_text'] && $header['alt_text'][0] ? $header['alt_text'][0] : '' ); ?>"
+									>
 									<button class="choice" data-customize-url="<?php echo esc_url( $header['url'] ); ?>">
 										<?php echo esc_html( $this->button_labels['frame_button'] ); ?>
 									</button>
