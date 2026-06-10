cdd77187d30f2b86842c05698db48df5d7badcf9
Guido07111975 <gjvdleest@yahoo.com>
2026-06-10 09:23:46 +0200

    Update class-wp-customize-widgets.php
    As requested by Tim: button instead of link.

--- src/wp-includes/class-wp-customize-widgets.php
+++ src/wp-includes/class-wp-customize-widgets.php
@@ -809,14 +809,14 @@ final class WP_Customize_Widgets {
 		<section id="widgets-left"><!-- compatibility with JS which looks for widget templates here -->
 			<div id="available-widgets">
 				<div class="customize-section-title">
-					<a href="#sub-accordion-panel-widgets" class="customize-section-back" tabindex="-1">
+					<button type="button" class="customize-section-back">
 						<span class="screen-reader-text">
 							<?php
 							/* translators: Hidden accessibility text. */
 							esc_html_e( 'Back' );
 							?>
 						</span>
-					</a>			
+					</button>
 					<h3>
 						<span class="customize-action">
 
