2e1ce12f1e299e475263e1556a9250177dfe7a8b
Tim Kaye <KTS915@users.noreply.github.com>
2026-07-28 17:48:27 +0200

    Adjust background properties for presets in customize-controls

--- src/wp-admin/js/customize-controls.js
+++ src/wp-admin/js/customize-controls.js
@@ -260,18 +260,18 @@ document.addEventListener( 'DOMContentLoaded', function() {
 		switch ( preset ) {
 			case 'fill':
 				_updatedControlsWatcher.background_repeat = 'no-repeat';
-				_updatedControlsWatcher.background_position_x = 'center';
-				_updatedControlsWatcher.background_position_y = 'center';
+				_updatedControlsWatcher.background_position_x = 'left';
+				_updatedControlsWatcher.background_position_y = 'top';
 				_updatedControlsWatcher.background_size = 'cover';
-				_updatedControlsWatcher.background_attachment = 'scroll';
+				_updatedControlsWatcher.background_attachment = 'fixed';
 				break;
 
 			case 'fit':
 				_updatedControlsWatcher.background_repeat = 'no-repeat';
-				_updatedControlsWatcher.background_position_x = 'center';
-				_updatedControlsWatcher.background_position_y = 'center';
+				_updatedControlsWatcher.background_position_x = 'left';
+				_updatedControlsWatcher.background_position_y = 'top';
 				_updatedControlsWatcher.background_size = 'contain';
-				_updatedControlsWatcher.background_attachment = 'scroll';
+				_updatedControlsWatcher.background_attachment = 'fixed';
 				break;
 
 			case 'repeat':
@@ -283,6 +283,11 @@ document.addEventListener( 'DOMContentLoaded', function() {
 				break;
 
 			default:
+				_updatedControlsWatcher.background_repeat = 'repeat';
+				_updatedControlsWatcher.background_position_x = 'left';
+				_updatedControlsWatcher.background_position_y = 'top';
+				_updatedControlsWatcher.background_size = 'auto';
+				_updatedControlsWatcher.background_attachment = 'scroll';
 				break;
 		}
 	}
