63b6b1ecfe28ce53bbb5da9f89cb593182247c73
mattyrob <mattyrobuk@gmail.com>
2026-08-13 15:50:31 +0100

    WP-r63258: 6.2.11
    Conflicts:
    - src/wp-admin/about.php
    - src/wp-includes/version.php
    
    ---
    
    Merges https://core.trac.wordpress.org/changeset/63258 / WordPress/wordpress-develop@710b1f91f0 to ClassicPress.

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -39,7 +39,7 @@ $cp_version = '2.7.0+dev';
  *
  * @global string $wp_version
  */
-$wp_version = '6.2.10';
+$wp_version = '6.2.11';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
