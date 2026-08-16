2743575ccc40549709e9cc3519ec462248d56a41
Tim Kaye <KTS915@users.noreply.github.com>
2026-08-16 15:50:14 +0200

    Localize admin tags script with strings
    Add localization for admin tags script.

--- src/wp-includes/script-loader.php
+++ src/wp-includes/script-loader.php
@@ -1204,6 +1204,14 @@ function wp_default_scripts( $scripts ) {
 	if ( is_admin() ) {
 		$scripts->add( 'admin-tags', "/wp-admin/js/tags$suffix.js", array( 'wp-ajax-response' ), false, 1 );
 		$scripts->set_translations( 'admin-tags' );
+		did_action( 'init' ) && $scripts->localize(
+			'admin-tags',
+			'adminTagsStrings',
+			array(
+				'dismiss' => 'Dismiss this notice.',
+				'deleted' => 'Tag successfully deleted.'
+			)
+		);
 
 		$scripts->add( 'admin-comments', "/wp-admin/js/edit-comments$suffix.js", array( 'wp-lists', 'quicktags', 'jquery-query' ), false, 1 );
 		$scripts->set_translations( 'admin-comments' );
