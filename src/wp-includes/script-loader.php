196c7f429e7d44ffec5bc59df74f272803667fc1
Tim Kaye <KTS915@users.noreply.github.com>
2026-08-21 22:43:54 +0200

    Add theme support localization for custom header

--- src/wp-includes/script-loader.php
+++ src/wp-includes/script-loader.php
@@ -1452,6 +1452,17 @@ function wp_default_scripts( $scripts ) {
 			)
 		);
 		$scripts->add( 'custom-header', "/wp-admin/js/custom-header$suffix.js", array( 'admin-media-modal' ), false, 1 );
+		$header_support = get_theme_support( 'custom-header' );
+		did_action( 'init' ) && $scripts->localize(
+			'custom-header',
+			'_cpCustomHeader',
+			array(
+				'width'       => absint( $header_support[0]['width'] ),
+				'height'      => absint( $header_support[0]['height'] ),
+				'flexWidth'   => ! empty( $header_support[0]['flex-width'] ),
+				'flexHeight'  => ! empty( $header_support[0]['flex-height'] ),
+			)
+		);
 		$scripts->add( 'custom-background', "/wp-admin/js/custom-background$suffix.js", array( 'admin-media-modal', 'coloris' ), false, 1 );
 		$scripts->add( 'media-gallery', "/wp-admin/js/media-gallery$suffix.js", array( 'jquery' ), false, 1 );
 
