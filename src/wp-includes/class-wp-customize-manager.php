e0e5ffc0b82033a131c1e21a945a283b1d390aca
Tim Kaye <KTS915@users.noreply.github.com>
2026-09-19 14:21:28 +0200

    Add nonce for dismissing autosave or lock

--- src/wp-includes/class-wp-customize-manager.php
+++ src/wp-includes/class-wp-customize-manager.php
@@ -4887,8 +4887,9 @@ final class WP_Customize_Manager {
 				'ajax' => sanitize_url( admin_url( 'admin-ajax.php', 'relative' ) ),
 			),
 			'nonce' => array(
-				'refreshLock'  => wp_create_nonce( 'customize_refresh_lock' ),
-				'takeOverLock' => wp_create_nonce( 'customize_take_over_lock' ),
+				'refreshLock'           => wp_create_nonce( 'customize_refresh_lock' ),
+				'takeOverLock'          => wp_create_nonce( 'customize_take_over_lock' ),
+				'dismissAutosaveOrLock' => wp_create_nonce( 'customize_dismiss_autosave_or_lock' ),
 			),
 			'user' => array(
 				'id' => get_current_user_id(),
