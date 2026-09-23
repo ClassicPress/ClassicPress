cf475e3210b0f374ad29182bcbfb932324f37516
mattyrob <mattyrobuk@gmail.com>
2026-09-23 11:02:35 +0100

    Fix typo

--- src/wp-admin/includes/class-wp-site-health.php
+++ src/wp-admin/includes/class-wp-site-health.php
@@ -1389,7 +1389,7 @@ class WP_Site_Health {
 			),
 			'description' => sprintf(
 				'<p>%s</p>',
-				__( 'Database repair mode is temporarily enabled in <code>wp-config.php</code> to repair and optimize database tables. The setting allows unauthenicated access so should be disbaled again at the earliest opportunity.' )
+				__( 'Database repair mode is temporarily enabled in <code>wp-config.php</code> to repair and optimize database tables. The setting allows unauthenicated access so should be disabled again at the earliest opportunity.' )
 			),
 			'actions'     => sprintf(
 				'<p><a href="%s" target="_blank" rel="noopener">%s <span class="screen-reader-text">%s</span><span aria-hidden="true" class="dashicons dashicons-external"></span></a></p>',
