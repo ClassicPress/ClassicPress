429725b350eb18998e6e89402f923e6f8d57b6c5
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2026-09-25 18:21:34 +0100

    WP-r55827: Docs: Fix a few more typos in DocBlocks and inline comments.
    Follow-up to https://core.trac.wordpress.org/changeset/6779, https://core.trac.wordpress.org/changeset/10565, https://core.trac.wordpress.org/changeset/12023, https://core.trac.wordpress.org/changeset/25224, https://core.trac.wordpress.org/changeset/27533, https://core.trac.wordpress.org/changeset/32806, https://core.trac.wordpress.org/changeset/34777, https://core.trac.wordpress.org/changeset/45262, https://core.trac.wordpress.org/changeset/46594, https://core.trac.wordpress.org/changeset/55823, https://core.trac.wordpress.org/changeset/55824.
    
    WP:Props Presskopp.
    See https://core.trac.wordpress.org/ticket/57840.
    
    ---
    
    Merges https://core.trac.wordpress.org/changeset/55827 / WordPress/wordpress-develop@faee35ab0d to ClassicPress.

--- src/wp-admin/includes/file.php
+++ src/wp-admin/includes/file.php
@@ -1379,7 +1379,7 @@ function verify_file_signature( $filename, $signatures, $filename_for_errors = f
 		);
 	}
 
-	// Check for a edge-case affecting PHP Maths abilities.
+	// Check for an edge-case affecting PHP Maths abilities.
 	if (
 		! extension_loaded( 'sodium' ) &&
 		in_array( PHP_VERSION_ID, array( 70200, 70201, 70202 ), true ) &&
